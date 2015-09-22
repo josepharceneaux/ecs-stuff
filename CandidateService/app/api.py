f509b5b6f3d828876ea6dcef61af46f70bcbbedb
Amir <amir@gettalent.com>
2015-09-22 13:57:22 -0700

    initializing directory structre for candidate services

---
+++ CandidateService/app/api.py
@@ -0,0 +1,22 @@
+__author__ = 'amirhb'
+
+import json
+from lib2to3.pgen2 import parse
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse, inputs
+from oauth import app
+
+api = Api(app=app)
+
+parser = reqparse.RequestParser()
+parse.add_argument(u'time_now', type=inputs.date)
+
+
+# Api order => CRUD: POST, GET, PUT, DELETE
+
+@app.route('/api/candidates', methods=['GET'])
+def get_candidates():
+    candidates = Candidate.query.all()
+    candidate_list = {'candidates': [candidate] for candidate in candidates}
+
+    return json.dumps({'candidates': candidate_list}), 200, {'Content-Type': 'application/json'}
