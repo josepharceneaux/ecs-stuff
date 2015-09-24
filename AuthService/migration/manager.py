f590ff73f66156626fa499e2a1034735a72afe0c
Umar Farooqi <farooqiumar@rocketmail.com>
2015-09-24 11:04:14 -0400

    GET-551 : Add User groups and roles in getTalent AuthService
    - Add an endpoint to get all roles of a particular domain
    - Added tests for this endpoint

--- AuthService/migration/manager.py
+++ AuthService/migration/manager.py
@@ -1,13 +1,15 @@
 __author__ = 'ufarooqi'
 
 from flask import Flask
+from oauth import app
 from oauth.models import *
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.script import Manager
-from flask.ext.migrate import Migrate, MigrateCommand
+from flask.ext.migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object('config')
+logger = app.config['LOGGER']
 
 db = SQLAlchemy(app)
 db.metadata.reflect(db.engine, only=['user'])
@@ -23,7 +25,10 @@ def add_roles_to_existing_users():
     existing_users = User.query.all()
     if len(roles) and len(existing_users):
         for existing_user in existing_users:
-            UserScopedRoles.add_roles(existing_user.id, roles)
+            try:
+                UserScopedRoles.add_roles(existing_user.id, roles)
+            except Exception as e:
+                logger.exception("Couldn't add user_scoped_roles to a user. %s" % e.message)
 
 if __name__ == '__main__':
     manager.run()
