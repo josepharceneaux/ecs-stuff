8fb1b46746e5137b58c4cfbe1f2b46f7502886e9
erik-farmer <erikdfarmer@gmail.com>
2015-09-28 14:33:29 -0700

    Change directories/paths for Dockerfile/Ansible set up. Introduce basic python script to copy requirements and build a Docker image locally.

--- ResumeService/resume_parsing_app/__init__.py
+++ ResumeService/resume_parsing_app/__init__.py
@@ -2,10 +2,9 @@
 __author__ = 'erikfarmer'
 
 from flask import Flask
+from resume_parsing_app.views import api
 
 
 app = Flask(__name__)
 app.config.from_object('config')
-
-from resume_parsing_app.views import api
 app.register_blueprint(api.mod)
--- ResumeService/run.py
+++ ResumeService/run.py
@@ -1,11 +1,12 @@
 """Local run file."""
 
 from flask import Flask
+from resume_parsing_app.views import api
 
 
 app = Flask(__name__)
 app.config.from_object('config')
-
+app.register_blueprint(api.mod)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
