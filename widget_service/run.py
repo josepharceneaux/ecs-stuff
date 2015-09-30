829b8fb10574d263a86ba11d3e25590789e91bc0
erik-farmer <erikdfarmer@gmail.com>
2015-09-30 09:56:27 -0700

    add run file

--- widget_service/run.py
+++ widget_service/run.py
@@ -1,1 +1,14 @@
+"""Local runfile/uWSGI callable"""
 __author__ = 'erikfarmer'
+
+
+from flask import Flask
+from widget_app.views import api
+
+
+app = Flask(__name__, template_folder='widget_app/templates', static_folder='widget_app/static')
+app.register_blueprint(api.mod, url_prefix='/widget')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8084, debug=True)
