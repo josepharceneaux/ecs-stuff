adde18a05b00bca413e9ca2cbd1351b9d42cc3d7
erik-farmer <erikdfarmer@gmail.com>
2015-09-30 09:56:07 -0700

    WIP commit, remove tempalte code from Kaiser Military. TODO: Remove all template code and turn domain specific items to ajax calls and others to JS/Dom manipulation

--- widget_service/widget_app/views/api.py
+++ widget_service/widget_app/views/api.py
@@ -1,1 +1,18 @@
-__author__ = 'erikfarmer'
+"""Widget serving/processing"""
+__author = 'erikfarmer'
+
+from flask import Blueprint
+from flask import request
+from flask import render_template
+
+
+mod = Blueprint('widget_api', __name__)
+
+
+@mod.route('/<domain>', methods=['GET', 'POST'])
+def widget(domain):
+    if request.method == 'GET':
+        if domain == 'kaiser-military':
+            return render_template('kaiser_military.html', domain=domain)
+        else:
+            return 'Return error message or awesome 404 page', 404
