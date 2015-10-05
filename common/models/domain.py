93610185ab876a1ebb0258e80201f01a9cea04bf
Waqas Younas <waqas.younas@gmail.com>
2015-10-05 16:51:04 +0500

    GET-509 Importing Events
    - Fixed some minor issues in the domain model

--- common/models/domain.py
+++ common/models/domain.py
@@ -1,23 +1,22 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 from db import db
 
 
 class Domain(db.Model):
     __tablename__ = 'domain'
-    id = Column(Integer, primary_key=True)
-    name = Column('name', String(50), unique=True, default=0)
-    usage_limitation = Column('usageLimitation', Integer)
-    expiration = Column('expiration', DateTime)
-    addedTime = Column('addedTime', DateTime)
-    organization_id = Column('organizationId', Integer, ForeignKey('organization.id'))
-    is_fair_check_on = Column('isFairCheckOn', Integer, default=0)
-    is_active = Column('isActive', Integer)
-    default_tracking_code = Column('defaultTrackingCode', String(255))
-    default_from_name = Column('defaultFromName', String(255))
-    default_culture_id = Column('defaultCultureId', Integer, ForeignKey('culture.id'), nullable=False)
-    settings_json = Column('settingsJson', String(1000))
-    dice_company_id = Column('diceCompanyId', Integer)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column('name', db.String(50), unique=True, default=0)
+    usage_limitation = db.Column('usageLimitation', db.Integer)
+    expiration = db.Column('expiration', db.DateTime)
+    addedTime = db.Column('addedTime', db.DateTime)
+    organization_id = db.Column('organizationId', db.Integer, db.ForeignKey('organization.id'))
+    is_fair_check_on = db.Column('isFairCheckOn', db.Integer, default=0)
+    is_active = db.Column('isActive', db.Integer)
+    default_tracking_code = db.Column('defaultTrackingCode', db.String(255))
+    default_from_name = db.Column('defaultFromName', db.String(255))
+    default_culture_id = db.Column('defaultCultureId', db.Integer, db.ForeignKey('culture.id'), nullable=False)
+    settings_json = db.Column('settingsJson', db.String(1000))
+    dice_company_id = db.Column('diceCompanyId', db.Integer)
 
     user = relationship('User', backref='domain')
     # TODO there was a duplicate definition of domain, removed the other one
