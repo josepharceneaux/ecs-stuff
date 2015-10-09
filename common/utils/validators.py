9035896429ad010eef8c7f4fdf8b529dc00d1a8a
Amir <amir@gettalent.com>
2015-10-09 10:58:21 -0700

    GET0-common_files
    - add conftest and validators

---
+++ common/utils/validators.py
@@ -0,0 +1,21 @@
+import re
+
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+
+def is_valid_email(email):
+    """
+    According to: http://www.w3.org/TR/html5/forms.html#valid-e-mail-address
+
+    :type email: str
+    """
+    regex = """^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"""
+    return email and re.match(regex, email)
+
+
