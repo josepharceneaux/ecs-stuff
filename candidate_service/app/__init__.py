229995aecfbf5704931a2aa89ac98886077eaffb
Amir <amir@gettalent.com>
2015-10-19 11:17:51 -0700

    GET-600 Migrate all candidate services to flask app
    - setup run file and initialize application

--- candidate_service/app/__init__.py
+++ candidate_service/app/__init__.py
@@ -1,6 +1,19 @@
+import config
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from candidate_service.common.models.db import db
+from common.error_handling import register_error_handlers
 
+# Initiate flask app and configure the application
 app = Flask(__name__)
+app.config.from_object(obj=config)
 
-db = SQLAlchemy(app)
+# Initialize application with database setup
+db.init_app(app=app)
+db.app = app
+
+# Configure logger
+logger = app.config['LOGGER']
+
+register_error_handlers(app=app, logger=logger)
+
+logger.infor("Starting candidate_service in %s environment", app.config['GT_ENVIRONMENT'])
---
+++ candidate_service/run.py
@@ -0,0 +1,5 @@
+from candidate_service.app import app
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=7000, use_reloader=True, debug=False)
+
