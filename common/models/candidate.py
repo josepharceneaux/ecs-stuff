1aadd11c61d2874156ad23e507bea746ce24ac6b
Hafiz Muhammad Basit <basit.gettalent@gmail.com>
2015-10-21 12:13:37 +0500

    GET-509: getTalent integration with Eventbrite and Meetup.
    - is_dirty removed from db Model candidate
    - Removed these fields from db model domain:
    	usage_limitation, expiration, is_fair_check_on, is_active
    - addedTime -> added_time

--- common/models/candidate.py
+++ common/models/candidate.py
@@ -47,7 +47,6 @@ class Candidate(db.Model):
     last_name = db.Column('LastName', db.String(50))
     formatted_name = db.Column('FormattedName', db.String(150))
     status_id = db.Column('StatusId', db.Integer, db.ForeignKey('candidate_status.id'))
-    is_dirty = db.Column('IsDirty', db.Boolean)
     is_web_hidden = db.Column('IsWebHidden', db.Boolean, default=False)
     is_mobile_hidden = db.Column('IsMobileHidden', db.Boolean, default=False)
     added_time = db.Column('AddedTime', db.DateTime, default=datetime.datetime.now())
--- common/models/domain.py
+++ common/models/domain.py
@@ -6,12 +6,8 @@ class Domain(db.Model):
     __tablename__ = 'domain'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column('name', db.String(50), unique=True, default=0)
-    usage_limitation = db.Column('usageLimitation', db.Integer)
-    expiration = db.Column('expiration', db.DateTime)
-    addedTime = db.Column('addedTime', db.DateTime)
+    added_time = db.Column('addedTime', db.DateTime)
     organization_id = db.Column('organizationId', db.Integer, db.ForeignKey('organization.id'))
-    is_fair_check_on = db.Column('isFairCheckOn', db.Integer, default=0)
-    is_active = db.Column('isActive', db.Integer)
     default_tracking_code = db.Column('defaultTrackingCode', db.String(255))
     default_from_name = db.Column('defaultFromName', db.String(255))
     default_culture_id = db.Column('defaultCultureId', db.Integer, db.ForeignKey('culture.id'), nullable=False)
@@ -19,7 +15,6 @@ class Domain(db.Model):
     dice_company_id = db.Column('diceCompanyId', db.Integer)
 
     user = relationship('User', backref='domain')
-    # TODO there was a duplicate definition of domain, removed the other one
 
     def __init__(self, name=None):
         self.name = name
