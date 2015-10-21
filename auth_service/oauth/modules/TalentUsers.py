94bdc8ad8d05db0bd3bb6505e8c1f5170f6c8a9d
Amir <amir@gettalent.com>
2015-10-20 17:08:17 -0700

    GET-600 Migrate all candidate services to flask app
    - add TalentUsers in modules
    - add test file + first test case for candidates
    - add run configuration for test_candidates

---
+++ auth_service/oauth/modules/TalentUsers.py
@@ -0,0 +1,7 @@
+
+# def is_current_user_admin(user_id=None):
+#     pass
+#
+#
+# def has_memebership():
+#     pass
---
+++ candidate_service/tests/api/test_v1_candidates.py
@@ -0,0 +1,43 @@
+from common.tests.conftest import *
+
+USER_PASSWORD = 'Talent15'
+
+def generate_single_candidate_data():
+    data = {'candidates': [
+        {
+            'first_name': 'Wu Tang',
+            'last_name': 'Clan',
+            'emails': [{'label': 'Primary', 'address': 'wutangclan_%s@hiphop.com' % str(uuid.uuid4())[0:8]}],
+            'phones': [{'label': 'mobile', 'value': '4084096677'}],
+            'addresses': [{'address_line_1': '%s S. third St.' % random.randint(100, 9999),
+                           'city': 'San Jose', 'state': 'CA', 'zip_code': '95118', 'country': 'US'}],
+            'work_preference': {"relocate": "true", "authorization": "US Citizen", "telecommute": "true",
+                                "travel": 25, "hourly_rate": 35.50, "salary": 75000,
+                                "tax_terms": "full-time employment", "security_clearance": "none",
+                                "third_party": "false"},
+            'educations': [{'school_name': 'SJSU', 'city': 'San Jose', 'country': 'USA'}]
+        }
+    ]}
+    return data
+
+
+def generate_multiple_candidates_data():
+    data = {'candidates': [
+        {'first_name': 'John', 'last_name': 'Kennedy',
+         'emails': [{'label': 'Primary', 'address': 'j.kennedy_%s@test.com' % str(uuid.uuid4())[0:8]}]},
+        {'first_name': 'Amir', 'last_name': 'Beheshty',
+         'emails': [{'label': 'Primary', 'address': 'j.kennedy_%s@test.com' % str(uuid.uuid4())[0:8]}]},
+        {'first_name': 'Nancy', 'last_name': 'Grey',
+         'emails': [{'label': 'Primary', 'address': 'j.kennedy_%s@test.com' % str(uuid.uuid4())[0:8]}]}
+    ]}
+    return data
+
+####################################
+# test cases for GETting candidate #
+####################################
+def test_get_candidate(sample_user):
+    user = sample_user
+    candidate_id =  1
+    r = requests.get("http://127.0.0.1:7000/v1/candidates/%s" % candidate_id)
+
+    print "\nresp = %s" % r
