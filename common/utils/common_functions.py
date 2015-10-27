c20f1c28a742250063a436900cb31240a28faefa
erik-farmer <erikdfarmer@gmail.com>
2015-10-27 15:22:42 -0700

    Move get_or_create from utils/common_functions to db_utils

--- common/utils/common_functions.py
+++ common/utils/common_functions.py
@@ -8,16 +8,3 @@ from flask import current_app
 with app.app_context():
     db.session.commit()
     print "current running app: %s" % current_app.name
-
-
-def get_or_create(session, model, defaults=None, **kwargs):
-    instance = session.query(model).filter_by(**kwargs).first()
-    if instance:
-        return instance, False
-    else:
-        params = dict((k, v) for k, v in kwargs.iteritems() if not isinstance(v, ClauseElement))
-        params.update(defaults or {})
-        instance = model(**params)
-        session.add(instance)
-        return instance, True
-
