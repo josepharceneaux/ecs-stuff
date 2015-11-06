48c55ce53143a78439e4d8da43ca2b4b0c0b8557
Amir <amir@gettalent.com>
2015-11-06 15:33:04 -0800

    Candidate-Service-Patch
    convert down create_candidate_from_params to create_or_update_candidate_from_params() and break it down into smaller functions to handle candidate creation and/or patching (updating select fields)

--- candidate_service/candidate_app/api/v1_candidates.py
+++ candidate_service/candidate_app/api/v1_candidates.py
@@ -30,7 +30,7 @@ from candidate_service.modules.talent_candidates import (
 from candidate_service.common.models.email_marketing import EmailCampaign
 # Module
 
-from candidate_service.modules.talent_candidates import create_candidate_from_params
+from candidate_service.modules.talent_candidates import create_or_update_candidate_from_params
 
 
 class CandidateResource(Resource):
@@ -164,7 +164,7 @@ class CandidateResource(Resource):
             dice_social_profile_id = body_dict.get('openweb_id')
             dice_profile_id=body_dict.get('dice_profile_id')
 
-            resp_dict = create_candidate_from_params(
+            resp_dict = create_or_update_candidate_from_params(
                 user_id=user_id,
                 first_name=first_name,
                 last_name=last_name,
@@ -191,104 +191,106 @@ class CandidateResource(Resource):
 
         return {'candidates': [{'id': candidate_id} for candidate_id in created_candidate_ids]}, 201
 
-    # def patch(self, **kwargs):
-    #     """
-    #     Function updates candidate(s).
-    #
-    #     Takes a JSON dict containing:
-    #         - a candidates key and a list of candidate-object(s) as values
-    #     Function only accepts JSON dict.
-    #     JSON dict must contain candidate's ID.
-    #
-    #     :return: {'candidates': [{'id': candidate_id}, {'id': candidate_id}, ...]}
-    #     """
-    #     # Logged in user
-    #     authed_user = request.user
-    #
-    #     # Parse request body
-    #     body_dict = request.get_json(force=True)
-    #     if not any(body_dict):
-    #         raise InvalidUsage(error_message="JSON body cannot be empty.")
-    #
-    #     # Retrieve candidate object(s)
-    #     list_of_candidate_dicts = body_dict.get('candidates')
-    #     if not any(list_of_candidate_dicts):
-    #         raise InvalidUsage(error_message="Missing input: At least one Candidate-object is required.")
-    #
-    #     # Candidate dict(s) must be in a list
-    #     if not isinstance(list_of_candidate_dicts, list):
-    #         raise InvalidUsage(error_message="Unacceptable input: Candidate object(s) must be in a list.")
-    #
-    #     for candidate_dict in list_of_candidate_dicts:
-    #
-    #         emails = [{'label': email.get('label'), 'address': email.get('address')}
-    #                   for email in candidate_dict.get('emails')]
-    #         # Email address is required for creating a candidate
-    #         if not any(emails):
-    #             raise InvalidUsage(error_message="Email address required")
-    #
-    #         # Validate email addresses' format
-    #         if filter(lambda email: not is_valid_email(email['address']), emails):
-    #             raise InvalidUsage(error_message="Invalid email address/format")
-    #
-    #         # Prevent user from updating custom field(s) from other domains
-    #         custom_fields = candidate_dict.get('custom_fields', [])
-    #         custom_field_ids = [custom_field['id'] for custom_field in custom_fields]
-    #         is_authorized = is_custom_field_authorized(custom_field_ids=custom_field_ids,
-    #                                                    user_domain_id=authed_user.domain_id)
-    #         if not is_authorized:
-    #             raise ForbiddenError(error_message="Unauthorized custom field IDs")
-    #
-    #         # Prevent user from updating area(s) of interest from other domains
-    #         areas_of_interest = candidate_dict.get('areas_of_interest', [])
-    #         area_of_interest_ids = [area_of_interest['id'] for area_of_interest in areas_of_interest]
-    #         is_authorized = is_area_of_interest_authorized(area_of_interest_ids=area_of_interest_ids,
-    #                                                        user_domain_id=authed_user.domain_id)
-    #         if not is_authorized:
-    #             raise ForbiddenError(error_message="Unauthorized area of interest IDs")
-    #
-    #         addresses = candidate_dict.get('addresses')
-    #         user_id = authed_user.id
-    #         first_name = candidate_dict.get('first_name')
-    #         last_name = candidate_dict.get('last_name')
-    #         full_name = candidate_dict.get('full_name')
-    #         status_id = body_dict.get('status_id')
-    #         phones = candidate_dict.get('phones')
-    #         educations = candidate_dict.get('educations')
-    #         military_services = candidate_dict.get('military_services')
-    #         social_networks = candidate_dict.get('social_networks', [])
-    #         work_experiences = candidate_dict.get('work_experiences', [])
-    #         work_preference = candidate_dict.get('work_preference')
-    #         preferred_locations = candidate_dict.get('preferred_locations')
-    #         skills = candidate_dict.get('skills', [])
-    #         dice_social_profile_id = body_dict.get('openweb_id')
-    #         dice_profile_id=body_dict.get('dice_profile_id')
-    #
-    #         resp_dict = create_candidate_from_params(
-    #             user_id=user_id,
-    #             first_name=first_name,
-    #             last_name=last_name,
-    #             formatted_name=full_name,
-    #             status_id=status_id,
-    #             emails=emails,
-    #             phones=phones,
-    #             addresses=addresses,
-    #             educations=educations,
-    #             military_services=military_services,
-    #             area_of_interest_ids=area_of_interest_ids,
-    #             custom_field_ids=custom_field_ids,
-    #             social_networks=social_networks,
-    #             work_experiences=work_experiences,
-    #             work_preference=work_preference,
-    #             preferred_locations=preferred_locations,
-    #             skills=skills,
-    #             dice_social_profile_id=dice_social_profile_id,
-    #             dice_profile_id=dice_profile_id
-    #         )
-    #         if not resp_dict:
-    #             raise InvalidUsage(error_message="Candidate already exists, creation failed.")
-    #
-    #     return 204
+    def patch(self, **kwargs):
+        """
+        Function updates candidate(s).
+
+        Takes a JSON dict containing:
+            - a candidates key and a list of candidate-object(s) as values
+        Function only accepts JSON dict.
+        JSON dict must contain candidate's ID.
+
+        :return: {'candidates': [{'id': candidate_id}, {'id': candidate_id}, ...]}
+        """
+        # Logged in user
+        authed_user = request.user
+
+        # Parse request body
+        body_dict = request.get_json(force=True)
+        if not any(body_dict):
+            raise InvalidUsage(error_message="JSON body cannot be empty.")
+
+        # Retrieve candidate object(s)
+        list_of_candidate_dicts = body_dict.get('candidates')
+        if not any(list_of_candidate_dicts):
+            raise InvalidUsage(error_message="Missing input: At least one Candidate-object is required.")
+
+        # Candidate dict(s) must be in a list
+        if not isinstance(list_of_candidate_dicts, list):
+            raise InvalidUsage(error_message="Unacceptable input: Candidate object(s) must be in a list.")
+
+        for candidate_dict in list_of_candidate_dicts:
+
+            emails = [{'label': email.get('label'), 'address': email.get('address')}
+                      for email in candidate_dict.get('emails')]
+            # Email address is required for creating a candidate
+            if not any(emails):
+                raise InvalidUsage(error_message="Email address required")
+
+            # Validate email addresses' format
+            if filter(lambda email: not is_valid_email(email['address']), emails):
+                raise InvalidUsage(error_message="Invalid email address/format")
+
+            # Prevent user from updating custom field(s) from other domains
+            custom_fields = candidate_dict.get('custom_fields', [])
+            custom_field_ids = [custom_field['id'] for custom_field in custom_fields]
+            is_authorized = is_custom_field_authorized(custom_field_ids=custom_field_ids,
+                                                       user_domain_id=authed_user.domain_id)
+            if not is_authorized:
+                raise ForbiddenError(error_message="Unauthorized custom field IDs")
+
+            # Prevent user from updating area(s) of interest from other domains
+            areas_of_interest = candidate_dict.get('areas_of_interest', [])
+            area_of_interest_ids = [area_of_interest['id'] for area_of_interest in areas_of_interest]
+            is_authorized = is_area_of_interest_authorized(area_of_interest_ids=area_of_interest_ids,
+                                                           user_domain_id=authed_user.domain_id)
+            if not is_authorized:
+                raise ForbiddenError(error_message="Unauthorized area of interest IDs")
+
+            user_id = authed_user.id
+            candidate_id = candidate_dict.get('candidate_id')
+            addresses = candidate_dict.get('addresses')
+            first_name = candidate_dict.get('first_name')
+            last_name = candidate_dict.get('last_name')
+            full_name = candidate_dict.get('full_name')
+            status_id = body_dict.get('status_id')
+            phones = candidate_dict.get('phones')
+            educations = candidate_dict.get('educations')
+            military_services = candidate_dict.get('military_services')
+            social_networks = candidate_dict.get('social_networks', [])
+            work_experiences = candidate_dict.get('work_experiences', [])
+            work_preference = candidate_dict.get('work_preference')
+            preferred_locations = candidate_dict.get('preferred_locations')
+            skills = candidate_dict.get('skills', [])
+            dice_social_profile_id = body_dict.get('openweb_id')
+            dice_profile_id=body_dict.get('dice_profile_id')
+
+            resp_dict = create_or_update_candidate_from_params(
+                user_id=user_id,
+                candidate_id=candidate_id,
+                first_name=first_name,
+                last_name=last_name,
+                formatted_name=full_name,
+                status_id=status_id,
+                emails=emails,
+                phones=phones,
+                addresses=addresses,
+                educations=educations,
+                military_services=military_services,
+                area_of_interest_ids=area_of_interest_ids,
+                custom_field_ids=custom_field_ids,
+                social_networks=social_networks,
+                work_experiences=work_experiences,
+                work_preference=work_preference,
+                preferred_locations=preferred_locations,
+                skills=skills,
+                dice_social_profile_id=dice_social_profile_id,
+                dice_profile_id=dice_profile_id
+            )
+            if not resp_dict:
+                raise InvalidUsage(error_message="Candidate already exists, creation failed.")
+
+        return {'candidates': 'updated'}, 204
     #
     # def delete(self, **kwargs):
     #     """
