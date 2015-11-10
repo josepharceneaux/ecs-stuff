ae0f14d09d107b239df5d9581d070ccd3249aa0c
erik-farmer <erikdfarmer@gmail.com>
2015-11-10 11:36:38 -0800

    Remove 'test' prepend from test fixtures

--- widget_service/tests/test_api.py
+++ widget_service/tests/test_api.py
@@ -36,22 +36,22 @@ APP = app.test_client()
 
 
 @pytest.fixture(autouse=True)
-def test_org(request):
+def org_fixture(request):
     org_attrs = dict(name='Rocket League All Stars - {}'.format(random_word(8)))
-    test_org, created = get_or_create(db.session, Organization, defaults=None, **org_attrs)
+    org, created = get_or_create(db.session, Organization, defaults=None, **org_attrs)
     if created:
-        db.session.add(test_org)
+        db.session.add(org)
         db.session.commit()
 
     def fin():
-        db.session.delete(test_org)
+        db.session.delete(org)
         db.session.commit()
     request.addfinalizer(fin)
-    return test_org
+    return org
 
 
 @pytest.fixture(autouse=True)
-def test_culture(request):
+def culture_fixture(request):
     culture_attrs = dict(description='Foo {}'.format(random_word(12)), code=random_word(5))
     culture, created = get_or_create(db.session, Culture, defaults=None, **culture_attrs)
     if created:
@@ -66,51 +66,51 @@ def test_culture(request):
 
 
 @pytest.fixture(autouse=True)
-def test_domain(test_culture, test_org, request):
-    test_domain = Domain(name=random_word(40),
+def domain_fixture(culture_fixture, org_fixture, request):
+    domain = Domain(name=random_word(40),
                          expiration=datetime.datetime(2050, 4, 26),
                          added_time=datetime.datetime.today(),
-                         organization_id=test_org.id, is_fair_check_on=False, is_active=1,
+                         organization_id=org_fixture.id, is_fair_check_on=False, is_active=1,
                          default_tracking_code=1, default_from_name=(random_word(100)),
-                         default_culture_id=test_culture.id,
+                         default_culture_id=culture_fixture.id,
                          settings_json=random_word(55))
-    db.session.add(test_domain)
+    db.session.add(domain)
     db.session.commit()
 
     def fin():
-        db.session.delete(test_domain)
+        db.session.delete(domain)
         db.session.commit()
     request.addfinalizer(fin)
-    return test_domain
+    return domain
 
 
 @pytest.fixture(autouse=True)
-def second_domain(test_culture, test_org, request):
-    test_domain2 = Domain(name=random_word(40),
+def second_domain(culture_fixture, org_fixture, request):
+    domain2 = Domain(name=random_word(40),
                           expiration=datetime.datetime(2050, 4, 26),
                           added_time=datetime.datetime.today(),
-                          organization_id=test_org.id, is_fair_check_on=False, is_active=1,
+                          organization_id=org_fixture.id, is_fair_check_on=False, is_active=1,
                           default_tracking_code=1, default_from_name=(random_word(100)),
-                          default_culture_id=test_culture.id,
+                          default_culture_id=culture_fixture.id,
                           settings_json=random_word(55))
-    db.session.add(test_domain2)
+    db.session.add(domain2)
     db.session.commit()
 
     def fin():
-        db.session.delete(test_domain2)
+        db.session.delete(domain2)
         db.session.commit()
     request.addfinalizer(fin)
-    return test_domain2
+    return domain2
 
 
 @pytest.fixture(autouse=True)
-def areas_of_interest(test_domain, second_domain, request):
+def areas_of_interest(domain_fixture, second_domain, request):
     aois = []
     sub_aois = []
     # Create our parent categories.
     for i in xrange(10):
         aois.append(
-            AreaOfInterest(domain_id=test_domain.id, description=random_word(16),
+            AreaOfInterest(domain_id=domain_fixture.id, description=random_word(16),
                            parent_id=None)
         )
     for i in xrange(2):
@@ -120,10 +120,10 @@ def areas_of_interest(test_domain, second_domain, request):
             )
     db.session.bulk_save_objects(aois)
     # Create our sub-categories.
-    parent_id = db.session.query(AreaOfInterest).filter_by(domain_id=test_domain.id).first().id
+    parent_id = db.session.query(AreaOfInterest).filter_by(domain_id=domain_fixture.id).first().id
     for i in xrange(2):
             sub_aois.append(
-                AreaOfInterest(domain_id=test_domain.id, description=random_word(16),
+                AreaOfInterest(domain_id=domain_fixture.id, description=random_word(16),
                                parent_id=parent_id)
             )
     db.session.bulk_save_objects(sub_aois)
@@ -156,7 +156,7 @@ def country_fixture(request):
 
 
 @pytest.fixture(autouse=True)
-def test_universities(request):
+def university_fixtures(request):
     universities = []
     for i in xrange(5):
         universities.append(
@@ -172,28 +172,28 @@ def test_universities(request):
 
 
 @pytest.fixture(autouse=True)
-def test_user(test_domain, request):
+def user_fixture(domain_fixture, request):
     user_attrs = dict(
-        domain_id=test_domain.id, first_name='Jamtry', last_name='Jonas',
+        domain_id=domain_fixture.id, first_name='Jamtry', last_name='Jonas',
         password='password', email='jamtry@{}.com'.format(random_word(7)),
         added_time=datetime.datetime.today()
     )
-    test_user, created = get_or_create(db.session, User, defaults=None, **user_attrs)
+    user, created = get_or_create(db.session, User, defaults=None, **user_attrs)
     if created:
-        db.session.add(test_user)
+        db.session.add(user)
         db.session.commit()
 
     def fin():
-        db.session.delete(test_user)
+        db.session.delete(user)
         db.session.commit()
     request.addfinalizer(fin)
-    return test_user
+    return user
 
 
 @pytest.fixture(autouse=True)
-def test_candidate_source(test_domain, request):
+def candidate_source_fixture(domain_fixture, request):
     test_source = CandidateSource(description=random_word(40), notes=random_word(40),
-                                  domain_id=test_domain.id)
+                                  domain_id=domain_fixture.id)
     db.session.add(test_source)
     db.session.commit()
 
@@ -205,10 +205,10 @@ def test_candidate_source(test_domain, request):
 
 
 @pytest.fixture(autouse=True)
-def test_majors(test_domain, request):
+def major_fixtures(domain_fixture, request):
     majors = []
     for i in xrange(5):
-        majors.append(Major(name=random_word(18), domain_id=test_domain.id))
+        majors.append(Major(name=random_word(18), domain_id=domain_fixture.id))
     db.session.bulk_save_objects(majors)
 
     def fin():
@@ -219,50 +219,50 @@ def test_majors(test_domain, request):
 
 
 @pytest.fixture(autouse=True)
-def test_widget_page(test_user, test_candidate_source, request):
-    test_widget_page = WidgetPage(url=random_word(20), page_views=0, sign_ups=0,
-                                  widget_name=random_word(20), user_id=test_user.id,
-                                  candidate_source_id=test_candidate_source.id,
-                                  welcome_email_text=random_word(40),
-                                  welcome_email_html=random_word(40),
-                                  welcome_email_subject=random_word(40),
-                                  request_email_text=random_word(40),
-                                  request_email_html=random_word(40),
-                                  request_email_subject=random_word(40),
-                                  email_source=random_word(40), reply_address=random_word(40),
-                                  unique_key=random_word(12)
-                                  )
-    db.session.add(test_widget_page)
+def widget_page_fixture(user_fixture, candidate_source_fixture, request):
+    widget_page = WidgetPage(url=random_word(20), page_views=0, sign_ups=0,
+                             widget_name=random_word(20), user_id=user_fixture.id,
+                             candidate_source_id=candidate_source_fixture.id,
+                             welcome_email_text=random_word(40),
+                             welcome_email_html=random_word(40),
+                             welcome_email_subject=random_word(40),
+                             request_email_text=random_word(40),
+                             request_email_html=random_word(40),
+                             request_email_subject=random_word(40),
+                             email_source=random_word(40), reply_address=random_word(40),
+                             unique_key=random_word(12)
+                             )
+    db.session.add(widget_page)
     db.session.commit()
 
     def fin():
-        db.session.delete(test_widget_page)
+        db.session.delete(widget_page)
         db.session.commit()
     request.addfinalizer(fin)
-    return test_widget_page
+    return widget_page
 
 
 @pytest.fixture(autouse=True)
-def test_extra_fields_location(test_domain, request):
-    state_field = CustomField(domain_id=test_domain.id, name='State of Interest',
+def extra_field_fixtures(domain_fixture, request):
+    state_field = CustomField(domain_id=domain_fixture.id, name='State of Interest',
                               type='string', added_time=datetime.datetime.now(),
                               updated_time=datetime.datetime.now())
     db.session.add(state_field)
     db.session.commit()
 
-    city_field = CustomField(domain_id=test_domain.id, name='City of Interest',
+    city_field = CustomField(domain_id=domain_fixture.id, name='City of Interest',
                              type='string', added_time=datetime.datetime.now(),
                              updated_time=datetime.datetime.now())
     db.session.add(city_field)
     db.session.commit()
 
-    nuid_field = CustomField(domain_id=test_domain.id, name='NUID', type='string',
+    nuid_field = CustomField(domain_id=domain_fixture.id, name='NUID', type='string',
                              added_time=datetime.datetime.now(),
                              updated_time=datetime.datetime.now())
     db.session.add(nuid_field)
     db.session.commit()
 
-    subscription_pref_field = CustomField(domain_id=test_domain.id, name='Subscription Preference',
+    subscription_pref_field = CustomField(domain_id=domain_fixture.id, name='Subscription Preference',
                                           type='string', added_time=datetime.datetime.now(),
                                           updated_time=datetime.datetime.now())
     db.session.add(subscription_pref_field)
@@ -278,12 +278,12 @@ def test_extra_fields_location(test_domain, request):
 
 
 @pytest.fixture(autouse=True)
-def test_oauth_credentials(request):
-    test_client = Client(client_id=random_word(16), client_secret=random_word(18))
-    db.session.add(test_client)
+def valid_oauth_credentials(request):
+    client = Client(client_id=random_word(16), client_secret=random_word(18))
+    db.session.add(client)
     db.session.commit()
-    app.config['WIDGET_CLIENT_ID'] = test_client.client_id
-    app.config['WIDGET_CLIENT_SECRET'] = test_client.client_secret
+    app.config['WIDGET_CLIENT_ID'] = client.client_id
+    app.config['WIDGET_CLIENT_SECRET'] = client.client_secret
 
     def fin():
         db.session.query(Token).delete()
@@ -291,20 +291,20 @@ def test_oauth_credentials(request):
         db.session.query(Client).delete()
         db.session.commit()
     request.addfinalizer(fin)
-    return {'test_client': test_client}
+    return client
 
 
 @pytest.fixture()
-def test_expired_oauth_credentials(test_user, request):
-    test_client = Client(client_id=random_word(16), client_secret=random_word(18))
-    db.session.add(test_client)
+def expired_oauth_credentials(user_fixture, request):
+    client = Client(client_id=random_word(16), client_secret=random_word(18))
+    db.session.add(client)
     db.session.commit()
-    app.config['WIDGET_CLIENT_ID'] = test_client.client_id
-    app.config['WIDGET_CLIENT_SECRET'] = test_client.client_secret
-    test_token = Token(client_id=test_client.client_id, user_id=test_user.id, token_type='bearer',
+    app.config['WIDGET_CLIENT_ID'] = client.client_id
+    app.config['WIDGET_CLIENT_SECRET'] = client.client_secret
+    token = Token(client_id=client.client_id, user_id=user_fixture.id, token_type='bearer',
                        access_token=random_word(18), refresh_token=random_word(18),
                        expires=datetime.datetime.now() - datetime.timedelta(days=30))
-    db.session.add(test_token)
+    db.session.add(token)
     db.session.commit()
 
     def fin():
@@ -313,16 +313,16 @@ def test_expired_oauth_credentials(test_user, request):
         db.session.query(Client).delete()
         db.session.commit()
     request.addfinalizer(fin)
-    return {'test_client': test_client}
+    return client
 
 
 @pytest.fixture(autouse=True)
-def test_email_label(request):
+def email_label_fixture(request):
     email_label_args = {'id': 1, 'description': 'Primary'}
-    test_email_label, created = get_or_create(db.session, EmailLabel, **email_label_args)
+    email_label, created = get_or_create(db.session, EmailLabel, **email_label_args)
     if not created:
         db.session.commit()
-    return test_email_label
+    return email_label
 
 
 @pytest.fixture(autouse=True)
@@ -347,8 +347,8 @@ def product_fixture(request):
 
 
 
-def test_api_returns_domain_filtered_aois(test_domain, request):
-    response = APP.get('/v1/domains/{}/interests'.format(gt_url_encrypt(test_domain.id)))
+def test_api_returns_domain_filtered_aois(domain_fixture, request):
+    response = APP.get('/v1/domains/{}/interests'.format(gt_url_encrypt(domain_fixture.id)))
     assert response.status_code == 200
     assert len(json.loads(response.data)['primary_interests']) == 10
     assert len(json.loads(response.data)['secondary_interests']) == 2
@@ -360,13 +360,13 @@ def test_api_returns_university_name_list(request):
     assert len(json.loads(response.data)['universities_list']) == 5
 
 
-def test_api_returns_majors_name_list(test_domain, request):
-    response = APP.get('/v1/domains/{}/majors'.format(gt_url_encrypt(test_domain.id)))
+def test_api_returns_majors_name_list(domain_fixture, request):
+    response = APP.get('/v1/domains/{}/majors'.format(gt_url_encrypt(domain_fixture.id)))
     assert response.status_code == 200
     assert len(json.loads(response.data)['majors']) == 5
 
 
-def test_military_candidate(test_widget_page, request):
+def test_military_candidate(widget_page_fixture, request):
     aoi_string = gen_mock_aois()
     candidate_dict = {
         'firstName': random_word(12),
@@ -382,13 +382,13 @@ def test_military_candidate(test_widget_page, request):
     }
     with APP as c:
         post_response = c.post('/v1/domains/potato/widgets/{}'.format(
-            gt_url_encrypt(test_widget_page.id)), data=candidate_dict)
+            gt_url_encrypt(widget_page_fixture.id)), data=candidate_dict)
     assert post_response.status_code == 201
     # TODO expand to check DB that our fields are there
     assert 'success' in post_response.data
 
 
-def test_university_candidate(test_widget_page, request):
+def test_university_candidate(widget_page_fixture, request):
     aoi_string = gen_mock_aois()
     candidate_dict = {
         'name': random_word(12),
@@ -405,13 +405,13 @@ def test_university_candidate(test_widget_page, request):
     }
     with APP as c:
         post_response = c.post('/v1/domains/potato/widgets/{}'.format(
-            gt_url_encrypt(test_widget_page.id)), data=candidate_dict)
+            gt_url_encrypt(widget_page_fixture.id)), data=candidate_dict)
     assert post_response.status_code == 201
     # TODO expand to check DB that our fields are there
     assert 'success' in post_response.data
 
 
-def test_corporate_candidate(test_widget_page, request):
+def test_corporate_candidate(widget_page_fixture, request):
     aoi_string = gen_mock_aois()
     candidate_dict = {
         'firstName': random_word(12),
@@ -423,13 +423,13 @@ def test_corporate_candidate(test_widget_page, request):
     }
     with APP as c:
         post_response = c.post('/v1/domains/potato/widgets/{}'.format(
-            gt_url_encrypt(test_widget_page.id)), data=candidate_dict)
+            gt_url_encrypt(widget_page_fixture.id)), data=candidate_dict)
     assert post_response.status_code == 201
     # TODO expand to check DB that our fields are there
     assert 'success' in post_response.data
 
 
-def test_expired_token_refreshes(test_widget_page, test_expired_oauth_credentials, request):
+def test_expired_token_refreshes(widget_page_fixture, expired_oauth_credentials, request):
     aoi_string = gen_mock_aois()
     candidate_dict = {
         'firstName': random_word(12),
@@ -441,7 +441,7 @@ def test_expired_token_refreshes(test_widget_page, test_expired_oauth_credential
     }
     with APP as c:
         post_response = c.post('/v1/domains/potato/widgets/{}'.format(
-            gt_url_encrypt(test_widget_page.id)), data=candidate_dict)
+            gt_url_encrypt(widget_page_fixture.id)), data=candidate_dict)
     assert post_response.status_code == 201
     # TODO expand to check DB that our fields are there
     assert 'success' in post_response.data
@@ -463,9 +463,9 @@ def test_parse_interest_ids_from_form(request):
     ]
 
 
-def test_parse_location_ids_from_form(test_extra_fields_location, request):
-    state_custom_field_id = test_extra_fields_location[0].id
-    city_custom_field_id = test_extra_fields_location[1].id
+def test_parse_location_ids_from_form(extra_field_fixtures, request):
+    state_custom_field_id = extra_field_fixtures[0].id
+    city_custom_field_id = extra_field_fixtures[1].id
     test_string = 'Northern California: All Cities|Southern California: Pomona'
     processed_ids = parse_city_and_state_ids_from_form(test_string)
     assert processed_ids == [
