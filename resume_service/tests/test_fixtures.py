195a3735f01c5162f4d95c06d7aa9f2c1e0c1614
erik-farmer <erikdfarmer@gmail.com>
2015-11-17 11:20:23 -0800

    Create db fixture for Integrity errors

--- resume_service/tests/test_fixtures.py
+++ resume_service/tests/test_fixtures.py
@@ -2,10 +2,11 @@ __author__ = 'erik@getTalent.com'
 # Standard Library
 import datetime
 # Third party
+from _mysql_exceptions import IntegrityError
 import pytest
 # Module Specific
-from common.utils.db_utils import get_or_create
-from common.utils.handy_functions import random_word
+from resume_service.common.utils.db_utils import get_or_create
+from resume_service.common.utils.handy_functions import random_word
 from resume_service.common.models.email import EmailLabel
 from resume_service.common.models.misc import Culture
 from resume_service.common.models.misc import Organization
@@ -19,6 +20,15 @@ from resume_service.common.models.misc import Country
 from resume_service.common.models.misc import Product
 
 
+def requireIntegrity(func):
+  def wrapped(*args, **kwargs):
+    try:
+      return func(*args, **kwargs)
+    except IntegrityError:
+      db.session.rollback()
+  return wrapped
+
+
 @pytest.fixture(autouse=True)
 def org_fixture(request):
     org_attrs = dict(name='Rocket League All Stars - {}'.format(random_word(8)))
@@ -26,7 +36,7 @@ def org_fixture(request):
     if created:
         db.session.add(org)
         db.session.commit()
-
+    @requireIntegrity
     def fin():
         db.session.delete(org)
         db.session.commit()
@@ -42,7 +52,7 @@ def culture_fixture(request):
     if created:
         db.session.add(culture)
         db.session.commit()
-
+    @requireIntegrity
     def fin():
         db.session.delete(culture)
         db.session.commit()
@@ -62,7 +72,7 @@ def domain_fixture(culture_fixture, org_fixture, request):
 
     db.session.add(domain)
     db.session.commit()
-
+    @requireIntegrity
     def fin():
        db.session.delete(domain)
        db.session.commit()
@@ -77,7 +87,7 @@ def user_fixture(domain_fixture, request):
                      added_time=datetime.datetime(2050, 4, 26))
     db.session.add(user)
     db.session.commit()
-
+    @requireIntegrity
     def fin():
         db.session.delete(user)
         db.session.commit()
@@ -90,7 +100,7 @@ def client_fixture(request):
     client = Client(client_id=random_word(12), client_secret=random_word(12))
     db.session.add(client)
     db.session.commit()
-
+    @requireIntegrity
     def fin():
         db.session.query(Client).delete()
         db.session.commit()
@@ -104,7 +114,7 @@ def token_fixture(user_fixture, client_fixture, request):
                        refresh_token=random_word(8), expires=datetime.datetime(2050, 4, 26))
     db.session.add(token)
     db.session.commit()
-
+    @requireIntegrity
     def fin():
         db.session.query(Token).delete()
         db.session.commit()
@@ -128,7 +138,7 @@ def country_fixture(request):
     if created:
         db.session.add(country)
         db.session.commit()
-
+    @requireIntegrity
     def fin():
         db.session.delete(country)
         db.session.commit()
