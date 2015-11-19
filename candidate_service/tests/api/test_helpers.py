6ee147a769558510b97fa985d8ce08c526dec0e4
Amir <amir@gettalent.com>
2015-11-19 15:58:00 -0800

    Candidate-Service-Patch
     - Refactor code
     - Add helper function to remove id-key from candidate object
        -  add test case for this function
    - Update test cases for test_v1_candidate_*

--- candidate_service/tests/api/test_helpers.py
+++ candidate_service/tests/api/test_helpers.py
@@ -13,7 +13,8 @@ from candidate_service.common.tests.conftest import *
 
 # Helper functions
 from helpers import (
-    check_for_id, post_to_candidate_resource, get_from_candidate_resource
+    check_for_id, post_to_candidate_resource, get_from_candidate_resource,
+    remove_id_key, generate_single_candidate_data, response_info
 )
 
 
@@ -33,47 +34,74 @@ def test_check_for_id(sample_user, user_auth):
 
     candidate_dict = get_from_candidate_resource(token, candidate_id).json()['candidate']
 
-    r = check_for_id(candidate_dict=candidate_dict)
+    r = check_for_id(_dict=candidate_dict)
     assert r is None
 
     # Send candidate-dict with an empty dict, e.g. work_preference = {}
     _dict = candidate_dict.copy()
     _dict['work_preference'] = {}
-    r = check_for_id(candidate_dict=_dict)
+    r = check_for_id(_dict=_dict)
     assert r is None
 
     # Remove top-level id-key
     _dict = candidate_dict.copy()
     del _dict['id']
-    r = check_for_id(candidate_dict=_dict)
+    r = check_for_id(_dict=_dict)
     assert r is False
 
     # Remove id-key of the first address-dict in addresses
     _dict = candidate_dict.copy()
     del _dict['addresses'][0]['id']
-    r = check_for_id(candidate_dict=_dict)
+    r = check_for_id(_dict=_dict)
     assert r is False
 
     # Remove id-key from first education-dict in educations
     _dict = candidate_dict.copy()
     del _dict['educations'][0]['id']
-    r = check_for_id(candidate_dict=_dict)
+    r = check_for_id(_dict=_dict)
     assert r is False
 
     # Remove id-key from first degree-dict in educations
     _dict = candidate_dict.copy()
     del _dict['educations'][0]['degrees'][0]['id']
-    r = check_for_id(candidate_dict=_dict)
+    r = check_for_id(_dict=_dict)
     assert r is False
 
     # Remove id-key from first degree_bullet-dict in degrees
     _dict = candidate_dict.copy()
     del _dict['educations'][0]['degrees'][0]['degree_bullets'][0]['id']
-    r = check_for_id(candidate_dict=_dict)
+    r = check_for_id(_dict=_dict)
     assert r is False
 
     # Remove id-key from work_preference
     _dict = candidate_dict.copy()
     del _dict['work_preference']['id']
-    r = check_for_id(candidate_dict=_dict)
+    r = check_for_id(_dict=_dict)
     assert r is False
+
+
+def test_remove_id_key(sample_user, user_auth):
+    """
+    Test:   Send candidate-dict with IDs to remove_id_key() to help remove all the id-keys
+            from candidate-dict
+    Expect: Candidate dict with no id-key
+    :type sample_user:  User
+    :type user_auth:    UserAuthentication
+    """
+    # Get access token
+    token = user_auth.get_auth_token(sample_user, True)['access_token']
+
+    # Create Candidate
+    data = generate_single_candidate_data()
+    print "\ndata = %s" % data['candidate']
+    create_resp = post_to_candidate_resource(token, data)
+
+    # Retrieve Candidate
+    candidate_id = create_resp.json()['candidates'][0]['id']
+    get_resp = get_from_candidate_resource(token, candidate_id)
+    candidate_dict_with_ids = get_resp.json()['candidate']
+    print response_info(get_resp.request, get_resp.json(), get_resp.status_code)
+
+    # Send Candidate dict with IDs to remove_id_keys
+    candidate_dict_without_ids = remove_id_key(_dict=candidate_dict_with_ids.copy())
+    print "candidate_dict_without_ids = %s" % candidate_dict_without_ids
--- candidate_service/tests/api/test_v1_candidates_create.py
+++ candidate_service/tests/api/test_v1_candidates_create.py
@@ -15,14 +15,14 @@ from candidate_service.common.tests.conftest import *
 # Helper functions
 from helpers import (
     response_info, post_to_candidate_resource, get_from_candidate_resource,
-    create_same_candidate
+    create_same_candidate, generate_single_candidate_data
 )
 
 ######################## Candidate ########################
 def test_create_candidate(sample_user, user_auth):
     """
     Test:   Create a new candidate and candidate's info
-    Expect: 200
+    Expect: 201
     :type   sample_user:  User
     :type   user_auth:    UserAuthentication
     """
@@ -36,6 +36,30 @@ def test_create_candidate(sample_user, user_auth):
     print response_info(create_resp.request, resp_dict, create_resp.status_code)
     assert create_resp.status_code == 201
     assert 'candidates' in resp_dict and 'id' in resp_dict['candidates'][0]
+
+
+def test_create_candidate_and_retrieve_it(sample_user, user_auth):
+    """
+    Test:   Create a Candidate and retrieve it. Ensure that the data sent in for creating the
+            Candidate is identical to the data obtained from retrieving the Candidate
+            minus id-keys
+    Expect: 201
+    :type sample_user:  User
+    :type user_auth:    UserAuthentication
+    """
+    # Get access token
+    token = user_auth.get_auth_token(sample_user, True)['access_token']
+
+    # Create Candidate
+    data = generate_single_candidate_data()
+    print "data = %s" % data
+    create_resp = post_to_candidate_resource(token, data=data)
+    print response_info(create_resp.request, create_resp.json(), create_resp.status_code)
+
+    # Retreive Candidate
+    candidate_id = create_resp.json()['candidates'][0]['id']
+    candidate_dict = get_from_candidate_resource(token, candidate_id)
+    # TODO: get-object must be identical to data after removing all ids & none values
 
 
 def test_create_an_existing_candidate(sample_user, user_auth):
