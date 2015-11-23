38f6028c7f9309654e4eca1b0003a075e06f1a9b
Waqas Younas <waqas.younas@gmail.com>
2015-11-23 13:05:08 +0500

    Made some changes in the Scheduling Service app.py so APScheduler jobs persist across app restarts

--- scheduler_service/app.py
+++ scheduler_service/app.py
@@ -26,11 +26,14 @@ from datetime import datetime, timedelta
 # from sms_campaign_service.gt_scheduler import GTScheduler
 from apscheduler.events import EVENT_JOB_ERROR
 from apscheduler.events import EVENT_JOB_EXECUTED
-from apscheduler.jobstores.redis_store import RedisJobStore
+# from apscheduler.jobstores.redis_store import RedisJobStore
 
-from apscheduler.scheduler import Scheduler
+from apscheduler.schedulers.background import BackgroundScheduler as Scheduler
+from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 scheduler = Scheduler()
-scheduler.add_jobstore(RedisJobStore(), 'redisJobStore')
+# scheduler.add_jobstore(RedisJobStore(), 'redisJobStore')
+scheduler.add_jobstore(SQLAlchemyJobStore(url='sqlite:///job_store_new.sqlite'), 'shelve')
+# scheduler.add_jobstore(ShelveJobStore('job_store'), 'shelve')
 
 
 def my_listener(event):
@@ -80,6 +83,21 @@ def tasks():
     tasks = [task.to_json() for task in tasks]
     return render_template('tasks.html', tasks=tasks)
 
+@app.route('/jobs/', methods=['GET'])
+def get_jobs():
+    jobs = scheduler.get_jobs()
+    print 'Jobs'
+    print jobs
+    if jobs:
+        print dir(jobs[0])
+    data = []
+    for job in jobs:
+        job_data = {}
+        job_data['job_id'] = job.id
+        job_data['job_name'] = job.name
+        # job_data['next_run_time'] = job.next_run_time
+        data.append(job_data)
+    return json.dumps(data)
 
 @app.route('/schedule/', methods=['GET', 'POST'])
 def task():
@@ -90,16 +108,18 @@ def task():
     start_date = datetime.now()
     start_date.replace(minute=(start_date.minute + 1))
     end_date = start_date + timedelta(minutes=5)
-    repeat_time_in_sec = int(request.args.get('frequency', 10))
+    repeat_time_in_sec = int(request.args.get('frequency', 20))
     func = request.args.get('func', 'send_sms_campaign')
     arg1 = request.args.get('arg1')
     arg2 = request.args.get('arg2')
-    job = scheduler.add_interval_job(callback,
-                                     seconds=repeat_time_in_sec,
-                                     start_date=start_date,
-                                     args=[arg1, arg2, end_date],
-                                     kwargs=dict(func=func, end_date=end_date),
-                                     jobstore='redisJobStore')
+    job = scheduler.add_job(callback, 'interval', jobstore='shelve', func=send_sms_campaign, minutes=2, misfire_grace_time=60)
+    # job = scheduler.add_interval_job(callback,
+    #                                  seconds=repeat_time_in_sec,
+    #                                  start_date=start_date,
+    #                                  args=[arg1, arg2, end_date],
+    #                                  kwargs=dict(func=func, end_date=end_date),
+    #                                  misfire_grace_time=60,
+    #                                  jobstore='shelve')
     print 'Task has been added and will run at %s ' % start_date
     return 'Task has been added to queue!!!'
     # response = send_sms_campaign(ids, body_text)
