62dd1ca5bb21f4ffa457d1cf5be1779ccbee32ec
Jordan <jordan@gettalent.com>
2015-12-02 10:40:34 -0800

    GET-619: revert turning angular debug mode on.

--- dashboard/src/core/core.config.js
+++ dashboard/src/core/core.config.js
@@ -26,7 +26,7 @@
         // During development, you may want to set debugInfoEnabled to true. This is required for tools like
         // Protractor, Batarang and ng-inspector to work correctly. However do not check in this change.
         // This flag must be set to false in production for a significant performance boost.
-        $compileProvider.debugInfoEnabled(true);
+        $compileProvider.debugInfoEnabled(false);
 
         // turn debugging off/on (no info or warn)
         if ($logProvider.debugEnabled) {
