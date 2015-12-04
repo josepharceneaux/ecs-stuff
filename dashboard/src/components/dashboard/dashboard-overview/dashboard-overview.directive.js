43c880348965d03770e42180c24054db98dfe7c4
Jordan <jordan@gettalent.com>
2015-12-04 15:51:18 -0800

    GET-619: update dashboard ("demo") page

--- dashboard/src/components/dashboard/dashboard-overview/dashboard-overview.directive.js
+++ dashboard/src/components/dashboard/dashboard-overview/dashboard-overview.directive.js
@@ -31,9 +31,57 @@
     function ControllerFunction(logger) {
         var vm = this;
 
-        vm.eventStartDate = new Date(2015, 11, 1, 17, 0);
-
+        init();
         activate();
+
+        function init() {
+            vm.recipients = [
+                { id: 'zack', name: 'Zack' },
+                { id: 'greg', name: 'Greg' },
+                { id: 'juergen', name: 'Jürgen' },
+                { id: 'viktor', name: 'Viktor' }
+            ];
+
+            vm.engagementCharts = [
+                { id: 'candidate-count-by-pipelines', name: 'Candidate Count by Pipelines' },
+                { id: 'candidate-growth-by-pipelines', name: 'Candidate Growth by Pipelines' },
+                { id: 'candidate-growth-by-user', name: 'Candidate Growth by User' },
+                { id: 'candidate-opt-outs-by-pipelines', name: 'Candidate Opt-Outs by Pipelines' },
+                { id: 'engagement-by-pipelines', name: 'Engagement by Pipelines' }
+            ];
+
+            vm.campaignTemplates = [
+                { id: 'zack', name: 'Template – Zack' },
+                { id: 'greg', name: 'Template – Greg' },
+                { id: 'juergen', name: 'Template – Jürgen' },
+                { id: 'viktor', name: 'Template – Viktor' }
+            ];
+
+            vm.eventTypes = [
+                { id: 'eventbrite', name: 'Eventbrite' },
+                { id: 'eventbrite02', name: 'Eventbrite 01' }
+            ];
+
+            vm.smartLists = [
+                { id: 'candidate-count-by-pipelines', name: 'Candidate Count by Pipelines' },
+                { id: 'candidate-growth-by-pipelines', name: 'Candidate Growth by Pipelines' },
+                { id: 'candidate-growth-by-user', name: 'Candidate Growth by User' },
+                { id: 'candidate-opt-outs-by-pipelines', name: 'Candidate Opt-Outs by Pipelines' },
+                { id: 'engagement-by-pipelines', name: 'Engagement by Pipelines' }
+            ];
+
+            vm.event = vm.event || {};
+            vm.event.startDate = new Date(2015, 11, 1, 17, 0);
+
+            vm.dateFilters = [
+                { id: '7-days', name: '7d' },
+                { id: '30-days', name: '30d' },
+                { id: '60-days', name: '60d' },
+                { id: '182-days', name: '6mo' },
+                { id: '365-days', name: '1yr' }
+            ];
+            vm.selectedTopContributionDateFilter = vm.dateFilters[0];
+        }
 
         function activate() {
             logger.log('Activated Dashboard Overview View');
