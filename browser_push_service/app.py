c2a08bdadc12c3ffb329758630190fe42d96bec7
Zohaib Ijaz <mzohaib.qc@gmail.com>
2015-12-15 23:50:35 +0500

    GET-509: Browser Push Notification Service
    - Added Roost and OneSignal to get notifications

--- browser_push_service/app.py
+++ browser_push_service/app.py
@@ -1,18 +1,8 @@
-import pusher
-
-p = pusher.Pusher(
-  app_id='160550',
-  key='7fbaa3e3bfe2607563cb',
-  secret='4f8bc5987c6f07dfd5d4',
-  ssl=True,
-  port=443
-)
-# p.trigger('test_channel', 'my_event', {'message': 'hello world'})
-
-from flask import Flask # requires `pip install flask`
+from flask import Flask
 from flask import render_template
-from flask import request
-app = Flask(__name__)
+import requests
+import json
+app = Flask(__name__, static_url_path='')
 
 
 @app.route("/")
@@ -20,10 +10,45 @@ def show_index():
     return render_template('index.html')
 
 
-@app.route("/notification")
-def trigger_notification():
-    p.trigger('getTalent', 'push_event', {'message': 'hello world'});
-    return "Notification triggered!"
+@app.route("/roost")
+def roost():
+    return render_template('roost.html')
+
+
+@app.route("/onesignal")
+def onesignal():
+    return render_template('onesignal.html')
+
+
+@app.route('/onesignal/push')
+def onesignal_push():
+    url =  'https://onesignal.com/api/v1/notifications'
+    header = {"Content-Type": "application/json",
+              "Authorization": "Basic Yjc1M2E2ZjgtYmQzYi00MTQzLTgwMjYtYTJhNTdjNjQwY2Q3"}
+
+    payload = {"app_id": "36194021-a89d-4cfa-b48b-f1fc452d17cb",
+               "included_segments": ["All"],
+               "contents": {"en": "English Message"},
+               "url": "https://www.google.com"
+               }
+
+    req = requests.post(url, headers=header, data=json.dumps(payload))
+
+    print(req.status_code, req.reason)
+    return req.content
+
+
+@app.route('/roost/push')
+def roost_push():
+    url = "https://go.goroost.com/api/push"
+    data = {'alert': 'How are you? I am roost.', 'url':'https://getbootstrap.com'}
+
+    headers = {'Content-type': 'application/json'}
+
+    r = requests.post(url, json=data, headers=headers, auth=('y654irhu7xre1a9nwagu5jc326x436nh',
+                                                             'kqu5oom8yvxy37wlrlf0b7zlm2373y8h'))
+    return r.content
+
 
 if __name__ == "__main__":
     app.run(port=8011)
