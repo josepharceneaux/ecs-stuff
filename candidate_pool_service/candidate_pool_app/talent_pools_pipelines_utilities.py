01e6a2fa9021becd36c67b0e117c4d3810b9269a
Zohaib Ijaz <mzohaib.qc@gmail.com>
2015-12-28 16:31:08 +0500

    GET-598: Push Notification Service
    - Added files that were created after getting pull from develop

---
+++ candidate_pool_service/candidate_pool_app/talent_pools_pipelines_utilities.py
@@ -0,0 +1,25 @@
+__author__ = 'ufarooqi'
+
+TALENT_PIPELINE_SEARCH_PARAMS = [
+    "query",
+    "user_ids",
+    "location",
+    "radius",
+    "area_of_interest_ids",
+    "status_ids",
+    "source_ids",
+    "minimum_years_experience",
+    "maximum_years_experience",
+    "skills",
+    "job_title",
+    "school_name",
+    "degree_type",
+    "major",
+    "degree_end_year_from",
+    "degree_end_year_to",
+    "military_service_status",
+    "military_branch",
+    "military_highest_grade",
+    "military_end_date_from",
+    "military_end_date_to"
+]
