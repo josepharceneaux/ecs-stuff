e5513f4ead9100dc00a4f4e022ee04aa7a397124
Amir <amir@gettalent.com>
2016-01-03 16:21:24 -0800

    candidate_service
    - Add code to index amazon cloud search

--- candidate_service/candidate_app/__init__.py
+++ candidate_service/candidate_app/__init__.py
@@ -253,6 +253,6 @@ db.session.commit()
 logger.info('Starting candidate_service in %s environment', app.config['GT_ENVIRONMENT'])
 
 # Index Amazon Cloud Search
-from candidate_service.modules.talent_cloud_search import define_index_fields, index_documents
-define_index_fields()
-index_documents()
+# from candidate_service.modules.talent_cloud_search import define_index_fields, index_documents
+# define_index_fields()
+# index_documents()
