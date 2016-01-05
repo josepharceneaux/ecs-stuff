c2f062cc8deecdfcb5d44e3522a849423182854e
Umar Farooqi <farooqiumar@rocketmail.com>
2016-01-05 02:31:11 -0500

    Update missing references after renaming talent_property_manager to talent_config_manager

--- resume_service/resume_parsing_app/__init__.py
+++ resume_service/resume_parsing_app/__init__.py
@@ -1,21 +1,24 @@
 """Initializer for Resume Parsing App"""
 __author__ = 'erikfarmer'
 # Third party
-from views import api
 from flask import Flask
 # Module specific
 from resume_service.common.models.db import db
+from resume_service.common.talent_config_manager import TalentConfig, ConfigKeys
 from healthcheck import HealthCheck
 import config
 
 app = Flask(__name__)
+app.config = TalentConfig(app.config).app_config
 app.config.from_object(config)
+
 db.init_app(app)
 db.app = app
 
+from views import api
 app.register_blueprint(api.mod, url_prefix='/v1')
 
-logger = app.config['LOGGER']
+logger = app.config[ConfigKeys.LOGGER]
 
 # wrap the flask app and give a heathcheck url
 health = HealthCheck(app, "/healthcheck")
@@ -23,4 +26,4 @@ health = HealthCheck(app, "/healthcheck")
 from resume_service.common.error_handling import register_error_handlers
 register_error_handlers(app, logger)
 
-logger.info("Starting resume_service in %s environment", app.config['GT_ENVIRONMENT'])
+logger.info("Starting resume_service in %s environment", app.config[ConfigKeys.ENV_KEY])
