4beb699dc92adf035c878b897bf65033e94c0862
erik-farmer <erikdfarmer@gmail.com>
2016-01-11 14:57:01 -0800

    Fix old config URL mapping

--- resume_service/resume_parsing_app/config.py
+++ resume_service/resume_parsing_app/config.py
@@ -13,31 +13,23 @@ logging.config.fileConfig(LOGGING_CONF)
 
 GT_ENVIRONMENT = os.getenv('GT_ENVIRONMENT') or 'dev'
 if GT_ENVIRONMENT == 'dev':
-    BATCH_PROCESSING_URI = 'http://127.0.0.1:8003/v1/batch/'
     BG_URL = 'http://sandbox-lensapi.burning-glass.com/v1.7/parserservice/resume'
-    CANDIDATE_CREATION_URI = 'http://127.0.0.1:8005/v1/candidates'
     DEBUG = True
     LOGGER = logging.getLogger("resume_service.dev")
     REDIS_HOST = '127.0.0.1'
     REDIS_PORT = '6379'
-    SCHEDULER_SERVICE_URI = 'http://127.0.0.1:8011/v1/tasks'
     SQLALCHEMY_DATABASE_URI = 'mysql://talent_web:s!loc976892@localhost/talent_local'
 elif GT_ENVIRONMENT == 'circle':
-    BATCH_PROCESSING_URI = 'http://127.0.0.1:8003/v1/batch/'
     BG_URL = 'http://sandbox-lensapi.burning-glass.com/v1.7/parserservice/resume'
-    CANDIDATE_CREATION_URI = 'http://127.0.0.1:8005/v1/candidates'
     DEBUG = True
     LOGGER = logging.getLogger("resume_service.ci")
-    SCHEDULER_SERVICE_URI = 'http://127.0.0.1:8011/v1/tasks'
     SQLALCHEMY_DATABASE_URI = 'mysql://talent_ci:s!ci976892@circleci.cp1kv0ecwo23.us-west-1.rds.amazonaws.com/talent_ci'
 elif GT_ENVIRONMENT == 'qa':
     BG_URL = 'http://sandbox-lensapi.burning-glass.com/v1.7/parserservice/resume'
-    CANDIDATE_CREATION_URI = 'https://candidate-service-webdev.gettalent.com/v1/candidates'
     DEBUG = False
     LOGGER = logging.getLogger("resume_service.qa")
     SQLALCHEMY_DATABASE_URI = 'mysql://talent_web:s!web976892@devdb.gettalent.com/talent_staging'
 elif GT_ENVIRONMENT == 'prod':
-    CANDIDATE_CREATION_URI = 'https://candidate-service.gettalent.com/v1/candidates'
     DEBUG = False
     LOGGER = logging.getLogger("resume_service.prod")
 else:
--- resume_service/resume_parsing_app/views/utils.py
+++ resume_service/resume_parsing_app/views/utils.py
@@ -2,12 +2,12 @@
 __author__ = 'erikfarmer'
 import json
 import requests
-from flask import current_app as app
+from resume_service.common.routes import CandidateApiUrl
 
 def create_candidate_from_parsed_resume(candidate_dict, token):
     """Sends candidate dict to candidate service and returns response. """
     payload = json.dumps({'candidates': [candidate_dict]})
-    candidate_response = requests.post(app.config['CANDIDATE_CREATION_URI'], data=payload,
+    candidate_response = requests.post(CandidateApiUrl.CANDIDATES, data=payload,
                                        headers={'Authorization': 'bearer {}'.format(token)})
     response_body = candidate_response.content
     return response_body
