1ecfba9206329ad993cd526c066f8e81e501c4f9
Hafiz Muhammad Basit <basit.gettalent@gmail.com>
2016-01-12 14:04:41 +0500

    GET-599: added find_missing_items and get_missing_keys() in handy_functions

--- flask_common/common/utils/handy_functions.py
+++ flask_common/common/utils/handy_functions.py
@@ -220,3 +220,56 @@ def http_request(method_type, url, params=None, headers=None, data=None, user_id
     else:
         logger.error('http_request: Unknown Method type %s ' % method_type)
         raise InvalidUsage('Unknown method type(%s) provided' % method_type)
+
+
+def get_missing_keys(data_dict, required_fields=None):
+    """
+    This function returns the keys that are not present in the data_dict.
+    If required_fields is provided, it only checks for those keys otherwise it checks all
+    the keys of data_dict.
+    :param data_dict:
+    :param required_fields:
+    :type data_dict: dict
+    :type required_fields: list | None
+    :return:
+    """
+    missing_keys = filter(lambda required_key: required_key not in data_dict,
+                          required_fields if required_fields else data_dict.keys())
+    return missing_keys
+
+
+def find_missing_items(data_dict, required_fields=None, verify_values_of_all_keys=False):
+    """
+    This function is used to find the missing items in given data_dict. If verify_all
+    is true, this function checks all the keys present in data_dict if they are empty or not.
+    Otherwise it verify only those fields as given in required_fields.
+    :param data_dict: given dictionary to be examined
+    :param required_fields: keys which need to be checked
+    :param verify_values_of_all_keys: indicator if we want to check values of all keys or only keys
+                            present in required_fields
+    :type data_dict: dict
+    :type required_fields: list | None
+    :type verify_values_of_all_keys: bool
+    :return: list of missing items
+    :rtype: list
+    """
+    if not data_dict:  # If data_dict is empty, return all the required_fields as missing_item
+        return [{item: ''} for item in required_fields]
+    elif verify_values_of_all_keys:
+        # Check if required keys are present
+        missing_keys = get_missing_keys(data_dict)
+        if missing_keys:
+            raise InvalidUsage('Required fields are missing from given data.%s' % missing_keys,
+                               error_code=InvalidUsage.http_status_code())
+        # verify that all keys in the data_dict have valid values
+        missing_items = [{key: value} for key, value in data_dict.iteritems()
+                         if not value and not value == 0]
+    else:
+        missing_keys = get_missing_keys(data_dict, required_fields=required_fields)
+        if missing_keys:
+            raise InvalidUsage('Required fields are missing from given data. %s' % missing_keys,
+                               error_code=InvalidUsage.http_status_code())
+        # verify that keys of data_dict present in required_field have valid values
+        missing_items = [{key: value} for key, value in data_dict.iteritems()
+                         if key in required_fields and not value and not value == 0]
+    return [missing_item for missing_item in missing_items]
