ba5308247d389cf33bfe358c9ff81c8df8d8855b
Umar Farooqi <farooqiumar@rocketmail.com>
2016-01-14 15:56:18 -0500

    Fixed a bug in Scheduler Service

--- resume_parsing_service/resume_parsing_app/views/utils.py
+++ resume_parsing_service/resume_parsing_app/views/utils.py
@@ -3,12 +3,12 @@ __author__ = 'erikfarmer'
 import json
 import requests
 from flask import current_app as app
-from resume_parsing_service.common.routes import CandidateApi
+from resume_parsing_service.common.routes import CandidateApiUrl
 
 def create_candidate_from_parsed_resume(candidate_dict, oauth_token):
     """Sends candidate dict to candidate service and returns response. """
     payload = json.dumps({'candidates': [candidate_dict]})
-    r = requests.post(CandidateApi.CANDIDATES, data=payload,
+    r = requests.post(CandidateApiUrl.CANDIDATES, data=payload,
                       headers={'Authorization': oauth_token})
     response_body = r.content
     return response_body
