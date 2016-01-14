25557019fc3717a16bc08a9cc546eddbb93db9fb
Umar Farooqi <farooqiumar@rocketmail.com>
2016-01-14 14:31:42 -0500

    Added docker file for Scheduler Service

--- resume_parsing_service/resume_parsing_app/views/utils.py
+++ resume_parsing_service/resume_parsing_app/views/utils.py
@@ -3,11 +3,12 @@ __author__ = 'erikfarmer'
 import json
 import requests
 from flask import current_app as app
+from resume_parsing_service.common.routes import CandidateApi
 
 def create_candidate_from_parsed_resume(candidate_dict, oauth_token):
     """Sends candidate dict to candidate service and returns response. """
     payload = json.dumps({'candidates': [candidate_dict]})
-    r = requests.post(app.config['CANDIDATE_CREATION_URI'], data=payload,
+    r = requests.post(CandidateApi.CANDIDATES, data=payload,
                       headers={'Authorization': oauth_token})
     response_body = r.content
     return response_body
