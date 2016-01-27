d65bdecaaeceae4826087b43b46e5a0c5b3a6f50
O. M <oamasood@gmail.com>
2016-01-27 00:03:07 -0800

    GET-816: Endpoints words should be hyphen instead of underscore separated
    -smart_list -> smartlist everywhere in API, to be clearer.  Because it's a Smartlist, not a list of Smart.
    -Also, reset_, update_, and forgot_ password changed to hyphen instead of underscore.

--- scheduler_service/tests/conftest.py
+++ scheduler_service/tests/conftest.py
@@ -28,7 +28,7 @@ def job_config_periodic(request):
         "post_data": {
             "campaign_name": "SMS Campaign",
             "phone_number": "09230862348",
-            "smart_list_id": 123456,
+            "smartlist_id": 123456,
             "content": "text to be sent as sms",
         }
     }
@@ -44,7 +44,7 @@ def job_config_one_time(request):
         "post_data": {
             "campaign_name": "Email Campaign",
             "email_id": "abc@hotmail.com",
-            "smart_list_id": 123456,
+            "smartlist_id": 123456,
             "content": "content to be sent as email",
         }
     }
