4ebba7aee83bf13fd75c9cb0e40a540fdcdc1bf2
erik-farmer <erikdfarmer@gmail.com>
2016-02-03 11:05:34 -0800

    Working single/multiple resume uploads from computer, needs success/error handling. Not sure what UI spec is for those events.

--- dashboard/src/components/candidates/candidate-add/candidate-add.directive.js
+++ dashboard/src/components/candidates/candidate-add/candidate-add.directive.js
@@ -58,17 +58,19 @@
             };
 
             vm.postFpKey = function(filepickerKey) {
-                var candidate_params = {filepicker_key: filepickerKey, resume_file_name: filepickerKey}
+                var candidate_params = {filepicker_key: filepickerKey,
+                    resume_file_name: filepickerKey, create_candidate: true};
                 console.log(candidate_params);
-                resumeService.all('parse_resume').customPOST(
-                    candidate_params,
-                    undefined,
-                    undefined,
-                    {"Content-Type": "application/x-www-form-urlencoded",
-                     "Accept": '*/*'}
-                ).then(
+                resumeService.all('parse_resume').post(candidate_params).then(
                     function (response) {
-                        console.log(response);
+                        if ('error' in response) {
+                            //Handle error via UI
+                            console.log('Error: ', response.error);
+                        }
+                        else {
+                            //Handle success in UI
+                            console.log('Success! ', response.candidate);
+                        }
                     }
                 )
             };
@@ -87,59 +89,58 @@
                 console.log('initializing FilePicker');
                 filepicker.setKey("Ay99lw0fFRXKz5n4qpXCmz");
                 filepicker.pickAndStore(
-                                {
-                                    mimetypes:[
-                                        'application/pdf',
-                                        'image/jpeg',
-                                        'image/pjpeg',
-                                        'image/png',
-                                        'image/tiff',
-                                        'image/gif',
-                                        'image/x-ms-bmp',
-                                        'image/pcx',
-                                        'image/jp2',
-                                        'image/jpc',
-                                        'image/vnd.djvu',
-                                        'application/msword',
-                                        'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-                                        'application/vnd.oasis.opendocument.text',
-                                        'application/vnd.oasis.opendocument.text-web',
-                                        'application/vnd.oasis.opendocument.text-template',
-                                        'application/vnd.oasis.opendocument.text-master',
-                                        'text/plain',
-                                        'application/rtf',
-                                        'text/rtf'
-                                    ],
-                                    container: 'modal',
-                                    maxFiles: 10000,
-                                    multiple: true,
-                                    services: ['COMPUTER', 'BOX', 'CLOUDDRIVE', 'GOOGLE_DRIVE', 'DROPBOX', 'GMAIL', 'URL', 'FLICKR', 'EVERNOTE', 'INSTAGRAM', 'SKYDRIVE', 'IMAGE_SEARCH', 'WEBCAM', 'FACEBOOK', 'GITHUB', 'CUSTOMSOURCE']
-                                },
-                                {
-                                    location: 'S3'
-                                },
-                                function(Blobs) {
-                                    var filepickerKeys = [];
-                                    $.each(Blobs, function() {
-                                        if (!this.key) {
-                                            console.error('An error occured during import of resume: %s', this.url);
-                                            showFlash('An error occured during resumes import process', 'error');
-                                            return false;
-                                        }
-                                        filepickerKeys.push(this.key)
-                                    });
-                                    if (filepickerKeys.length != Blobs.length && Blobs.length) {
-                                        return false;
-                                    }
-                                    var i;
-                                    for (i = 0; i < filepickerKeys.length; i++) {
-                                        vm.postFpKey(filepickerKeys[i]);
-                                    }
-                                },
-                                function(FPError) {
-                                    console.error("Error response to parse_filepicker_resume: ", FPError.toString());
-                                }
-                        );
+                    {
+                        mimetypes:[
+                            'application/pdf',
+                            'image/jpeg',
+                            'image/pjpeg',
+                            'image/png',
+                            'image/tiff',
+                            'image/gif',
+                            'image/x-ms-bmp',
+                            'image/pcx',
+                            'image/jp2',
+                            'image/jpc',
+                            'image/vnd.djvu',
+                            'application/msword',
+                            'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+                            'application/vnd.oasis.opendocument.text',
+                            'application/vnd.oasis.opendocument.text-web',
+                            'application/vnd.oasis.opendocument.text-template',
+                            'application/vnd.oasis.opendocument.text-master',
+                            'text/plain',
+                            'application/rtf',
+                            'text/rtf'
+                        ],
+                        container: 'modal',
+                        maxFiles: 10000,
+                        multiple: true,
+                        services: ['COMPUTER', 'BOX', 'CLOUDDRIVE', 'GOOGLE_DRIVE', 'DROPBOX', 'GMAIL', 'URL', 'FLICKR', 'EVERNOTE', 'INSTAGRAM', 'SKYDRIVE', 'IMAGE_SEARCH', 'WEBCAM', 'FACEBOOK', 'GITHUB', 'CUSTOMSOURCE']
+                    },
+                    {
+                        location: 'S3'
+                    },
+                    function(Blobs) {
+                        var filepickerKeys = [];
+                        $.each(Blobs, function() {
+                            if (!this.key) {
+                                console.error('An error occured during import of resume: %s', this.url);
+                                return false;
+                            }
+                            filepickerKeys.push(this.key)
+                        });
+                        if (filepickerKeys.length != Blobs.length && Blobs.length) {
+                            return false;
+                        }
+                        var i;
+                        for (i = 0; i < filepickerKeys.length; i++) {
+                            vm.postFpKey(filepickerKeys[i]);
+                        }
+                    },
+                    function(FPError) {
+                        console.error("Error response to parse_filepicker_resume: ", FPError.toString());
+                    }
+                );
             }
         }
     }
