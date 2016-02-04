b025fa9e099f55b4951e89a4707f5d3af0eaf539
Amir <amir@gettalent.com>
2016-02-03 16:56:30 -0800

    GET-862 Prod DB migration - Updates in models to reflect what already exists in the prod db schema

--- app_common/common/models/talent_pools_pipelines.py
+++ app_common/common/models/talent_pools_pipelines.py
@@ -6,17 +6,15 @@ from candidate import Candidate
 
 
 class TalentPool(db.Model):
-
     __tablename__ = 'talent_pool'
-
     id = db.Column(db.Integer, primary_key=True)
     domain_id = db.Column(db.Integer, db.ForeignKey('domain.id', ondelete='CASCADE'), nullable=False)
-    owner_user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'),  nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
     name = db.Column(db.String(50), nullable=False)
     description = db.Column(db.TEXT)
     added_time = db.Column(db.DateTime, server_default=db.text("CURRENT_TIMESTAMP"), nullable=False)
-    updated_time = db.Column(db.DateTime, server_default=db.text("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"),
-                             nullable=False)
+    updated_time = db.Column(db.DateTime, server_default=db.text(
+            "CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"), nullable=False)
 
     domain = db.relationship('Domain', backref=db.backref('talent_pool', cascade="all, delete-orphan"))
     user = db.relationship('User', backref=db.backref('talent_pool', cascade="all, delete-orphan"))
