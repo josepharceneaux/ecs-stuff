616348542c958496186e2c59e9f8daa7c65a0184
erik-farmer <erikdfarmer@gmail.com>
2016-02-05 12:51:30 -0800

    Fix for string boolean mismatch with create candidate field for RPS. 'create_candidate' is explicitly a BOOL

--- resume_parsing_service/app/views/parse_lib.py
+++ resume_parsing_service/app/views/parse_lib.py
@@ -42,8 +42,7 @@ def process_resume(parse_params):
     """
     filepicker_key = parse_params.get('filepicker_key')
     # None may be explicitly passed so the normal .get('attr', default) doesn't apply here.
-    create_candidate = parse_params.get('create_candidate') or 'False'
-    create_mode = True if create_candidate.lower() == 'true' else False
+    create_candidate = parse_params.get('create_candidate', False)
     talent_pools = parse_params.get('talent_pools')
     if filepicker_key:
         file_picker_bucket, unused_conn = get_s3_filepicker_bucket_and_conn()
@@ -59,9 +58,9 @@ def process_resume(parse_params):
     parsed_resume = parse_resume(file_obj=resume_file, filename_str=filename_str)
     # Emails and talent pools are the ONLY thing required to create a candidate.
     email_present = True if parsed_resume['candidate'].get('emails') else False
-    if create_mode and not email_present:
+    if create_candidate and not email_present:
         raise InvalidUsage('Email fields (required for candidate creation) could not be parsed.')
-    if create_mode and email_present and talent_pools:
+    if create_candidate and email_present and talent_pools:
         parsed_resume['candidate']['talent_pool_ids']['add'] = talent_pools
         candidate_response = create_parsed_resume_candidate(parsed_resume['candidate'],
                                                             parse_params.get('oauth'))
