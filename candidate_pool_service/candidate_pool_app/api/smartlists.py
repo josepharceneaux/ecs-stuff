dc1d57629b6f71259c8c5df3d2c06cc8e569b99b
Umar Farooqi <farooqiumar@rocketmail.com>
2016-02-04 23:18:42 -0500

    There were some issues due to scheduler_service is_jwt_request flag so resolved those issue

--- candidate_pool_service/candidate_pool_app/api/smartlists.py
+++ candidate_pool_service/candidate_pool_app/api/smartlists.py
@@ -22,7 +22,7 @@ smartlist_blueprint = Blueprint('smartlist_api', __name__)
 
 class SmartlistCandidates(Resource):
 
-    decorators = [require_oauth(allow_jwt_based_auth=True, allow_null_user=True)]
+    decorators = [require_oauth()]
 
     def get(self, **kwargs):
         """
@@ -125,7 +125,7 @@ class SmartlistResource(Resource):
 
 
 @smartlist_blueprint.route(CandidatePoolApi.SMARTLIST_STATS, methods=['POST'])
-@require_oauth(allow_jwt_based_auth=True, allow_null_user=True)
+@require_oauth(allow_null_user=True)
 @require_all_roles(DomainRole.Roles.CAN_EDIT_SMARTLISTS_STATS)
 def update_smartlists_stats():
     """
