1640950c19827002ef2744fa6f622989af390783
erik-farmer <erikdfarmer@gmail.com>
2016-02-16 13:57:44 -0800

    Basic demo angular widget available

--- widget_service/widget_app/__init__.py
+++ widget_service/widget_app/__init__.py
@@ -1,14 +1,14 @@
 __author__ = 'erikfarmer'
 
 import config
-from flask import Flask
+from flask import Flask, render_template
 from flask.ext.cors import CORS
 from healthcheck import HealthCheck
 from widget_service.common.models.db import db
 from widget_service.common.routes import WidgetApi, HEALTH_CHECK
 from widget_service.common.talent_config_manager import load_gettalent_config, TalentConfigKeys
 
-app = Flask(__name__, template_folder='templates', static_folder='static')
+app = Flask(__name__, static_folder='static')
 app.config.from_object(config)  # Widget service has its own config as well
 load_gettalent_config(app.config)
 logger = app.config[TalentConfigKeys.LOGGER]
@@ -16,6 +16,7 @@ logger = app.config[TalentConfigKeys.LOGGER]
 db.init_app(app)
 db.app = app
 from .views import api
+
 
 # Enable CORS for *.gettalent.com and localhost
 CORS(app, resources={r"*": {"origins": [r".*\.gettalent\.com", "http://127.0.0.1", "http://localhost"]}})
