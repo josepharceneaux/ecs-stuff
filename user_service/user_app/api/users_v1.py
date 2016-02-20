6ee18ea57f276fcd95817bf1691c85c8104f6146
Umar Farooqi <farooqiumar@rocketmail.com>
2016-02-19 22:07:54 -0500

    GET-963 Users once dsabled can not be reactivated

--- user_service/user_app/api/users_v1.py
+++ user_service/user_app/api/users_v1.py
@@ -4,7 +4,7 @@ from flask import request, Blueprint
 from user_service.common.routes import UserServiceApi
 from user_service.common.error_handling import *
 from user_service.common.talent_api import TalentApi
-from user_service.common.models.user import User, db, DomainRole
+from user_service.common.models.user import User, db, DomainRole, Token
 from user_service.common.utils.validators import is_valid_email
 from user_service.common.utils.auth_utils import require_oauth, require_any_role, require_all_roles
 from user_service.user_app.user_service_utilties import check_if_user_exists, create_user_for_company
@@ -97,8 +97,12 @@ class UserApi(Resource):
                 raise InvalidUsage(error_message="Email Address %s is not properly formatted" % email)
 
             # Check if user already exist
-            if check_if_user_exists(email):
-                raise InvalidUsage(error_message="User with email=%s already exists in Database" % email)
+            already_existing_user = check_if_user_exists(email)
+            if already_existing_user:
+                if already_existing_user.is_disabled:
+                    raise InvalidUsage(error_message="User with email=%s already exists in Database but is disabled" % email)
+                else:
+                    raise InvalidUsage(error_message="User with email=%s already exists in Database" % email)
 
         user_ids = []  # Newly created user object's id(s) are appended to this list
         for user_dict in users:
@@ -156,6 +160,11 @@ class UserApi(Resource):
         User.query.filter(User.id == user_id_to_delete).update({'is_disabled': '1'})
         db.session.commit()
 
+        # Delete all tokens of deleted user
+        tokens = Token.query.filter_by(user_id=user_id_to_delete).all()
+        for token in tokens:
+            token.delete()
+
         return {'deleted_user': {'id': user_id_to_delete}}
 
     # 'SELF' is for readability. It means this endpoint will be accessible to any user
@@ -193,6 +202,8 @@ class UserApi(Resource):
         phone = posted_data.get('phone', '').strip()
         thumbnail_url = posted_data.get('thumbnail_url', '').strip()
         last_read_datetime = posted_data.get('last_read_datetime', '').strip()
+        is_disabled = posted_data.get('is_disabled', '0').strip()
+
         try:
             last_read_datetime = parser.parse(last_read_datetime)
         except Exception as e:
@@ -211,7 +222,8 @@ class UserApi(Resource):
             'email': email,
             'phone': phone,
             'thumbnail_url': thumbnail_url,
-            'last_read_datetime': last_read_datetime
+            'last_read_datetime': last_read_datetime,
+            'is_disabled': is_disabled
         }
         update_user_dict = dict((k, v) for k, v in update_user_dict.iteritems() if v)
         User.query.filter(User.id == requested_user_id).update(update_user_dict)
