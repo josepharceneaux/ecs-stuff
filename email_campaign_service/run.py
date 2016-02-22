c545b6d2be87e910d59abdeb6849bad1e06016f8
Hafiz Muhammad Basit <basit.gettalent@gmail.com>
2016-02-22 16:06:32 +0500

    Trying C_FORCE_ROOT for celery worker

--- email_campaign_service/run.py
+++ email_campaign_service/run.py
@@ -1,7 +1,9 @@
+import os
 from email_campaign_app.app import app
 from email_campaign_service.common.routes import GTApis
 
 __author__ = 'jitesh'
 
 if __name__ == '__main__':
+    os.environ.setdefault('C_FORCE_ROOT', 'true')
     app.run(host='0.0.0.0', port=GTApis.EMAIL_CAMPAIGN_SERVICE_PORT, use_reloader=True, debug=False)
