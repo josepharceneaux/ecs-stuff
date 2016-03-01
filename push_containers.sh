f2c217978867f84831c0ffb9abbe3d7d83506bd1
Zohaib Ijaz <mzohaib.qc@gmail.com>
2016-03-01 23:28:23 +0500

    GET-598: Push Campaign Service
    - Added docker config files for push_campaign_service
    	- Build_Container__push_campaign_service.xml
    	- Push_Container_to_ECR__push_campaign_service.xml
    	- Run_Container__push_campaign_service.xml
    
    - Added push_campaign_service name in push_container.sh
    
    - Modified Dockerfile

--- push_containers.sh
+++ push_containers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FLASK_APPS=("auth-service" "activity-service" "resume-parsing-service" "user-service" "candidate-service" "candidate-pool-service" "spreadsheet-import-service" "scheduler-service" "sms-campaign-service" "email-campaign-service")
+FLASK_APPS=("auth-service" "activity-service" "resume-parsing-service" "user-service" "candidate-service" "candidate-pool-service" "spreadsheet-import-service" "scheduler-service" "sms-campaign-service" "email-campaign-service" "push-campaign-service")
 ecr_registry_url="528222547498.dkr.ecr.us-east-1.amazonaws.com"
 
 for app_index in ${!FLASK_APPS[@]}
