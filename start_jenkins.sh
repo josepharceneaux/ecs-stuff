4c45de9b71a19eefeee7a5a144edb500fedf2f86
Saad Abdullah <saadfast.qc@gmail.com>
2016-03-02 15:05:52 +0500

    GET-694: SN service
    - Parrallism removed for social network service

--- start_jenkins.sh
+++ start_jenkins.sh
@@ -55,5 +55,5 @@ done
 
 sleep 10
 
-
-py.test -n 24 scheduler_service/tests/ auth_service/tests/ user_service/tests activity_service/tests/ resume_parsing_service/tests candidate_pool_service/tests/ candidate_service/tests spreadsheet_import_service/tests/ email_campaign_service/tests social_network_service/tests/
+py.test social_network_service/tests/
+py.test -n 24 scheduler_service/tests/ auth_service/tests/ user_service/tests activity_service/tests/ resume_parsing_service/tests candidate_pool_service/tests/ candidate_service/tests spreadsheet_import_service/tests/ email_campaign_service/tests
