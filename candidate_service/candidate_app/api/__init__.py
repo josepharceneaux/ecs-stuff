d4230ce1f845be43a0b3bf64f247ba84907c3025
Amir <amir@gettalent.com>
2016-03-02 15:46:56 -0800

    GET-997 - Ensure connection to cloud search is established on service start to avoid delay - Add benchmarking logs

--- candidate_service/candidate_app/api/__init__.py
+++ candidate_service/candidate_app/api/__init__.py
@@ -1,1 +1,10 @@
-__author__ = 'amirhb'
+from candidate_service.candidate_app import logger
+from candidate_service.common.error_handling import InternalServerError
+from candidate_service.modules.talent_cloud_search import _cloud_search_domain_connection
+
+
+try:
+    _cloud_search_domain_connection()
+    logger.info("connection to cloud-search successful")
+except Exception:
+    raise InternalServerError("connection to cloud search failed")
