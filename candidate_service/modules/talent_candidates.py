1e69c8a5f709c1d0f154527f101041065e569904
Amir <amir@gettalent.com>
2016-03-03 15:41:07 -0800

    candidate_service_hotfix
    - Accept optional args for CandidateView API and return view information pertaining to logged in user's team members
    - Add functional test for retrieving aggregated candidate view information

--- candidate_service/modules/talent_candidates.py
+++ candidate_service/modules/talent_candidates.py
@@ -541,7 +541,35 @@ def fetch_candidate_views(candidate_id):
              } for view in candidate_views]
 
 
-def add_candidate_view(user_id, candidate_id, view_datetime=datetime.datetime.now(), view_type=3):
+def fetch_aggregated_candidate_views(domain_id, candidate_id):
+    """
+    Function will return a list of view objects displaying all the domain users
+     that viewed the candidate, last datetime of view, and the number of times each
+     user viewed the same candidate
+    :type domain_id:  int|long
+    :type candidate_id:  int|long
+    :rtype:  list[dict[str]]
+    """
+    team_members = User.all_users_of_domain(domain_id)
+    """
+    :type team_members:  list[User]
+    """
+    return_obj = []
+    for user in team_members:
+        views = CandidateView.get_by_user_and_candidate(user_id=user.id, candidate_id=candidate_id)
+        if views:
+            return_obj.append(
+                {
+                    'user_id': user.id,
+                    'last_view_datetime': str(views[-1].view_datetime),
+                    'view_count': len(views)
+                }
+            )
+
+    return return_obj
+
+
+def add_candidate_view(user_id, candidate_id):
     """
     Once a Candidate has been viewed, this function should be invoked
     and add a record to CandidateView
@@ -551,8 +579,8 @@ def add_candidate_view(user_id, candidate_id, view_datetime=datetime.datetime.no
     db.session.add(CandidateView(
         user_id=user_id,
         candidate_id=candidate_id,
-        view_type=view_type,
-        view_datetime=view_datetime
+        view_type=3,
+        view_datetime=datetime.datetime.utcnow()
     ))
     db.session.commit()
 
