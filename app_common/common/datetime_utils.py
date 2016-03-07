086317827118cb509d8e19f9a4d43f557bc05e66
O. M <oamasood@gmail.com>
2016-03-07 12:11:47 -0800

    GET-1073 email campaign GET: Add timezone to datetime in response
    -Using pytz.utc instead of own class for UTC tzinfo implementation

--- app_common/common/datetime_utils.py
+++ app_common/common/datetime_utils.py
@@ -1,18 +1,4 @@
-from datetime import tzinfo, timedelta
-
-
-class SimpleUTC(tzinfo):
-    """
-    Python's datetime.utcnow() does not technically conform to ISO-8601 because it omits the timezone information
-    at the end of the string.
-
-    http://stackoverflow.com/questions/19654578/python-utc-datetime-objects-iso-format-dont-include-z-zulu-or-zero-offset
-    """
-    def tzname(self, date_time):
-        return "UTC"
-
-    def utcoffset(self, dt):
-        return timedelta(0)
+import pytz
 
 
 def utc_isoformat(datetime_obj):
@@ -22,4 +8,4 @@ def utc_isoformat(datetime_obj):
     :return: Datetime object in ISO-8601 format (UTC)
     :rtype: str
     """
-    return datetime_obj.replace(tzinfo=SimpleUTC()).isoformat()
+    return datetime_obj.replace(tzinfo=pytz.utc).isoformat()
