6c7104dd7a0ac58070591087ad27ded59770665e
haniqadri <haniqadri.qc@gmail.com>
2016-03-15 19:08:49 +0500

    GET-996 Jenkins test issue

--- email_campaign_service/tests/api_tests/test_email_template_api.py
+++ email_campaign_service/tests/api_tests/test_email_template_api.py
@@ -11,8 +11,7 @@ from email_campaign_service.common.utils.handy_functions import add_role_to_test
 from email_campaign_service.common.models.email_campaign import (EmailTemplateFolder, UserEmailTemplate)
 from email_campaign_service.tests.modules.handy_functions import (request_to_email_template_resource,
                                                                   get_template_folder, create_email_template,
-                                                                  update_email_template, add_domain_role,
-                                                                  del_domain_roles, add_email_template)
+                                                                  update_email_template, add_email_template)
 
 
 def test_create_email_template_folder(sample_user, user_auth):
@@ -26,7 +25,6 @@ def test_create_email_template_folder(sample_user, user_auth):
 
     # Add or get Role
     role = DomainRole.Roles.CAN_CREATE_EMAIL_TEMPLATE_FOLDER
-    role_id = add_domain_role(role, domain_id)
 
     # Add 'CAN_CREATE_EMAIL_TEMPLATE_FOLDER' to sample_user
     add_role_to_test_user(sample_user, [role])
@@ -38,7 +36,6 @@ def test_create_email_template_folder(sample_user, user_auth):
     db.session.commit()
     folder_row = EmailTemplateFolder.query.filter_by(id=template_folder_id).first()
     assert folder_row.name == template_folder_name
-    del_domain_roles(role_id)
 
 
 def test_delete_email_template_folder(sample_user, sample_user_2, user_auth):
@@ -52,7 +49,6 @@ def test_delete_email_template_folder(sample_user, sample_user_2, user_auth):
 
     # Add or get Role
     role1 = DomainRole.Roles.CAN_CREATE_EMAIL_TEMPLATE_FOLDER
-    role_id1 = add_domain_role(role1, domain_id)
 
     # Add 'CAN_CREATE_EMAIL_TEMPLATE_FOLDER' to sample_user
     add_role_to_test_user(sample_user, [role1])
@@ -68,7 +64,6 @@ def test_delete_email_template_folder(sample_user, sample_user_2, user_auth):
     token2 = auth_token['access_token']
     # Add or get Role
     role = DomainRole.Roles.CAN_DELETE_EMAIL_TEMPLATE_FOLDER
-    role_id2 = add_domain_role(role, domain_id)
 
     # Add 'CAN_DELETE_EMAIL_TEMPLATE' to sample_user_2
     add_role_to_test_user(sample_user_2, [role])
@@ -80,7 +75,6 @@ def test_delete_email_template_folder(sample_user, sample_user_2, user_auth):
                      'Content-type': 'application/json'}
     )
     assert response.status_code == requests.codes.no_content
-    del_domain_roles([role_id1, role_id2])
 
 
 def test_create_email_template(sample_user, user_auth, template_body):
@@ -113,7 +107,6 @@ def test_create_email_template_without_name(sample_user, user_auth, template_bod
     domain_id = sample_user.domain_id
     # Add or get Role
     role = DomainRole.Roles.CAN_CREATE_EMAIL_TEMPLATE
-    role_id = add_domain_role(role, domain_id)
 
     # Add 'CAN_CREATE_EMAIL_TEMPLATE' to sample_user
     add_role_to_test_user(sample_user, [role])
@@ -129,9 +122,8 @@ def test_create_email_template_without_name(sample_user, user_auth, template_bod
     template_name = ''
 
     resp = create_email_template(token, sample_user.id, template_name, template_body, template_name,
-                                 is_immutable=1, folder_id=template_folder_id, domain_id=domain_id, role_id=role_id)
+                                 is_immutable=1, folder_id=template_folder_id, domain_id=domain_id, role_name=role)
     assert resp.status_code == requests.codes.bad_request
-    del_domain_roles(role_id)
 
 
 def test_create_template_without_email_body(sample_user, user_auth):
@@ -144,7 +136,7 @@ def test_create_template_without_email_body(sample_user, user_auth):
     domain_id = sample_user.domain_id
     # Add or get Role
     role = DomainRole.Roles.CAN_CREATE_EMAIL_TEMPLATE
-    role_id = add_domain_role(role, domain_id)
+    add_role_to_test_user(sample_user, [role])
     # Get access token
     auth_token = user_auth.get_auth_token(sample_user, get_bearer_token=True)
     token = auth_token['access_token']
@@ -156,9 +148,8 @@ def test_create_template_without_email_body(sample_user, user_auth):
 
     # Pass empty email template body
     resp = create_email_template(token, sample_user.id, template_name, '', template_name,
-                                 is_immutable=1, folder_id=template_folder_id, domain_id=domain_id, role_id=role_id)
+                                 is_immutable=1, folder_id=template_folder_id, domain_id=domain_id, role_name=role)
     assert resp.status_code == requests.codes.bad_request
-    del_domain_roles(role_id)
 
 
 def test_delete_email_template(sample_user, sample_user_2, template_body, user_auth):
@@ -178,7 +169,6 @@ def test_delete_email_template(sample_user, sample_user_2, template_body, user_a
 
     # Add or get Role
     role = DomainRole.Roles.CAN_DELETE_EMAIL_TEMPLATE
-    role_id2 = add_domain_role(role, sample_user_2.domain_id)
 
     # Add 'CAN_DELETE_EMAIL_TEMPLATE' to sample_user_2
     add_role_to_test_user(sample_user_2, [role])
@@ -187,7 +177,6 @@ def test_delete_email_template(sample_user, sample_user_2, template_body, user_a
     assert resp.status_code == requests.codes.no_content
     template_after_delete = UserEmailTemplate.query.get(template_id)
     assert template_after_delete is None
-    del_domain_roles(role_id2)
 
 
 def test_delete_template_with_non_existing_template_id(sample_user, sample_user_2, template_body, user_auth):
@@ -206,14 +195,12 @@ def test_delete_template_with_non_existing_template_id(sample_user, sample_user_
 
     # Add or get Role
     role = DomainRole.Roles.CAN_DELETE_EMAIL_TEMPLATE
-    role_id = add_domain_role(role, sample_user_2.domain_id)
 
     # Add 'CAN_DELETE_EMAIL_TEMPLATE' to sample_user_2
     add_role_to_test_user(sample_user_2, [role])
 
     resp = request_to_email_template_resource(token2, 'delete', template_id + 1)
     assert resp.status_code == requests.codes.not_found
-    del_domain_roles(role_id)
 
 
 def test_delete_template_from_different_domain(sample_user, user_from_diff_domain, template_body, user_auth):
@@ -233,14 +220,12 @@ def test_delete_template_from_different_domain(sample_user, user_from_diff_domai
 
     # Add or get Role
     role = DomainRole.Roles.CAN_DELETE_EMAIL_TEMPLATE
-    role_id = add_domain_role(role, user_from_diff_domain.domain_id)
 
     # Add 'CAN_DELETE_EMAIL_TEMPLATE' to sample_user_2
     add_role_to_test_user(user_from_diff_domain, [role])
 
     resp = request_to_email_template_resource(token2, 'delete', template_id)
     assert resp.status_code == requests.codes.forbidden
-    del_domain_roles(role_id)
 
 
 def test_get_email_template_via_id(sample_user, sample_user_2, template_body, user_auth):
@@ -256,7 +241,6 @@ def test_get_email_template_via_id(sample_user, sample_user_2, template_body, us
 
     # Add or get Role
     role = DomainRole.Roles.CAN_GET_EMAIL_TEMPLATE
-    role_id2 = add_domain_role(role, sample_user_2.domain_id)
 
     # Add 'CAN_GET_EMAIL_TEMPLATE' to sample_user_2
     add_role_to_test_user(sample_user_2, [role])
@@ -270,7 +254,6 @@ def test_get_email_template_via_id(sample_user, sample_user_2, template_body, us
     resp_dict = response.json()['template']
     assert isinstance(resp_dict, dict)
     assert resp_dict['id'] == template_id
-    del_domain_roles(role_id2)
 
 
 def test_get_email_template_with_non_existing_id(sample_user, sample_user_2, template_body, user_auth):
@@ -286,7 +269,6 @@ def test_get_email_template_with_non_existing_id(sample_user, sample_user_2, tem
 
     # Add or get Role
     role = DomainRole.Roles.CAN_GET_EMAIL_TEMPLATE
-    role_id2 = add_domain_role(role, sample_user_2.domain_id)
 
     # Add 'CAN_GET_EMAIL_TEMPLATE' to sample_user_2
     add_role_to_test_user(sample_user_2, [role])
@@ -298,7 +280,6 @@ def test_get_email_template_with_non_existing_id(sample_user, sample_user_2, tem
                 'Authorization': 'Bearer %s' % token2, 'Content-type': 'application/json'}
     )
     assert response.status_code == requests.codes.not_found
-    del_domain_roles(role_id2)
 
 
 def test_update_email_template(sample_user, sample_user_2, template_body, user_auth):
@@ -312,7 +293,6 @@ def test_update_email_template(sample_user, sample_user_2, template_body, user_a
     token2 = user_auth.get_auth_token(sample_user_2, True)['access_token']
     # Add or get Role
     role = DomainRole.Roles.CAN_UPDATE_EMAIL_TEMPLATE
-    role_id2 = add_domain_role(role, sample_user_2.domain_id)
     # Add 'CAN_UPDATE_EMAIL_TEMPLATE' to sample_user_2
     add_role_to_test_user(sample_user_2, [role])
 
@@ -331,7 +311,6 @@ def test_update_email_template(sample_user, sample_user_2, template_body, user_a
     resp_dict = resp.json()['template']
     print resp_dict
     assert resp_dict['body_html'] == updated_template_body
-    del_domain_roles(role_id2)
 
 
 def test_update_non_existing_email_template(sample_user, sample_user_2, template_body, user_auth):
@@ -346,7 +325,6 @@ def test_update_non_existing_email_template(sample_user, sample_user_2, template
     token2 = user_auth.get_auth_token(sample_user_2, True)['access_token']
     # Add or get Role
     role = DomainRole.Roles.CAN_UPDATE_EMAIL_TEMPLATE
-    role_id2 = add_domain_role(role, sample_user_2.domain_id)
     # Add 'CAN_UPDATE_EMAIL_TEMPLATE' to sample_user_2
     add_role_to_test_user(sample_user_2, [role])
 
@@ -361,6 +339,4 @@ def test_update_non_existing_email_template(sample_user, sample_user_2, template
                                  template['template_name'],
                                  updated_template_body, '', template['template_folder_id'],
                                  template['is_immutable'])
-    db.session.commit()
     assert resp.status_code == requests.codes.not_found
-    del_domain_roles(role_id2)
