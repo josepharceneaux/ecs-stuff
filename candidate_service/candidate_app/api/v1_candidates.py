330e22b7861b6eec1c34ffd804fa8dad2bcc7655
Janim007 <ahmed_janim@yahoo.com>
2016-03-23 22:07:05 +0200

    fix: waiting for talent_pool_id from email clients, where it should be queried from db on runtime

--- candidate_service/candidate_app/api/v1_candidates.py
+++ candidate_service/candidate_app/api/v1_candidates.py
@@ -51,7 +51,7 @@ from candidate_service.common.models.candidate import (
     CandidateDevice, CandidateSubscriptionPreference, CandidatePhoto, CandidateTextComment
 )
 from candidate_service.common.models.misc import AreaOfInterest, Frequency, CustomField
-from candidate_service.common.models.talent_pools_pipelines import TalentPipeline
+from candidate_service.common.models.talent_pools_pipelines import TalentPipeline, TalentPoolCandidate
 from candidate_service.common.models.associations import CandidateAreaOfInterest
 from candidate_service.common.models.user import User, DomainRole
 
@@ -1313,15 +1313,21 @@ class CandidateClientEmailCampaignResource(Resource):
 
         campaign_name = 'Campaign %s %s' % (subject, email_client_name[0])
         list_name = 'List %s' % campaign_name
+
         # In first version, we only have one candidate when sending campaign via email-client.
         # So, here we are picking first candidate and the id of first talent_pool it is
         # associated with.
+
+        first_candidate_id = candidates_list[0].get('id')
+        talent_pool_id_for_first_candidate = db.session.query(TalentPoolCandidate).\
+            filter(TalentPoolCandidate.candidate_id == first_candidate_id).first()
+
         talent_pipeline = TalentPipeline.get_by_user_and_talent_pool_id(
-            request.user.id, body_dict.get('candidates')[0]['talent_pool_ids'][0])
+            request.user.id, talent_pool_id_for_first_candidate.talent_pool_id)
         smartlist_object = {
             "name": list_name,
             "candidate_ids": candidate_ids,
-            "talent_pipeline_id": talent_pipeline.id
+            "talent_pipeline_id": talent_pipeline.id if talent_pipeline else 1
         }
 
         create_smartlist_resp = create_smartlist(smartlist_object, request.headers.get('authorization'))
