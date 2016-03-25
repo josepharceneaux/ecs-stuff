ca7ff14ba17eefdb6ef29a96835901f837bce5e5
Joseph Arceneaux <joseph.arceneaux@orange-ftgroup.com>
2016-03-25 14:13:28 -0500

    Revert "Use init_talent_app."
    This reverts commit c20428730643430bfd9788be3c0f486a82467b40.

--- candidate_pool_service/candidate_pool_app/__init__.py
+++ candidate_pool_service/candidate_pool_app/__init__.py
@@ -1,24 +1,39 @@
 __author__ = 'ufarooqi'
-
 from flask.ext.cors import CORS
 from flask.ext.cache import Cache
-
-from candidate_pool_service.common.utils.models_utils import init_talent_app
 from candidate_pool_service.common.routes import HEALTH_CHECK, CandidatePoolApi, GTApis
 from candidate_pool_service.common.talent_config_manager import load_gettalent_config, TalentConfigKeys
 from candidate_pool_service.common.utils.talent_ec2 import get_ec2_instance_id
 from candidate_pool_service.common.talent_flask import TalentFlask
 from candidate_pool_service.common.talent_celery import init_celery_app
-from candidate_pool_service.common.models.db import db
 
-app, logger = init_talent_app(__name__)
+app = TalentFlask(__name__)
+load_gettalent_config(app.config)
+logger = app.config[TalentConfigKeys.LOGGER]
+logger.info("Starting app %s in EC2 instance %s", app.import_name, get_ec2_instance_id())
 
 try:
+    from candidate_pool_service.common.error_handling import register_error_handlers
+    print "register error handlers"
+    register_error_handlers(app, logger)
+
+    from candidate_pool_service.common.models.db import db
+    db.init_app(app)
+    db.app = app
+
     # Instantiate Flask-Cache object
     cache = Cache(app, config={'CACHE_TYPE': 'redis', 'CACHE_REDIS_URL': app.config['REDIS_URL']})
 
     # Instantiate Celery
     celery_app = init_celery_app(app, 'celery_stats_scheduler')
+
+    # Initialize Redis Cache
+    from candidate_pool_service.common.redis_cache import redis_store
+    redis_store.init_app(app)
+
+    # wrap the flask app and give a heathcheck url
+    from healthcheck import HealthCheck
+    health = HealthCheck(app, HEALTH_CHECK)
 
     from api.talent_pools import talent_pool_blueprint
     from api.talent_pipelines import talent_pipeline_blueprint
@@ -30,6 +45,11 @@ try:
 
     db.create_all()
     db.session.commit()
+
+    # Enable CORS for *.gettalent.com and localhost
+    CORS(app, resources=GTApis.CORS_HEADERS)
+
+    logger.info("Starting candidate_pool_service in %s environment", app.config[TalentConfigKeys.ENV_KEY])
 
 except Exception as e:
     logger.exception("Couldn't start candidate_pool_service in %s environment because: %s"
