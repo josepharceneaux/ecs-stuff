b36f6a975679698d71b8a31a4e502b9d58d091c1
Hafiz Muhammad Basit <basit.gettalent@gmail.com>
2016-03-31 16:32:38 +0500

    GET-1106: Fixing GET-1106 and adding tests for validations of email-campaign creation

--- email_campaign_service/tests/conftest.py
+++ email_campaign_service/tests/conftest.py
@@ -1,3 +1,5 @@
+from email_campaign_service.common.models.misc import Frequency
+
 __author__ = 'basit'
 
 import re
@@ -10,7 +12,8 @@ from email_campaign_service.common.models.email_campaign import (EmailClient, Us
 from email_campaign_service.tests.modules.handy_functions import (create_email_campaign,
                                                                   assign_roles,
                                                                   create_email_campaign_smartlist,
-                                                                  delete_campaign, send_campaign)
+                                                                  delete_campaign, send_campaign,
+                                                                  create_smartlist_with_candidate)
 
 
 @pytest.fixture()
@@ -247,3 +250,27 @@ def template_id(domain_id):
     template = db.session.query(UserEmailTemplate).filter_by(template_folder_id=template_folder_id)
 
     return template['id']
+
+
+@pytest.fixture(params=['name', 'subject', 'body_html', 'frequency_id', 'list_ids'])
+def invalid_data_for_campaign_creation(request):
+    """
+    This function returns the data to create an email campaign. It also removes a required
+    field from data to make it invalid.
+    Required fields are 'name', 'subject', 'body_html', 'frequency_id', 'list_ids'
+    """
+    email_from = 'no-reply@gettalent.com'
+    reply_to = fake.safe_email()
+    body_text = fake.sentence()
+    body_html = "<html><body><h1>%s</h1></body></html>" % body_text
+    campaign_data = {'name': fake.name(),
+                     'subject': fake.sentence(),
+                     'from': email_from,
+                     'reply_to': reply_to,
+                     'body_html': body_html,
+                     'body_text': body_text,
+                     'frequency_id': Frequency.ONCE,
+                     'list_ids': [fake.random_number()]
+                     }
+    del campaign_data[request.param]
+    return campaign_data, request.param
