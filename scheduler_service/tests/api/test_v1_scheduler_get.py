15a5086738ddff963bda11391e8cc5553888845d
Saad Abdullah <saadfast.qc@gmail.com>
2016-04-05 16:14:34 +0500

    Hotfix: Scheduler Service

--- scheduler_service/tests/api/test_v1_scheduler_get.py
+++ scheduler_service/tests/api/test_v1_scheduler_get.py
@@ -5,11 +5,9 @@ using bearer token
 
 # Std imports
 import json
+import uuid
 
 # Third party imports
-import random
-import string
-
 import requests
 
 # Application imports
@@ -71,8 +69,7 @@ class TestSchedulerGet(object):
         :return:
         """
         # Assign task_name in job post data (general task)
-        job_config['task_name'] = 'General_Named_Task' + ''.join([random.choice(string.ascii_letters + string.digits)
-                                                                  for _ in xrange(16)])
+        job_config['task_name'] = 'General_Named_Task' +  uuid.uuid4().__str__()[0:8]
 
         # Get the job using correct task name
         response_get = requests.get(SchedulerApiUrl.TASK_NAME % job_config['task_name'],
@@ -111,8 +108,7 @@ class TestSchedulerGet(object):
         :return:
         """
         # Assign task_name in job post data (general task)
-        job_config['task_name'] = 'General_Named_Task' + ''.join([random.choice(string.ascii_letters + string.digits)
-                                                                  for _ in xrange(16)])
+        job_config['task_name'] = 'General_Named_Task' + uuid.uuid4().__str__()[0:8]
         response = requests.post(SchedulerApiUrl.TASKS, data=json.dumps(job_config),
                                  headers=auth_header_no_user)
         assert response.status_code == 201
