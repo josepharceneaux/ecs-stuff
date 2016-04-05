2549464a32bb3e66e8c4eca6ddcf26c8a77b38bd
Joseph Arceneaux <joseph.arceneaux@orange-ftgroup.com>
2016-04-05 12:09:48 -0700

    Add searching and recording to the DB.

--- app_common/common/models/migration.py
+++ app_common/common/models/migration.py
@@ -1,7 +1,9 @@
+from datetime import datetime
+
 from db import db
 
 class Migration(db.Model):
     __tablename__ = 'migration'
     id = db.Column('Id', db.Integer, primary_key=True)
     name = db.Column('Name', db.String(200))
-    run_at_timestamp = db.Column('RunAtTimestamp', db.DateTime, default=datetime.datetime.now())
+    run_at_timestamp = db.Column('RunAtTimestamp', db.DateTime, default=datetime.now())
