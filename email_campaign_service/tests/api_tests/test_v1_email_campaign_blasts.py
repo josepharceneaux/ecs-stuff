3a4a77a335a7c5eb475c39bbd42619967a0cb051
Hafiz Muhammad Basit <basit.gettalent@gmail.com>
2016-04-12 12:51:04 +0500

    GET-1181: Polling added in one new test added in develop for email-campaign-blasts

--- email_campaign_service/tests/api_tests/test_v1_email_campaign_blasts.py
+++ email_campaign_service/tests/api_tests/test_v1_email_campaign_blasts.py
@@ -66,6 +66,8 @@ class TestEmailCampaignBlasts(object):
         But email-campaign should be sent to only primary-email-addresses of candidates.
         If primary email is not found then email-campaign will be sent to latest emails added for candidates.
         """
+        expected_count = 2
+        assert_campaign_sends(sent_campaign_multiple_email, expected_count)
         response = requests.get(
             self.URL % sent_campaign_multiple_email.id,
             headers=dict(Authorization='Bearer %s' % access_token_first))
@@ -73,7 +75,7 @@ class TestEmailCampaignBlasts(object):
                                                             check_count=False)
         json_resp = response.json()[self.ENTITY]
         assert json_resp[0]['campaign_id'] == int(sent_campaign_multiple_email.id)
-        assert json_resp[0]['sends'] == 2
+        assert json_resp[0]['sends'] == expected_count
 
     def test_get_blasts_with_paginated_response(self, access_token_first, sent_campaign):
         """
