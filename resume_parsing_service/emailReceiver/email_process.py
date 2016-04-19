aab6725b169b7c1d5e583db62f8e611a43e9cc04
erik-farmer <erikdfarmer@gmail.com>
2016-04-19 16:04:31 -0700

    Add some abstraction and comments for new utility functions.

--- resume_parsing_service/emailReceiver/email_process.py
+++ resume_parsing_service/emailReceiver/email_process.py
@@ -12,67 +12,41 @@ import pytz
 import requests
 from sqlalchemy.exc import SQLAlchemyError
 
-from models import session, Client, Token, User
+from .models import session, Client, TalentPool, Token, User
 
 RESUMES_BUCKET = 'resume-emails'
-EMAIL_HASH_PATTERN = re.compile(r'\+(.+?)\@')
+EMAIL_HASH_PATTERN = re.compile(r'\+(.+?)@')
 AUTH_URL = 'http://localhost:8001/v1/oauth2/token'
 RESUMES_URL = 'http://localhost:8003/v1/parse_resume'
 
-s3_client = boto3.client(
+S3_CLIENT = boto3.client(
     's3',
     aws_access_key_id='AKIAJBUXGOYRGOGOMULA',
     aws_secret_access_key='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'
 )
 
-def lambda_handler(event, context):
+
+def lambda_handler(event, unused_context):
     """
-    Basic conversion of email file from s3 and upload extracted file attachment.
+    Extracts a resume attachment from an email uploaded to S3 and sends it to resumeParsing.
     :param event:
-    :param context:
+    :param unused_context:
     :return:
     """
-
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = urllib.unquote_plus(event['Records'][0]['s3']['object']['key']).decode('utf8')
     try:
-        email_obj = s3_client.get_object(Bucket=RESUMES_BUCKET, Key=key)
+        email_obj = S3_CLIENT.get_object(Bucket=RESUMES_BUCKET, Key=key)
         email_file = email.message_from_string(email_obj['Body'].read())
     except Exception as e:
-        print('Error getting object {} from bucket {}. {}.'.format(key, bucket, e))
+        print 'Error getting object {} from bucket {}. {}.'.format(key, bucket, e)
         raise e
-    sender = email_file.get('From')
-    if not sender:
-        raise KeyError('Could not retrieve email sender with file {}'.format(key))
-    receiver = email_file.get('To')
-    if not receiver:
-        raise KeyError('Could not retrieve email receiver with file {}'.format(key))
-    talent_pool_hashes = EMAIL_HASH_PATTERN.findall(sender)
-    if not talent_pool_hashes:
-        raise UserWarning('The email address {} did not specify a talentPool hash'.format(receiver))
-    elif len(talent_pool_hashes) > 1:
-        raise UserWarning('RegEx obtained more than one hash value from {}'.format(receiver))
-    # TODO get talent pool id
-    user = session.query(User).filter(User.email==sender).first()
-    if not user:
-        raise UserWarning('Unable to retrieve a user with the email {}'.format(sender))
-    token = session.query(Token).filter(Token.user_id==user.Id)
-    if not token:
-        raise SQLAlchemyError('Unable to retrieve a token with the user_id {}'.format(user.Id))
-    access_token = token.access_token
-    if token.expires < utcnow():
-        access_token = refreshToken(token)
-    """
-    For an email with an attachment the first item in payloads should be an email.message.Message
-    with content-type of `multipart/alternative`. This then should produce another list of two items
-    if you run .get_payloads() on that (one for plain text and another for HTML). The second item in
-    the first list should be the file attachment.
-    """
-    payloads = email_file.get_payload()
-    if len(payloads) < 2:
-        raise UserWarning('User email {} does not have sufficient payload.'.format(key))
-    raw_attachment = payloads[1]
+
+    validated_sender, talent_pool_hash = validate_email_file(email_file, key)
+    access_token = get_user_access_token(validated_sender)
+    raw_attachment = get_email_attachement(email_file, key)
     attachment_filename = raw_attachment.get_filename()
+    talent_pool_id = get_desired_talent_pool(talent_pool_hash)
     headers = {
         'Authorization': 'Bearer {}'.format(access_token),
         'Content-Type': 'application/json'
@@ -83,18 +57,86 @@ def lambda_handler(event, context):
             'resume_file_name': attachment_filename,
             'resume_file': outfile,
             'create_candidate': True,
-            # 'talent_pool_id': foo
+            'talent_pool_id': talent_pool_id
         }
         try:
             resume_response_response = requests.post(RESUMES_URL, headers=headers, data=payload)
             response_content = json.loads(resume_response_response.content)
         except (requests.exceptions.ConnectionError, requests.exceptions.Timeout) as e:
             raise e('Error during POST to resumeParsingService')
-    if resume_response_response.status_code is not requests.codes.ok:
-        raise AssertionError('Candidate was not created from email {}. Content {}'.format(
-            key, response_content))
-    else:
-        print 'Candidate created from {}, with id {}'.format(key, response_content['candidate']['id'])
+        finally:
+            if resume_response_response.status_code is not requests.codes.ok:
+                raise AssertionError('Candidate was not created from email {}. Content {}'.format(
+                    key, response_content))
+            else:
+                print 'Candidate created from {}, with id {}'.format(key, response_content['candidate']['id'])
+
+
+def validate_email_file(email_file, key):
+    """
+    Consumes a email file, validates proper to/from, and extracts a supplied hash using '+'.
+    :param email_file:
+    :param key:
+    :return (str sender, str talent_pool_hash):
+    """
+    sender = email_file.get('From')
+    if not sender:
+        raise KeyError('Could not retrieve email sender with file {}'.format(key))
+    receiver = email_file.get('To')
+    if not receiver:
+        raise KeyError('Could not retrieve email receiver with file {}'.format(key))
+    talent_pool_hashes = EMAIL_HASH_PATTERN.findall(receiver)
+    if not talent_pool_hashes:
+        raise UserWarning('The email address {} did not specify a talentPool hash'.format(receiver))
+    elif len(talent_pool_hashes) > 1:
+        raise UserWarning('RegEx obtained more than one hash value from {}'.format(receiver))
+    talent_pool_hash = talent_pool_hashes[0]
+    return sender, talent_pool_hash
+
+
+def get_user_access_token(email_address):
+    """
+    Retrieves a user's access token from the database and refreshes it if needed.
+    :param str email_address:
+    :return str access_token:
+    """
+    user = session.query(User).filter(User.email == email_address).first()
+    if not user:
+        raise UserWarning('Unable to retrieve a user with the email {}'.format(email_address))
+    token = session.query(Token).filter(Token.user_id == user.Id)
+    if not token:
+        raise SQLAlchemyError('Unable to retrieve a token with the user_id {}'.format(user.Id))
+    access_token = token.access_token
+    if token.expires < utcnow():
+        access_token = refresh_token(token)
+    return access_token
+
+
+def get_email_attachement(email_file, key):
+    """
+    For an email with an attachment the first item in payloads should be an email.message.Message
+    with content-type of `multipart/alternative`. This then should produce another list of two items
+    if you run .get_payloads() on that (one for plain text and another for HTML). The second item in
+    the first list should be the file attachment.
+    """
+    payloads = email_file.get_payload()
+    if len(payloads) < 2:
+        raise UserWarning('User email {} does not have sufficient payload.'.format(key))
+    raw_attachment = payloads[1]
+    return raw_attachment
+
+
+def get_desired_talent_pool(simple_hash):
+    """
+    Queries the database for a talent_pool.id based on the simple_hash attr for it.
+    :param str simple_hash:
+    :return int talent_pool.id:
+    """
+    talent_pool = session.query(TalentPool).filter(TalentPool.simple_hash == simple_hash).first()
+    if not talent_pool:
+        raise SQLAlchemyError('Unable to get talent_pool from hash {}'.format(simple_hash))
+    return talent_pool.id
+
 
 
 def utcnow():
@@ -105,29 +147,28 @@ def utcnow():
     return datetime.datetime.now(tz=pytz.utc)
 
 
-def refreshToken(tokenObj):
+def refresh_token(token_obj):
     """
     Refreshes an expired token.
-    :param Token tokenObj:
+    :param Token token_obj:
     :return Token:
     """
     GRANT_TYPE = 'refresh_token'
-    client_id = tokenObj.client_id
-    token_client = session.query(Client).filter(Client.client_id==client_id)
+    client_id = token_obj.client_id
+    token_client = session.query(Client).filter(Client.client_id == client_id)
     if not token_client:
-        raise SQLAlchemyError('Unable to get a client for User\'s token.id: {}'.format(tokenObj.id))
+        raise SQLAlchemyError('Unable to get a client for User\'s token.id: {}'.format(token_obj.id))
     CLIENT_SECRET = token_client.client_secret
-    refresh_token = 'refresh_bar'
 
     payload = {
-      'grant_type': GRANT_TYPE,
-      'client_id': client_id,
-      'client_secret': token_client.client_secret,
-      'refresh_token': tokenObj.refresh_token
+        'grant_type': GRANT_TYPE,
+        'client_id': client_id,
+        'client_secret': CLIENT_SECRET,
+        'refresh_token': token_obj.refresh_token
     }
     try:
         refresh_response = requests.post(AUTH_URL, data=payload)
     except (requests.exceptions.ConnectionError, requests.exceptions.Timeout) as e:
-        raise e('Error during token refresh!')
+        raise Exception('Error during token refresh! {}'.format(e.message))
     token_json = json.loads(refresh_response.content)
     return token_json.access_token
