d64f4677c0cceffd0ce46bfca11946547f57ddca
Joseph Arceneaux <joseph.arceneaux@orange-ftgroup.com>
2016-04-22 13:47:44 -0500

    Tag non-prod builds with branch and timestamp

--- push_containers.sh
+++ push_containers.sh
@@ -4,7 +4,10 @@
 
 eval $(aws ecr get-login --region us-east-1)
 
-FLASK_APPS=("auth-service" "activity-service" "resume-parsing-service" "user-service" "candidate-service" "social-network-service" "candidate-pool-service" "spreadsheet-import-service" "scheduler-service" "sms-campaign-service" "push-campaign-service" "email-campaign-service")
+# FLASK_APPS="auth-service activity-service resume-parsing-service user-service candidate-service social-network-service candidate-pool-service spreadsheet-import-service scheduler-service sms-campaign-service push-campaign-service email-campaign-service"
+
+# Skipping resume-parsing-service as Erik's testing.
+FLASK_APPS="auth-service activity-service user-service candidate-service social-network-service candidate-pool-service spreadsheet-import-service scheduler-service sms-campaign-service push-campaign-service email-campaign-service"
 
 ecr_registry_url="528222547498.dkr.ecr.us-east-1.amazonaws.com"
 
@@ -15,28 +18,34 @@ fi
 
 # Consider error if branch is master and production was specified
 
+# Consider handling docker errors - just exit?
+
 timestamp=`date +"%Y-%m-%d-%H-%M-%S"`
 timestamp_tag="built-at-$timestamp"
-echo "Using tag ${timestamp_tag}"
-git --version
 
-for app_index in ${!FLASK_APPS[@]}
+# for app_index in ${FLASK_APPS}
+for app in ${FLASK_APPS}
 
 do
-    if [ $production ] ; then
-	tag_command="docker tag -f gettalent/${FLASK_APPS[$app_index]}:latest ${ecr_registry_url}/gettalent/${FLASK_APPS[$app_index]}:latest"
-	echo $tag_command
-        eval $tag_command
-	push_command="docker push ${ecr_registry_url}/gettalent/${FLASK_APPS[$app_index]}:latest"
-	echo $push_command
-        eval $push_command
-    else
-	tag_command="docker tag -f gettalent/${FLASK_APPS[$app_index]}:${timestamp_tag} ${ecr_registry_url}/gettalent-stage/${FLASK_APPS[$app_index]}:${timestamp_tag}"
-	echo $tag_command
-        eval $tag_command
+    tag_command="docker tag -f gettalent/${app} ${ecr_registry_url}/gettalent/${app}:${timestamp_tag}"
+    echo $tag_command
+    eval $tag_command
 
-	push_command="docker push ${ecr_registry_url}/gettalent-stage/${FLASK_APPS[$app_index]}:${timestamp_tag}"
-	echo $push_command
-        eval $push_command
+    push_command="docker push ${ecr_registry_url}/gettalent/${app}:${timestamp_tag}"
+    echo $push_command
+    eval $push_command
+
+    if [ $production ] ; then
+	# Update task definition for this service
+	# python ecs_task_update.py ${app} ${timestamp_tag} prod
+    else
+	# Update task definition for this service and restart
+	python ecs_task_update.py ${app} ${timestamp_tag} stage
     fi
 done
+
+# If we've pushed and tagged all the images, tag the branch
+echo "Tagging branch with ${timestamp_tag}"
+# Need to turn off triggering from tag push
+# git tag -a ${timestamp_tag} -m "Adding timestamp tag"
+# git push --tags
