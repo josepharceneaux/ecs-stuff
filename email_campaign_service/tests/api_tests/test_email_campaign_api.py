d39c239a9bbb155be5485d35ad13c37d1727949a
haniqadri <haniqadri.qc@gmail.com>
2016-04-23 15:52:17 +0500

    GET-1091

--- email_campaign_service/tests/api_tests/test_email_campaign_api.py
+++ email_campaign_service/tests/api_tests/test_email_campaign_api.py
@@ -548,7 +548,7 @@ class TestSendCampaign(object):
                                          email_campaign_id=campaign.id)
         response = requests.post(
             self.URL % campaign.id, headers=dict(Authorization='Bearer %s' % access_token_first))
-        assert_campaign_send(response, campaign, user_first, 40, abort_time_for_sends=150)
+        assert_campaign_send(response, campaign, user_first, 40, abort_time_for_sends=300)
 
 
 # Test for healthcheck
