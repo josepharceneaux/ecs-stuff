65f3de271256bc6fbf35ccc64944871f311bd42f
Joseph Arceneaux <joseph.arceneaux@orange-ftgroup.com>
2016-04-26 17:15:10 -0500

    Get arguments.

--- ecs_task_update.py
+++ ecs_task_update.py
@@ -1,5 +1,19 @@
 import boto3
+import argparse
+
+SERVICE_NAME = 'service-name'
+TAG_NAME = 'tag-name'
+
+parser = argparse.ArgumentParser(description="Update and restart ECS tasks.")
+parser.add_argument(SERVICE_NAME, nargs=1)
+parser.add_argument(TAG_NAME, nargs=1)
+args = parser.parse_args()
+
+service = vars(args)[SERVICE_NAME][0]
+tag = vars(args)[TAG_NAME][0]
+
+print "Service: %s" % service
+print "Tag: %s" % tag
 
 client = boto3.client('ecs')
-
 print "Client: %s" % client
