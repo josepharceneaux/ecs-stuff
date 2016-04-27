49760a42a0a264aeab43a36abe58a9ac13041295
Joseph Arceneaux <joseph.arceneaux@orange-ftgroup.com>
2016-04-27 16:13:58 -0500

    Register the new task definition.

--- ecs_task_update.py
+++ ecs_task_update.py
@@ -2,6 +2,7 @@ import boto3
 import argparse
 import json
 
+
 # Command line arguments
 SERVICE_NAME = 'service-name'
 TAG_NAME = 'tag-name'
@@ -22,11 +23,28 @@ try:
     task_definition = client.describe_task_definition(taskDefinition=service)
 except Exception as e:
     print "Exception {} searching for task definition {}".format(e.message, service)
+    exit(1)
 
 # We are running single container tasks
+if len(task_definition['taskDefinition']['containerDefinitions'][0]):
+    print "EXCEPTION: More than 1 container definition for %s" % service
+    exit(1)
+
 image = task_definition['taskDefinition']['containerDefinitions'][0]['image']
 
 # Get the image without the tag and create new image with our new tag
 new_image = image.split(':')[0] + ':' + tag
 
 print "Creating new task definition with image: %s" % new_image
+task_definition['taskDefinition']['containerDefinitions'][0]['image'] = new_image
+
+try:
+    response = client.register_task_definition(family=service, containerDefinitions=task_definition['taskDefinition']['containerDefinitions'])
+except Exception as e:
+    print "Exception {} registering task definition for {}".format(e.message, service)
+    exit(1)
+
+# Consider garbage collecting Task Definitions?
+
+print "Updated: %s" % task_definition['taskDefinition']['containerDefinitions'][0]['image']
+exit(0)
--- push_containers.sh
+++ push_containers.sh
@@ -6,6 +6,8 @@ eval $(aws ecr get-login --region us-east-1)
 
 FLASK_APPS=("auth-service" "activity-service" "resume-parsing-service" "user-service" "candidate-service" "social-network-service" "candidate-pool-service" "spreadsheet-import-service" "scheduler-service" "sms-campaign-service" "push-campaign-service" "email-campaign-service")
 
+FLASK_STAGE_APPS=("auth-service-stage" "activity-service-stage" "resume-parsing-service-stage" "user-service-stage" "candidate-service-stage" "social-network-service-stage" "candidate-pool-service-stage" "spreadsheet-import-service-stage" "scheduler-service-stage" "sms-campaign-service-stage" "email-campaign-service-stage")
+
 ecr_registry_url="528222547498.dkr.ecr.us-east-1.amazonaws.com"
 
 # Only push to production if explicitly specified
@@ -15,13 +17,13 @@ fi
 
 # Consider error if branch is master and production was specified
 
+# Consider handling docker errors - just exit?
+
 timestamp=`date +"%Y-%m-%d-%H-%M-%S"`
 timestamp_tag="built-at-$timestamp"
-echo "Tagging branch with ${timestamp_tag}"
-git tag -a ${timestamp_tag} -m "Adding timestamp tag"
-git push --tags
 
-for app_index in ${!FLASK_APPS[@]}
+# for app_index in ${!FLASK_APPS[@]}
+for app_index in ${!FLASK_STAGE_APPS[@]}
 
 do
     if [ $production ] ; then
@@ -44,3 +46,8 @@ do
 	python ecs_task_update.py ${FLASK_APPS[$app_index]} ${timestamp_tag}
     fi
 done
+
+# If we've pushed and tagged all the images, tag the branch
+echo "Tagging branch with ${timestamp_tag}"
+git tag -a ${timestamp_tag} -m "Adding timestamp tag"
+git push --tags
