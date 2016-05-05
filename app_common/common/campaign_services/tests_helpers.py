d124411243d14011d3ff262d2e6b18fe6a46cbd9
Hafiz Muhammad Basit <basit.gettalent@gmail.com>
2016-05-06 01:13:14 +0500

    GET-1141: Increasing time for candidates smartlist creation

--- app_common/common/campaign_services/tests_helpers.py
+++ app_common/common/campaign_services/tests_helpers.py
@@ -541,7 +541,7 @@ class CampaignsTestsHelpers(object):
     def create_smartlist_with_candidate(access_token, talent_pipeline, count=1,
                                         data=None, emails_list=False, create_phone=False,
                                         assign_role=False, assert_candidates=True,
-                                        smartlist_name=fake.word(), timeout=80):
+                                        smartlist_name=fake.word(), timeout=100):
         """
         This creates candidate(s) as specified by the count and assign it to a smartlist.
         Finally it returns smartlist_id and candidate_ids.
