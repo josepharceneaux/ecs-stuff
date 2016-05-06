b79ba3d2f21c4639b6466e8c0f6b94c5581f8194
Joseph Arceneaux <joseph.arceneaux@orange-ftgroup.com>
2016-05-06 10:51:49 -0500

    Add updating of the AWS service.

--- scripts/move-stage-to-prod.py
+++ scripts/move-stage-to-prod.py
@@ -1,5 +1,5 @@
 '''
-Look for a service running on production and create a new task definition based upon that service
+Look for a service running on production and create a new task definition based upon that service's image
 in staging, but with the tag we are passed.
 
 Syntax: python move-stage-to-prod.py <service-name> <tag-name>
@@ -61,6 +61,51 @@ def copy_stage_values_to_prod(source_td, destination_td, tag):
         destination_definition['image'] = new_image
         index += 1
 
+def update_prod_task_definition(client, family_name, definitions):
+    '''
+    Create a new revision of a task definition.
+    :param client: The boto ECS client.
+    :param family_name: The task name.
+    :param definitions: Container definitions (may only be one)
+    '''
+
+    try:
+        response = client.register_task_definition(family=family_name, containerDefinitions=definitions)
+        validate_http_status('update_prod_task_definition', response)
+    except Exception as e:
+        print "Exception {} registering task definition for {}".format(e.message, service_svc)
+        exit(1)
+
+    td = response['taskDefinition']
+    print "Task definition for {} updated to revision {}.".format(td['family'], td['revision'])
+    return td['taskDefinitionArn']
+
+def update_service(client, prod_service_name, new_td_arn):
+    '''
+    Update production service to use a new task definition revision.
+    :param service: The getTalent service name.
+    :param tag: The tag to be used (as in a version tag).
+    '''
+
+    try:
+        response = client.describe_services(cluster=PROD_CLUSTER_NAME, services=[ prod_service_name ])
+        validate_http_status('describe_services', response)
+    except Exception as e:
+        print "Exception {} obtaining service description for {}".format(e.message, prod_service_name)
+        exit(1)
+
+    desired_count = response['services'][0]['desiredCount']
+    deployment_configuration = response['services'][0]['deploymentConfiguration']
+    try:
+        response = client.update_service(cluster=PROD_CLUSTER_NAME, service=prod_service_name, desiredCount=desired_count,
+                                         taskDefinition=new_td_arn, deploymentConfiguration=deployment_configuration)
+        validate_http_status('update_service', response)
+    except Exception as e:
+        print "Exception {} updating service for {}".format(e.message, prod_service_name)
+        exit(1)
+
+    print "Successfully updated AWS service for {}".format(prod_service_name)
+
 def migrate_stage_image_to_prod(service, tag):
     '''
     Update production to use the docker image currently in staging.
@@ -69,15 +114,13 @@ def migrate_stage_image_to_prod(service, tag):
     '''
 
     print "Migrating {} to production".format(service)
+
     # Service name
     stage_service_name = service + '-stage'
     prod_service_name = service + '-svc'
     # Task description name
     stage_td_name = service + '-stage-td'
     prod_td_name = service + '-td'
-    # GT_ENVIRONMENT values
-    stage_env = 'qa'
-    prod_env = 'prod'
 
     client = boto3.client('ecs')
     stage_td = get_task_definition(client, stage_td_name)
@@ -87,10 +130,9 @@ def migrate_stage_image_to_prod(service, tag):
     print "Prod: {}:{} {}".format(prod_td['taskDefinition']['family'], prod_td['taskDefinition']['revision'], prod_td['taskDefinition']['status'])
 
     copy_stage_values_to_prod(stage_td, prod_td, tag)
-    print
-    print "New Prod {}".format(prod_td['taskDefinition']['containerDefinitions'][0]['image'])
-    # create new revision of prod task definition
+    new_td_arn = update_prod_task_definition(client, prod_td_name, prod_td['taskDefinition']['containerDefinitions'])
     # update the service and restart it
+    update_service(client, prod_service_name, new_td_arn)
 
 # Command line arguments
 SERVICE_NAME = 'service-name' # Service means getTalent micro service, not ECS service.
@@ -101,5 +143,8 @@ parser.add_argument(TAG_NAME, nargs=1)
 args = parser.parse_args()
 service = vars(args)[SERVICE_NAME][0]
 tag = vars(args)[TAG_NAME][0]
+
+PROD_CLUSTER_NAME = 'prod'
+STAGE_CLUSTER_NAME = 'stage'
 
 migrate_stage_image_to_prod(service, tag)
