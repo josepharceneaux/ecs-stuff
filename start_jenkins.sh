c021c4c2e29e86c3115f88087163350bf348de00
Joseph Arceneaux <joseph.arceneaux@orange-ftgroup.com>
2016-05-11 13:18:48 -0500

    Remove migrations tests.

--- start_jenkins.sh
+++ start_jenkins.sh
@@ -61,4 +61,4 @@ done
 
 sleep 10
 
-py.test -n 48 scheduler_service/tests auth_service/tests user_service/tests activity_service/tests resume_parsing_service/tests candidate_pool_service/tests candidate_service/tests spreadsheet_import_service/tests email_campaign_service/tests app_common/common/test_migrations.py
+py.test -n 48 scheduler_service/tests auth_service/tests user_service/tests activity_service/tests resume_parsing_service/tests candidate_pool_service/tests candidate_service/tests spreadsheet_import_service/tests email_campaign_service/tests
