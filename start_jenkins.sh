2dd56844a5dcaa396ac8669116154b72f9e593bf
Joseph Arceneaux <joseph.arceneaux@orange-ftgroup.com>
2016-05-12 15:30:01 -0500

    Remove more tests.

--- start_jenkins.sh
+++ start_jenkins.sh
@@ -57,4 +57,4 @@ done
 sleep 10
 
 # py.test -n 48 scheduler_service/tests auth_service/tests user_service/tests activity_service/tests resume_parsing_service/tests candidate_pool_service/tests candidate_service/tests spreadsheet_import_service/tests email_campaign_service/tests sms_campaign_service/tests
-py.test -n 48 scheduler_service/tests auth_service/tests user_service/tests activity_service/tests spreadsheet_import_service/tests email_campaign_service/tests
+py.test -n 48 scheduler_service/tests auth_service/tests user_service/tests activity_service/tests spreadsheet_import_service/tests
