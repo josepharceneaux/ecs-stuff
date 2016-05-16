2f21c93197b1f3343ea8b2091acb9ff7b353c822
Hafiz Muhammad Basit <basit.gettalent@gmail.com>
2016-05-16 17:09:35 +0500

    GET-1287: Fixed and related test added

--- sms_campaign_service/tests/conftest.py
+++ sms_campaign_service/tests/conftest.py
@@ -13,6 +13,8 @@ from sqlalchemy.orm.exc import ObjectDeletedError
 
 # Application Specific
 # common conftest
+from sms_campaign_service.common.inter_service_calls.candidate_service_calls import \
+    create_candidates_from_candidate_api
 from sms_campaign_service.common.tests.conftest import \
     (db, pytest, fake, requests, gen_salt, user_auth, access_token_first,
      sample_client, test_domain, first_group, domain_first, user_first, candidate_first,
@@ -253,6 +255,32 @@ def sms_campaign_with_one_valid_candidate(request, campaign_valid_data,
                                                                             talent_pipeline,
                                                                             data=candidates_data)
     campaign_valid_data['smartlist_ids'] = [smartlist_id]
+    test_sms_campaign = create_sms_campaign_via_api(campaign_valid_data, headers,
+                                                    talent_pipeline.user.id)
+
+    def fin():
+        _delete_campaign(test_sms_campaign, headers)
+
+    request.addfinalizer(fin)
+    return test_sms_campaign
+
+
+@pytest.fixture()
+def sms_campaign_with_same_candidate_in_multiple_smartlists(request, campaign_valid_data,
+                                                            smartlist_with_two_candidates,
+                                                            access_token_first, talent_pipeline,
+                                                            headers):
+    """
+    This fixture creates an SMS campaign with two smartlists.
+    Smartlist 1 will have two candidates and smartlist 2 will have one candidates(which will be
+    same as one of the two candidates of smartlist 1).
+    """
+    smartlist_1_id, candidate_ids = smartlist_with_two_candidates
+    candidate_ids_for_smartlist_2 = [candidate_ids[0]]
+    smartlist_2_id, _ = CampaignsTestsHelpers.create_smartlist_with_candidate(access_token_first,
+                                                                              talent_pipeline,
+                                                                              candidate_ids=candidate_ids_for_smartlist_2)
+    campaign_valid_data['smartlist_ids'] = [smartlist_1_id, smartlist_2_id]
     test_sms_campaign = create_sms_campaign_via_api(campaign_valid_data, headers,
                                                     talent_pipeline.user.id)
 
--- sms_campaign_service/tests/restful_api_tests/test_celery_tasks.py
+++ sms_campaign_service/tests/restful_api_tests/test_celery_tasks.py
@@ -142,6 +142,24 @@ class TestCeleryTasks(object):
         assert_on_blasts_sends_url_conversion_and_activity(user_first.id, 2, campaign_id,
                                                            access_token_first)
 
+    def test_campaign_send_with_same_candidate_in_multiple_smartlists(
+            self, access_token_first, user_first,
+            sms_campaign_with_same_candidate_in_multiple_smartlists):
+        """
+        - This tests the endpoint /v1/sms-campaigns/:id/send
+
+        User auth token is valid, campaign has two smartlists associated, Smartlists have one
+        common candidate. One smartlist has 2 candidate and other smartlist has 1 candidates.
+        Total number of sends should be 2.
+        """
+        campaign_id = sms_campaign_with_same_candidate_in_multiple_smartlists['id']
+        response_post = self.send_campaign(sms_campaign_with_same_candidate_in_multiple_smartlists,
+                                           access_token_first)
+        assert_api_send_response(sms_campaign_with_same_candidate_in_multiple_smartlists,
+                                 response_post, requests.codes.OK)
+        assert_on_blasts_sends_url_conversion_and_activity(user_first.id, 2, campaign_id,
+                                                           access_token_first)
+
 # TODO: Assigned a JIRA GET-1277 to saad for these
 # class TestCampaignSchedule(object):
 #     """
