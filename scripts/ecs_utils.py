2441b7ddd95f76529349c506d660339e4d8abae2
Joseph Arceneaux <joseph.arceneaux@orange-ftgroup.com>
2016-05-17 15:51:59 -0500

    Utilities for ECS operations.e

---
+++ scripts/ecs_utils.py
@@ -0,0 +1,74 @@
+import boto3
+
+# ECS cluster name
+STAGE_CLUSTER_NAME = 'stage'
+PROD_CLUSTER_NAME = 'prod'
+
+# ECS service suffix we use
+PROD_SVC_SUFFIX = '-svc'
+STAGE_SVC_SUFFIX = '-stage'
+
+# ECS Task definition suffix we use
+STAGE_TD_SUFFIX = '-stage-td'
+PROD_TD_SUFFIX = '-td'
+
+# Base of our namespace for several structures
+ECS_BASE_PATH = 'gettalent'
+
+def validate_http_status(request_name, response):
+    '''
+    Validate that we got a good status on our request.
+
+    :param request_name: Caller name to put in error message.
+    :param response: The response to be validated.
+    :return: None.
+    '''
+  
+    try:
+        http_status = response['ResponseMetadata']['HTTPStatusCode']
+    except Exception as e:
+        print "Exception getting HTTP status {}: {}".format(request_name, e.message)
+        exit(1)
+
+    if http_status != 200:
+        print "Error with {}. HTTP Status: {}".format(request_name, http_status)
+        exit(1)
+
+def tag_exists_in_repo(repo_path, tag):
+    '''
+    Search for an image with a specific tag in a docker repository.
+
+    :param repo_name: The path of the repository to search.
+    :param image: The tag to search for.
+    '''
+
+    ecr_client = boto3.client('ecr')
+
+    response = ecr_client.list_images(repositoryName=repo_path)
+    validate_http_status('list_images', response)
+    count = 0
+    while True:
+        image_ids = response['imageIds']
+        for image in image_ids:
+            if 'imageTag' in image and image['imageTag'] == tag:
+                return True
+
+        if 'nextToken' not in response:
+            break
+
+        response = ecr_client.list_images(repositoryName=repo_path, nextToken=response['nextToken'])
+        validate_http_status('list_images', response)
+
+    return False
+
+def image_exists_in_repo(service_name, image):
+    '''
+    Search for an image with a specific tag in a docker repository.
+
+    :param service_name: The name of the repository to search.
+    :param image: The image and tag to search for.
+    '''
+
+    repo_path = ECS_BASE_PATH + "/" + service_name
+    tag = image.split(':')[1]
+    return tag_exists_in_repo(repo_path, tag)
