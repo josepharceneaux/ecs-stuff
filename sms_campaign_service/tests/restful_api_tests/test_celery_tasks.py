6e902b53d9dd2e81fa3e71849d9ac74ac705fdac
Waqas Younas <waqas.younas@gmail.com>
2016-05-20 17:01:23 +0500

    GET-1287
    - Added some minor nits for Basit

--- sms_campaign_service/tests/restful_api_tests/test_celery_tasks.py
+++ sms_campaign_service/tests/restful_api_tests/test_celery_tasks.py
@@ -163,7 +163,7 @@ class TestCeleryTasks(object):
         - This tests the endpoint /v1/sms-campaigns/:id/send
 
         User auth token is valid, campaign has two smartlists associated, Smartlists have one
-        common candidate. One smartlist has 2 candidate and other smartlist has 1 candidates.
+        common candidate. One smartlist has 2 candidate and other smartlist has 1 candidate.
         Total number of sends should be 2.
         """
         campaign_id = sms_campaign_with_same_candidate_in_multiple_smartlists['id']
@@ -171,6 +171,8 @@ class TestCeleryTasks(object):
                                            access_token_first)
         assert_api_send_response(sms_campaign_with_same_candidate_in_multiple_smartlists,
                                  response_post, requests.codes.OK)
+        # TODO-if we can set two in a variable, then for a reader like me it may be tad more obvious that it is infact
+        # an expected result
         assert_on_blasts_sends_url_conversion_and_activity(user_first.id, 2, campaign_id,
                                                            access_token_first)
 
