e72caaf5b509337bb646b436cd974a83956827af
Joseph Arceneaux <joseph.arceneaux@orange-ftgroup.com>
2016-05-20 17:26:03 -0500

    Start on garbage collection of ECR images.

--- scripts/ecs_utils.py
+++ scripts/ecs_utils.py
@@ -64,15 +64,63 @@ def tag_exists_in_repo(repo_path, tag):
     return False
 
 
-def image_exists_in_repo(service_name, image):
+def image_exists_in_repo(name, image):
     """
     Search for an image with a specific tag in a docker repository.
 
-    :param str service_name: The name of the repository to search.
+    :param str name: The name of the repository to search.
     :param str image: The image and tag to search for.
     :rtype: bool
     """
 
-    repo_path = ECS_BASE_PATH + "/" + service_name
+    repo_path = ECS_BASE_PATH + "/" + name
     tag = image.split(':')[1]
     return tag_exists_in_repo(repo_path, tag)
+
+
+def gather_images_from_repo(name, tags):
+    """
+    Collect images from repository.
+
+    :param str name: Repository name.
+    :param str tags: Can be 'none', 'only', 'all' - return untagged, tagged, or all images.
+    """
+
+    if tags not in [ 'none', 'only', 'all' ]:
+        raise Exception("gather_images_in_repo: parameter TAGS must be 'none', 'only', or 'all'. Called with {}".format(tags))
+
+    ecr_client = boto3.client('ecr')
+    service_uri = ECS_BASE_PATH + '/' + name
+
+    response = ecr_client.list_images(repositoryName=service_uri)
+    validate_http_status('list_images', response)
+
+    return_list = []
+    while True:
+        image_list = response['imageIds']
+
+        for image in image_list:
+            if 'imageTag' in image:
+                if tags == 'only' or tags == 'all':
+                    return_list.append(image)
+            else:
+                if tags == 'none' or tags == 'all':
+                    return_list.append(image)
+
+        if 'nextToken' not in response:
+            break
+
+        response = ecr_client.list_images(repositoryName=service_uri, nextToken=response['nextToken'])
+        validate_http_status('list_images', response)
+
+    return return_list
+
+
+def sort_image_list_by_tag(image_list):
+    """
+    Sort a list of ECR images by their tag.
+
+    :param json image_list: A list of ECR imgaes.
+    """
+
+    return sorted(image_list, key=lambda x:x['imageTag'])
