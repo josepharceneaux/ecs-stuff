3cecbef9965e070e62b8d8ce8c510090400843d4
Joseph Arceneaux <joseph.arceneaux@orange-ftgroup.com>
2016-05-24 17:19:12 -0500

    Additional GC utilities. Debugging.

--- scripts/ecs_utils.py
+++ scripts/ecs_utils.py
@@ -86,7 +86,7 @@ def image_exists_in_repo(name, image):
     return tag_exists_in_repo(repo_path, tag)
 
 
-def gather_images_from_repo(name, tags):
+def gather_images_from_repository(name, tags):
     """
     Collect images from repository.
 
@@ -134,22 +134,92 @@ def sort_image_list_by_tag(image_list):
     return sorted(image_list, key=lambda x:x['imageTag'])
 
 
-def delete_images_from_repository(ecr_client, image, service_name):
+def repository_components_from_uri(uri):
     """
+    Break up and return the constituent components of a container URI.
+
+    :param str uri: URI of a container.
     """
 
-    components = image.split(':')
-    image_ids = [ { 'imageDigest' : components[0], 'imageTag': components[1] } ]
-    repository_uri = ECS_BASE_PATH + '/' + service_name
+    components = uri.split(':')
+    if len(components) != 2:
+        raise Exception("image_digest_from_uri called with invalid uri {}: no tag".format(uri))
 
-    response = ecr_client.batch_delete_image(repositoryName=repository_uri, imageIds=image_ids)
-    validate_http_status('batch_delete_image', response)
+    base = components[0]
+    tag = components[1]
+    components = base.split('/')
+    if len(components) != 3:
+        raise Exception("image_digest_from_uri called with invalid uri {}: bad base uri".format(uri))
 
-    failures = len(response['failures'])
-    if failures > 0:
-        print "{} failures"
+    repo_name = components[1] + '/' + components[2]
+    return repo_name, components[2], tag
 
-    print response
+
+def image_digest_from_tag(digest_list, tag):
+    """
+    Look for a container image with a certain tag.
+
+    :param list digest_list: A list of container digests and tags.
+    :param str tag: The tag to search for.
+    """
+
+    entry = [ e for e in digest_list if e['imageTag'] == tag ]
+    if entry:
+        return entry[0]['imageDigest']
+
+    return None
+
+
+MAX_CONTAINER_DELETIONS = 100
+
+def delete_images_from_repository_by_digest(ecr_client, repository_name, digest_list):
+    """
+    Delete a list of containers. ECR will only delete a certain number at a time, so we iterate.
+
+    :param obj ecr_client: The boto ECR (EC2 Container Registry) object.
+    :param str repository_name: Name of the repository we're deleting from.
+    :param list digest_list: List of tagged containers found for the particular repository.
+
+    """
+
+    while len(digest_list) > 0:
+        n = min(len(digest_list), MAX_CONTAINER_DELETIONS)
+        to_delete = digest_list[:n]
+
+        response = ecr_client.batch_delete_image(repositoryName=repository_name, imageIds=to_delete)
+        validate_http_status('batch_delete_image', response)
+
+        if len(response['failures']) > 0:
+            print "WARNING: failures deleting images in {}".format(repository_name)
+            for fail in response['failures']:
+                print "    {}".format(fail)
+
+        del digest_list[:n]
+        print "Deleted {}".format(n)
+
+
+def delete_images_from_repository_by_uri(ecr_client, image_list, digest_list):
+    """
+    Given a list of container URIs. Find the digest SHA for the container and delete it. It's done this way because AWS uses
+    a URI to specify a container on a task definition, but deleting the container requires the digest.
+
+    :param obj ecr_client: The boto ECR (EC2 Container Registry) object.
+    :param list image_list: List of ECR container URIs to be removed.
+    :param list digest_list: List of tagged containers found for the particular service.
+    """
+
+    to_delete = []
+    for image in image_list:
+        repository_name, service_name, tag = repository_components_from_uri(image)
+        digest = image_digest_from_tag(digest_list, tag)
+        if digest:
+            to_delete.append({ 'imageDigest' : digest, 'imageTag' : tag })
+            print "Deleting {}".format(image)
+        else:
+            print "WARNING: Can't find digest for {}. Not deleted.".format(image)
+
+    if len(digest_list) > 0:
+        delete_images_from_repository_by_digest(ecr_client, repository_name, to_delete)
 
 
 # Task Definition functions
@@ -159,6 +229,7 @@ def gather_task_definitions(ecs_client, service, cluster):
     """
     Collect all task definitions for getTalent service in a cluster.
 
+    :param obj ecs_client: The boto ECS object.
     :param str service: Name of the getTalent service.
     :param str cluster: name of the cluster to inspect.
     """
@@ -189,7 +260,10 @@ def gather_task_definitions(ecs_client, service, cluster):
 
 def task_definition_image(ecs_client, td_arn):
     """
-    Return the image used by a task definition.
+    Return the image URI used by a task definition.
+
+    :param obj ecs_client: The boto ECS object.
+    :param str td_arn: The AWS Resource Name.
     """
 
     response = ecs_client.describe_task_definition(taskDefinition=td_arn)
@@ -202,43 +276,32 @@ def task_definition_image(ecs_client, td_arn):
 
 def deregister_task(ecs_client, td_arn):
     """
+    Make a task definition revision inactive.
+
+    :param obj ecs_client: The boto ECS object.
+    :param str td_arn: The AWS Resource Name.
     """
 
+    print "Deactivating Task Definition {}".format(td_arn)
     response = ecs_client.deregister_task_definition(taskDefinition=td_arn)
     validate_http_status('derigester_task_definition', response)
-    print response['taskDefinition']['status']
+    if response['taskDefinition']['status'] != 'INACTIVE':
+        print "WARNING: {} not marked INACTIVE".format(td_arn)
 
 
-# Service functions
-
-
-def gather_all_services_for_cluster(ecs_client, cluster):
+def remove_untagged_containers(ecr_client, service):
     """
+    Remove any ECR containers which don't have a tag.
+    
+    :param str service: Name of the getTalent service.
+    :param str cluster: name of the cluster to inspect.
     """
 
-    # Adjust to our ECS naming convention
-    if cluster not in TASKS_SUFFIX_DICT:
-        raise Exception("gather_task_definitions called with invalid cluster name {}".format(cluster))
+    repository_name = ECS_BASE_PATH + "/" + service
 
-    response = ecs_client.list_services(cluster=cluster)
-    validate_http_status('gather_all_services_for_cluster', response)
-
-    service_list = []
-    while True:
-        arn_list = response['serviceArns']
-        for arn in arn_list:
-            service_list.append(arn)
-
-        if 'nextToken' not in response:
-            break
-
-        response = ecs_client.list_services(cluster=cluster, nextToken=response['nextToken'])
-        validate_http_status('gather_all_services_for_cluster', response)
-
-    return service_list
-
-
-# Garbage collect task definitions and their images
+    # Collect all untagged containers for this service
+    digest_list = gather_images_from_repository(service, 'none')
+    delete_images_from_repository_by_digest(ecr_client, repository_name, digest_list)
 
 
 def garbage_collect_ecs(service, cluster):
@@ -251,10 +314,11 @@ def garbage_collect_ecs(service, cluster):
 
     # Adjust to our ECS naming convention
     if cluster not in SERVICES_SUFFIX_DICT:
-        raise Exception("gather_task_definitions called with invalid cluster name {}".format(cluster))
+        raise Exception("garbage_collect_ecs called with invalid cluster name {}".format(cluster))
     service_name = service + SERVICES_SUFFIX_DICT[cluster]
 
     ecs_client = boto3.client('ecs')
+    ecr_client = boto3.client('ecr')
 
     # Find the currently running task definition
     response = ecs_client.describe_services(cluster=cluster, services=[ service_name ])
@@ -264,30 +328,20 @@ def garbage_collect_ecs(service, cluster):
     current_td = response['services'][0]['taskDefinition']
     current_image = task_definition_image(ecs_client, current_td)
 
-    print "Currently Running TD: {}".format(current_td)
-
     # Get a list of all ACTIVE task definitions
     td_arn_list = gather_task_definitions(ecs_client, service, cluster)
 
     # Remove the task definition attached to the currently active service
-    print "Found {} task definitions.".format(len(td_arn_list))
     if current_td in td_arn_list:
-        print "Found current TD {} for service {}".format(current_td, service_name)
         td_arn_list.remove(current_td)
-        print "List now has {} tds.".format(len(td_arn_list))
     else:
         print "WARNING: Currently running task definition {} for {} not found in Task Definition list.".format(current_td, service_name)
 
     # Cull the newest revisions that we want to keep out of the list
-    for arn in td_arn_list:
-        print arn
-    print
     count = 0
     while count < GC_THRESHOLD:
         del td_arn_list[0] 
         count += 1
-    for arn in td_arn_list:
-        print arn
 
     # Gather all the images from our updated task def list
     image_delete_list = []
@@ -301,14 +355,18 @@ def garbage_collect_ecs(service, cluster):
 
     # Remove the task definitions on our list
     for arn in td_arn_list:
-        print "DELETE {}".format(arn)
+        deregister_task(ecs_client, arn)
 
     print
     # Remove the images from our list, unless the current service uses it
-    for image in image_delete_list:
-        if image == current_image:
-            print "WARNING: image {} in use by task {}. Not removed.".format(image, td_arn)
-        else:
-            print "DELETE {}".format(image)
+    if current_image in image_delete_list:
+        image_delete_list.remove(image)
+
+    # Get a list of the tagged images for this repository
+    digest_list = gather_images_from_repository(service, 'only')
+    delete_images_from_repository_by_uri(ecr_client, image_delete_list, digest_list)
+
+    # Remove any untagged containers
+    remove_untagged_containers(ecr_client, service)
 
     # TODO: go through all images and remove any dangling ones (those not attached to a task definition)
