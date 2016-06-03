54f82c73a1f810b1a7511a492e0b809fb67a5b1d
erik-farmer <erikdfarmer@gmail.com>
2016-06-03 09:48:11 -0700

    WIP base files/dirs

---
+++ resume_parsing_service/resume_lambda/lambda_handlers.py
@@ -0,0 +1,24 @@
+"""
+This is the base set of lambda handlers. The end result feature set should be to have 4 endpoints.
+These endpoints will be the combonations of consuming:
+    1) FilePicker Keys -or- binaries
+    2) Creating a candidate or simply parsing a resume
+
+Each handler will pull code from the resume parser library/modules and act accordingly.
+"""
+__author__ = 'erik@gettalent.com'
+
+def s3_parsing(event, context):
+    pass
+
+
+# def s3_creation(event, context):
+#     pass
+#
+#
+# def file_parsing(event, context):
+#     pass
+#
+#
+# def file_creation(event, context):
+#     pass
