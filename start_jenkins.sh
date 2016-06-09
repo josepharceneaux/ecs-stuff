4a8e5d85a39e13fc4ec8cf278fd82c572fc53fe6
Umar Farooqi <farooqiumar@rocketmail.com>
2016-06-09 13:23:06 -0700

    Added timing information for each batch of tests

--- start_jenkins.sh
+++ start_jenkins.sh
@@ -64,7 +64,7 @@ printf "\n"
 batch_one=`py.test -n auto user_service/tests activity_service/tests spreadsheet_import_service/tests scheduler_service/tests resume_parsing_service/tests candidate_pool_service/tests push_campaign_service/tests`
 printf "\n========================== Batch 1 execution ends. =========================="
 IFS=$'\n' read -d '' -r -a arr  <<< "$batch_one"
-if [[ "$batch_one" =~ [0-9]+\ passed\ in\ [0-9]*.[0-9]+\ seconds ]]; then batch_one_status=1 && printf "\n${arr[${#arr[@]}-1]}"; else batch_one_status=0 && printf "\n${batch_one}"; fi
+if [[ "$batch_one" =~ =\ [0-9]+\ passed\ in\ [0-9]*.[0-9]+\ seconds\ = ]]; then batch_one_status=1 && printf "\n${arr[${#arr[@]}-1]}"; else batch_one_status=0 && printf "\n${batch_one}"; fi
 
 printf "\n========================== Batch 2 execution starts =========================="
 printf "\nAuth Service\nCandidate Service"
@@ -72,7 +72,7 @@ printf "\n"
 batch_two=`py.test -n auto auth_service/tests candidate_service/tests`
 printf "\n========================== Batch 2 execution ends. =========================="
 IFS=$'\n' read -d '' -r -a arr  <<< "$batch_two"
-if [[ "$batch_two" =~ [0-9]+\ passed\ in\ [0-9]*.[0-9]+\ seconds ]]; then batch_two_status=1 && printf "\n${arr[${#arr[@]}-1]}"; else batch_two_status=0 && printf "\n${batch_two}"; fi
+if [[ "$batch_two" =~ =\ [0-9]+\ passed\ in\ [0-9]*.[0-9]+\ seconds\ = ]]; then batch_two_status=1 && printf "\n${arr[${#arr[@]}-1]}"; else batch_two_status=0 && printf "\n${batch_two}"; fi
 
 printf "\n========================== Batch 3 execution starts =========================="
 printf "\nSMS Campaign Service"
@@ -80,7 +80,7 @@ printf "\n"
 batch_three=`py.test -n auto sms_campaign_service/tests`
 printf "\n========================== Batch 3 execution ends. =========================="
 IFS=$'\n' read -d '' -r -a arr  <<< "$batch_three"
-if [[ "$batch_three" =~ [0-9]+\ passed\ in\ [0-9]*.[0-9]+\ seconds ]]; then batch_three_status=1 && printf "\n${arr[${#arr[@]}-1]}"; else batch_three_status=0 && printf "\n${batch_three}"; fi
+if [[ "$batch_three" =~ =\ [0-9]+\ passed\ in\ [0-9]*.[0-9]+\ seconds\ = ]]; then batch_three_status=1 && printf "\n${arr[${#arr[@]}-1]}"; else batch_three_status=0 && printf "\n${batch_three}"; fi
 
 if [[ $(($batch_one_status * $batch_two_status * $batch_three_status)) == 1 ]]
 then
