ffdc29037f3ae5fd2095681be5e90666640a49c0
Joseph Arceneaux <joseph.arceneaux@orange-ftgroup.com>
2016-06-17 11:49:17 -0500

    Remove print statement, add docstring.

--- ats_service/tests/test_app.py
+++ ats_service/tests/test_app.py
@@ -4,11 +4,11 @@ from auth_service.common.routes import ATSServiceApiUrl
 
 def test_healthcheck():
     """
+    Test basic healthcheck request.
     """
     response = requests.get(ATSServiceApiUrl.HEALTH_CHECK)
     assert response.status_code == 200
 
     # Testing Health Check URL with trailing slash
     response = requests.get(ATSServiceApiUrl.HEALTH_CHECK + '/')
-    print response
     assert response.status_code == 200
