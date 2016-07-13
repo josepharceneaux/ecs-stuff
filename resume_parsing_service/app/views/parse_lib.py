ed02f33e4bf8f0ea0a786ee5a4ff22203b400fd8
erik-farmer <erikdfarmer@gmail.com>
2016-07-13 14:43:51 -0700

    Rename function to more appropriate name (unencrypt vs decrypt)

--- resume_parsing_service/app/views/parse_lib.py
+++ resume_parsing_service/app/views/parse_lib.py
@@ -17,7 +17,8 @@ from resume_parsing_service.app.views.optic_parse_lib import fetch_optic_respons
 from resume_parsing_service.app.views.optic_parse_lib import parse_optic_xml
 from resume_parsing_service.app.views.utils import gen_hash_from_file
 from resume_parsing_service.app.views.ocr_lib import google_vision_ocr
-from resume_parsing_service.common.error_handling import InvalidUsage, InternalServerError
+from resume_parsing_service.app.views.pdf_utils import convert_pdf_to_text, decrypt_pdf
+from resume_parsing_service.common.error_handling import InvalidUsage
 from resume_parsing_service.common.utils.talent_s3 import boto3_put
 
 
@@ -38,9 +39,9 @@ def parse_resume(file_obj, filename_str, cache_key):
 
     file_ext = basename(splitext(filename_str.lower())[-1]) if filename_str else ""
 
-    # PDFs must have attempted unencryption with the empty string password if applicable.
+    # PDFs must have attempted decryption with the empty string password if applicable.
     if file_ext == '.pdf':
-        file_obj = unencrypt_pdf(file_obj)
+        file_obj = decrypt_pdf(file_obj)
 
     if is_resume_image(file_ext, file_obj):
         # If file is an image, OCR it
@@ -77,61 +78,6 @@ def parse_resume(file_obj, filename_str, cache_key):
     else:
         raise InvalidUsage('No XML text received from Optic Response for {}'.format(filename_str))
 
-
-def convert_pdf_to_text(pdf_file_obj):
-    """
-    Attempts to extract text from an unencrypted PDF file. This is to see if the PDF has text
-    contents or if it is an embedded picture.
-    :param cStringIO.StringIO pdf_file_obj:
-    :return str:
-    """
-    pdf_file_obj.seek(0)
-    pdf_reader = PyPDF2.PdfFileReader(pdf_file_obj)
-    page_count = pdf_reader.numPages
-    pdf_info = {
-        'pages': page_count,
-        'pages_with_text': 0,
-        'text': ''
-    }
-
-    for i in xrange(page_count):
-        new_text = pdf_reader.getPage(i).extractText()
-
-        if new_text:
-            pdf_info['text'] += new_text
-            pdf_info['pages_with_text'] =+ 1
-
-    return pdf_info['text']
-
-
-def unencrypt_pdf(pdf_file_obj):
-    """
-    Returns an unencrypted pdf_file, if encrypted , or the original file.
-    :param cStringIO.StringIO pdf_file_obj:
-    :return cStringIO.StringIO:
-    """
-    pdf_file_obj.seek(0)
-    pdf_reader = PyPDF2.PdfFileReader(pdf_file_obj)
-
-    if pdf_reader.isEncrypted:
-        decrypted = pdf_reader.decrypt('')
-        if not decrypted:
-            raise InternalServerError(
-                'The PDF appears to be encrypted and could not be read. Please try using an un-encrypted PDF')
-
-        unencrypted_pdf_io = StringIO()
-        pdf_writer = PyPDF2.PdfFileWriter()
-        page_count = pdf_reader.numPages
-
-        for page_no in xrange(page_count):
-            pdf_writer.addPage(pdf_reader.getPage(page_no))
-            pdf_writer.write(unencrypted_pdf_io)
-
-        return unencrypted_pdf_io
-
-    else:
-        pdf_file_obj.seek(0)
-        return pdf_file_obj
 
 def get_or_store_parsed_resume(resume_file, filename_str):
     """
--- resume_parsing_service/app/views/pdf_utils.py
+++ resume_parsing_service/app/views/pdf_utils.py
@@ -1,76 +1,61 @@
-from resume_parsing_service.app.views.parse_lib import unencrypt_pdf
+# Standard Lib
 from cStringIO import StringIO
-#
-from wand.image import Image
-import base64,requests, json
-
-GOOGLE_API_KEY="AIzaSyD4i4j-8C5jLvQJeJnLmoFW6boGkUhxSuw"
-GOOGLE_CLOUD_VISION_URL="https://vision.googleapis.com/v1/images:annotate"
-
-def ocr_with_google(string_io_obj):
-    unencd = unencrypt_pdf(string_io_obj)
-    unencd.seek(0)
-    outfile = StringIO()
-    # im = Image(file=unencd, resolution=200)
-    im = convert_pdf_to_png(unencd)
-    outfile = StringIO(im)
-    gData = google_vision_ocr(outfile)
-    return gData
+# Third Party/Common
+from resume_parsing_service.common.error_handling import InternalServerError
+import PyPDF2
 
 
-def convert_pdf_to_png(blob):
-    pdf = Image(blob=blob)
-    Image.save()
-
-    pages = len(pdf.sequence)
-
-    image = Image(
-        width=pdf.width,
-        height=pdf.height * pages,
-        resolution=200
-    )
-
-    for i in xrange(pages):
-        image.composite(
-            pdf.sequence[i],
-            top=pdf.height * i,
-            left=0
-        )
-
-    return image.make_blob('jpeg')
-
-
-def google_vision_ocr(file_string_io):
+def convert_pdf_to_text(pdf_file_obj):
     """
-    Utilizes Google Vision API to OCR image with Abbyy as a fallback.
-    Root Docs: https://cloud.google.com/vision/docs/
-    Specific JSON responses:
-        https://cloud.google.com/vision/reference/rest/v1/images/annotate#annotateimageresponse
-        https://cloud.google.com/vision/reference/rest/v1/images/annotate#entityannotation
-    :param cStringIO.StringIO file_string_io:
-    :return unicode:
+    Attempts to extract text from an unencrypted PDF file. This is to see if the PDF has text
+    contents or if it is an embedded picture.
+    :param cStringIO.StringIO pdf_file_obj:
+    :return str:
     """
-    file_string_io.seek(0)
-    b64_string = base64.b64encode(file_string_io.getvalue())
-    req_data = {
-        "requests": [
-            {
-                "image": {
-                    "content": b64_string
-                },
-                "features": [
-                    {
-                        "type": "TEXT_DETECTION",
-                        "maxResults": 1
-                    }
-                ]
-            }
-        ]
+    pdf_file_obj.seek(0)
+    pdf_reader = PyPDF2.PdfFileReader(pdf_file_obj)
+    page_count = pdf_reader.numPages
+    pdf_info = {
+        'pages': page_count,
+        'pages_with_text': 0,
+        'text': ''
     }
 
-    google_response = requests.post("{}?key={}".format(GOOGLE_CLOUD_VISION_URL,
-                                                       GOOGLE_API_KEY),
-                                    json.dumps(req_data),
-                                    timeout=20,
-                                    headers={'content-type': 'application/json'})
-    return google_response
+    for i in xrange(page_count):
+        new_text = pdf_reader.getPage(i).extractText()
+
+        if new_text:
+            pdf_info['text'] += new_text
+            pdf_info['pages_with_text'] =+ 1
+
+    return pdf_info['text']
+
+
+def decrypt_pdf(pdf_file_obj):
+    """
+    Returns an unencrypted pdf_file, if encrypted , or the original file.
+    :param cStringIO.StringIO pdf_file_obj:
+    :return cStringIO.StringIO:
+    """
+    pdf_file_obj.seek(0)
+    pdf_reader = PyPDF2.PdfFileReader(pdf_file_obj)
+
+    if pdf_reader.isEncrypted:
+        decrypted = pdf_reader.decrypt('')
+        if not decrypted:
+            raise InternalServerError(
+                'The PDF appears to be encrypted and could not be read. Please try using an un-encrypted PDF')
+
+        unencrypted_pdf_io = StringIO()
+        pdf_writer = PyPDF2.PdfFileWriter()
+        page_count = pdf_reader.numPages
+
+        for page_no in xrange(page_count):
+            pdf_writer.addPage(pdf_reader.getPage(page_no))
+            pdf_writer.write(unencrypted_pdf_io)
+
+        return unencrypted_pdf_io
+
+    else:
+        pdf_file_obj.seek(0)
+        return pdf_file_obj
