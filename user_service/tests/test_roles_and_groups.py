ecc79a6c8c0203b593d9c1f2b79f8d866155f999
Umar Farooqi <farooqiumar@rocketmail.com>
2016-07-15 16:59:20 -0700

    GET-1492 Fixed user_service tests

--- user_service/tests/test_roles_and_groups.py
+++ user_service/tests/test_roles_and_groups.py
@@ -6,6 +6,9 @@ from common_functions import *
 
 def test_user_scoped_roles_get(access_token_first, user_first, user_second):
 
+    user_first.role_id = Role.get_by_name('ADMIN').id
+    db.session.commit()
+
     # Logged-in user getting roles of a non-existing user
     response, status_code = user_scoped_roles(access_token_first, user_id=user_first.id + 1000)
     assert status_code == 404
