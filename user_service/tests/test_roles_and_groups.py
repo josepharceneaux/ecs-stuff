7fc4d9f081e0b436d4582145379f592508aaf6bc
Umar Farooqi <farooqiumar@rocketmail.com>
2016-07-15 17:21:55 -0700

    GET-1492 Fixed user_service tests

--- user_service/tests/test_roles_and_groups.py
+++ user_service/tests/test_roles_and_groups.py
@@ -20,7 +20,7 @@ def test_user_scoped_roles_get(access_token_first, user_first, user_second):
     # Logged-in user getting roles of itself
     response, status_code = user_scoped_roles(access_token_first, user_id=user_first.id)
     assert status_code == 200
-    assert response.get('role_name') == 'USER'
+    assert response.get('role_name') == 'ADMIN'
 
     user_first.role_id = Role.get_by_name('TALENT_ADMIN').id
     db.session.commit()
