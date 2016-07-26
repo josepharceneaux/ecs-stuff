e01431eaf5429ed744c2f4f88cb83d0774a7a1cb
Joseph Arceneaux <joseph.arceneaux@orange-ftgroup.com>
2016-07-26 10:56:33 -0500

    More flaky test exploration

--- start_jenkins.sh
+++ start_jenkins.sh
@@ -67,10 +67,5 @@ sleep 10
 
 py.test -n 48 scheduler_service/tests auth_service/tests user_service/tests activity_service/tests candidate_pool_service/tests spreadsheet_import_service/tests sms_campaign_service/tests resume_parsing_service/tests candidate_service/tests email_campaign_service/tests
 
-# echo "py.test ats_service/tests/test_v1_ats_account.py"
-# py.test ats_service/tests/test_v1_ats_account.py
-
-# echo "py.test ats_service/tests/test_v1_ats_candidate.py"
-# py.test ats_service/tests/test_v1_ats_candidate.py
-
+# Needs to run separately as parallelism causes DB collisions
 py.test ats_service/tests
