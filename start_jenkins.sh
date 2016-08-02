3ffccde818b9d496220a63b9f39295ce00565427
mzohaibqc <mzohaib.qc@gmail.com>
2016-08-02 21:06:12 +0500

    GET-1508:
    - Run only push campaign tests

--- start_jenkins.sh
+++ start_jenkins.sh
@@ -65,4 +65,4 @@ done
 
 sleep 10
 
-py.test -n 48 scheduler_service/tests auth_service/tests user_service/tests activity_service/tests candidate_pool_service/tests spreadsheet_import_service/tests sms_campaign_service/tests resume_parsing_service/tests candidate_service/tests email_campaign_service/tests push_campaign_service/tests
+py.test push_campaign_service/tests
