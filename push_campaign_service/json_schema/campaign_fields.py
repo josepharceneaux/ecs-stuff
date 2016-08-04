ac579bf861ea893991112d68d1f424c074c50a09
mzohaibqc <mzohaib.qc@gmail.com>
2016-08-04 17:07:21 +0500

    GET-1501: Retrieve candidate with celery task
    - Self code review and then added some changes like
    	- Added doctrings at module level
    	- Added some comments
    	- Added new line at the end of file

--- push_campaign_service/json_schema/campaign_fields.py
+++ push_campaign_service/json_schema/campaign_fields.py
@@ -1,7 +1,18 @@
+"""
+Author: Zohaib Ijaz, QC-Technologies,
+        Lahore, Punjab, Pakistan <mzohaib.qc@gmail.com>
+
+This module contains json schema for Push Campaign data validation.
+Required properties are "name", "body_text", "smartlist_ids", and "url".
+Schema for first 3 required fields are already defined in common code. We need to add validation for "url" field here.
+"""
 from push_campaign_service.common.campaign_services.json_schema.campaign_fields import get_campaign_schema
 from push_campaign_service.modules.push_campaign_base import PushCampaignBase
 
+# Copy common schema for campaign
 CAMPAIGN_SCHEMA = get_campaign_schema()
+
+# Add url as required property with pattern validation.
 CAMPAIGN_SCHEMA['properties'].update({
     "url": {
             "type": "string",
@@ -9,4 +20,5 @@ CAMPAIGN_SCHEMA['properties'].update({
         }
 })
 
+# Specify required fields
 CAMPAIGN_SCHEMA['required'] = list(PushCampaignBase.REQUIRED_FIELDS)
