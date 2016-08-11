49aef88baaab47d61023cc6027ad80c6c8ddc9d8
erik-farmer <erikdfarmer@gmail.com>
2016-08-11 14:28:38 -0700

    Changed IM code to return list of picture data.

--- resume_parsing_service/IMaaS/lambda_handlers.py
+++ resume_parsing_service/IMaaS/lambda_handlers.py
@@ -1,46 +1,69 @@
 """DocString"""
+from itertools import izip_longest
+from wand.image import Image
 import base64
 import zlib
-from wand.image import Image
+
+
+def grouper(iterable, n, fillvalue=None):
+    """
+    Collect data into fixed-length chunks or blocks from an interable.
+
+    This is used to organize PDFs into manageable pictures. Three pages was the sweet spot in
+    in testing before the image became to large to OCR accurately and efficiently.
+
+    Usage example:
+        grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx
+    """
+    args = [iter(iterable)] * n
+    return izip_longest(fillvalue=fillvalue, *args)
 
 
 def convert_pdf_to_png(event, context):
     """
-    Simple use of ImageMagick. Takes in b64 encoded PDF files and turns them into b64 encoded
-    PNG files to be sent to Google Vision OCR. Print statements are used for CloudWatch logging.
+    Simple use of ImageMagick to convert a 1-n page PDF into images in groups of 3 pages.
+
+    Takes in b64 encoded PDF files and turns them into b64 encoded PNG files to be sent to Google
+    Vision OCR. Print statements are used for CloudWatch logging.
     :param event: JSON data POST'd to endpoint.
     :param context: Unused param in all lambda functions.
-    :return: b64 PNG data.
-    :rtype: dict
+    :return: list of zlibcompressed/b64encoded PNG data.
     """
+    GROUP_SIZE = 3
+    img_data = []
     pdf_data = event.get('pdf_bin')
 
     if not pdf_data:
         return {'error': 'No PDF data passed'}
 
     decoded = base64.b64decode(pdf_data)
-    print 'decoded'
+    print 'Decoded'
 
     with Image(blob=decoded, resolution=300) as pdf:
-        pages = len(pdf.sequence)
-        image = Image(width=pdf.width, height=pdf.height * pages)
-        print 'base image made'
+        grouped = grouper(pdf.sequence, GROUP_SIZE)
 
-        for i in xrange(pages):
-            image.composite(
-                pdf.sequence[i],
-                top=pdf.height * i,
-                left=0
-            )
-        print 'image composed'
+        for group in grouped:
+            image = Image(width=pdf.width, height=pdf.height * GROUP_SIZE)
+            print "Base image created"
 
-        blob = image.make_blob('png')
-        print 'blob made'
+            if group:
+                for i, sequence in enumerate(group):
+                    image.composite(
+                        sequence,
+                        top=pdf.height * i,
+                        left=0
+                    )
+            print 'Image composed'
 
-        compressed = zlib.compress(blob)
-        print 'compressed'
+            blob = image.make_blob('png')
+            print 'Blob made'
 
-        encoded = base64.b64encode(compressed)
-        print 'encoded'
+            compressed = zlib.compress(blob)
+            print 'Compressed'
 
-        return {'img_data': encoded}
+            encoded = base64.b64encode(compressed)
+            print 'Encoded'
+
+            img_data.append(encoded)
+
+    return {'img_data': img_data}
