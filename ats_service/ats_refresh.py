0c1efb07a5332ed5cb3cccd7136f74e1bb1181c7
Joseph Arceneaux <joseph.arceneaux@orange-ftgroup.com>
2016-08-15 14:05:57 -0500

    Fix one import issue.

--- ats_service/ats_refresh.py
+++ ats_service/ats_refresh.py
@@ -4,6 +4,7 @@ Refresh our ATS data from the ATS
 
 
 # from ats_service.ats.workday import *
+# from bar.foo import FOO
+from ats.workday import Workday
 
-
-print "Refresh ATS"
+print "Refresh ATS {}".format(FOO)
