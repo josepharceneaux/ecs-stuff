f958058ae0c7889d80262c2f86c9efcf61d0958a
mzohaibqc <mzohaib.qc@gmail.com>
2016-09-09 23:36:17 +0500

    develop: Hotfix, adding talentbot_service in container_config.py, start_jenkins.sh and push_container.sh 	- adding push campaig tests

--- start_jenkins.sh
+++ start_jenkins.sh
@@ -28,6 +28,7 @@ cd sms_campaign_service && tar -czh . | docker build -t gettalent/sms-campaign-s
 cd push_campaign_service && tar -czh . | docker build -t gettalent/push-campaign-service:latest - && cd ../
 cd email_campaign_service && tar -czh . | docker build -t gettalent/email-campaign-service:latest - && cd ../
 cd ats_service && tar -czh . | docker build -t gettalent/ats-service:latest - && cd ../
+cd talentbot_service && tar -czh . | docker build -t gettalent/talentbot-service:latest - && cd ../
 
 # Build the scheduler admin image, which is a nodejs web application
 # This is disabled temporarily due to npm issues
@@ -41,10 +42,10 @@ python setup_environment/reset_database_and_cloud_search.py
 
 ENV_VARIABLES=("GT_ENVIRONMENT" "AWS_ACCESS_KEY_ID" "AWS_SECRET_ACCESS_KEY")
 
-FLASK_APPS=("auth-service" "activity-service" "resume-parsing-service" "user-service" "candidate-service" "social-network-service" "candidate-pool-service" "spreadsheet-import-service" "scheduler-service" "sms-campaign-service" "push-campaign-service" "email-campaign-service" "ats-service")
+FLASK_APPS=("auth-service" "activity-service" "resume-parsing-service" "user-service" "candidate-service" "social-network-service" "candidate-pool-service" "spreadsheet-import-service" "scheduler-service" "sms-campaign-service" "push-campaign-service" "email-campaign-service" "ats-service" "talentbot-service")
 
 # Note that port 8016 is used for scheduler admin web app
-FLASK_APP_PORTS=("8001" "8002" "8003" "8004" "8005" "8007" "8008" "8009" "8011" "8012" "8013" "8014" "8015")
+FLASK_APP_PORTS=("8001" "8002" "8003" "8004" "8005" "8007" "8008" "8009" "8011" "8012" "8013" "8014" "8015" "8017")
 
 
 env_variable_parameters=""
@@ -67,7 +68,7 @@ sleep 10
 
 echo "Beginning tests."
 
-py.test -n 48 scheduler_service/tests auth_service/tests user_service/tests activity_service/tests candidate_pool_service/tests spreadsheet_import_service/tests resume_parsing_service/tests email_campaign_service/tests
+py.test -n 48 push_campaign_service/tests scheduler_service/tests auth_service/tests user_service/tests activity_service/tests candidate_pool_service/tests spreadsheet_import_service/tests resume_parsing_service/tests email_campaign_service/tests
 
 if [ $? -ne 0 ] ; then
     exit 1
