fb11cda86e487faa7188d9efc8b12ba4f0ea786c
Saad <saadfast.qc@gmail.com>
2016-09-28 14:31:28 +0500

    Increased lock expiry time

--- scheduler_service/modules/scheduler.py
+++ scheduler_service/modules/scheduler.py
@@ -176,7 +176,7 @@ def run_job(user_id, access_token, url, content_type, post_data, is_jwt_request=
     lock_uuid = kwargs.get('lock_uuid')
     if lock_uuid:
         if not redis_store.get(LOCK_KEY + lock_uuid):
-            res = redis_store.set(LOCK_KEY + lock_uuid, True, nx=True, ex=2)
+            res = redis_store.set(LOCK_KEY + lock_uuid, True, nx=True, ex=4)
             # Multiple executions. No need to execute job if race condition occurs
             if not res:
                 logger.error('CODE-VERONICA: Escaping {}'.format(lock_uuid))
