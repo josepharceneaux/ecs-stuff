991e5aca087813566ed5de5454ec633c5f3ea656
Amir <amir@gettalent.com>
2016-10-12 15:30:55 -0700

    GET-1698
    - Add logic for accurately updating candidate's records
    - Remove duplicate records before inserting them into database
    - Simplify test cases
    - Improve docstrings
    - Add todos for missing logic

--- graphql_service/tests/test_candidates.py
+++ graphql_service/tests/test_candidates.py
@@ -2,6 +2,8 @@ from graphql_service.application import app
 from graphql_service.common.tests.conftest import *
 from helpers import GraphQLClient
 
+headers = {'content-type': 'application/json'}
+
 
 class TestQueryCandidate(object):
     def test_get_candidate(self):
@@ -25,9 +27,26 @@ class TestAddCandidate(object):
             emails=[dict(address=fake.safe_email())]
         )
         client = GraphQLClient(is_creating=True, **candidate_data)
-        response = requests.post(url=client.base_url,
-                                 headers={'content-type': 'application/json'},
-                                 data=json.dumps(client.generated_query_string))
+        response = requests.post(url=client.base_url, headers=headers, data=json.dumps(client.generated_query_string))
         assert response.status_code == requests.codes.OK
         print '\ndata: {}'.format(response.json())
         assert response.json()['data']['create_candidate']['id'] is not None
+
+
+class TestUpdateCandidate(object):
+    def test_change_candidates_primary_information(self):
+        # Add candidate
+        candidate_data = dict(
+            first_name=fake.first_name(),
+            middle_name=fake.first_name(),
+            last_name=fake.last_name()
+        )
+        client = GraphQLClient(is_creating=True, **candidate_data)
+        response = requests.post(url=client.base_url, headers=headers, data=json.dumps(client.generated_query_string))
+        print '\ndata: {}'.format(response.json())
+
+        candidate_id = response.json()['data']['create_candidate']['id']
+
+        # Change candidate's first & last names
+        update_data = dict(first_name=fake.first_name(), last_name=fake.last_name())
+        client = GraphQLClient()
