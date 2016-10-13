9cdcdfcf4513453f40645d56a74049b4252b75dd
Hafiz Muhammad Basit <basit.gettalent@gmail.com>
2016-10-13 19:15:11 +0500

    GET-1472: Added db.create_all() in email_campaign_service

--- email_campaign_service/email_campaign_app/__init__.py
+++ email_campaign_service/email_campaign_app/__init__.py
@@ -1,8 +1,10 @@
 """Initialize Email campaign service app, register error handlers and register blueprint"""
 
 from flask.ext.cache import Cache
+from email_campaign_service.common.models.db import db
 from email_campaign_service.common.talent_celery import init_celery_app
 from email_campaign_service.common.utils.models_utils import init_talent_app
+from email_campaign_service.common.talent_config_manager import TalentConfigKeys
 from email_campaign_service.common.campaign_services.campaign_utils import CampaignUtils
 
 app, logger = init_talent_app(__name__)
@@ -10,8 +12,15 @@ app, logger = init_talent_app(__name__)
 # Instantiate Flask-Cache object
 cache = Cache(app, config={'CACHE_TYPE': 'redis', 'CACHE_REDIS_URL': app.config['REDIS_URL']})
 
-# Celery app
-celery_app = init_celery_app(app, CampaignUtils.EMAIL,
-                             ['email_campaign_service.modules.email_marketing',
-                              'email_campaign_service.modules.utils',
-                              'email_campaign_service.modules.email_clients'])
+try:
+    # Celery app
+    celery_app = init_celery_app(app, CampaignUtils.EMAIL,
+                                 ['email_campaign_service.modules.email_marketing',
+                                  'email_campaign_service.modules.utils',
+                                  'email_campaign_service.modules.email_clients'])
+    db.create_all()
+    db.session.commit()
+
+except Exception as e:
+    logger.exception("Couldn't start email_campaign_service in %s environment because: %s"
+                     % (app.config[TalentConfigKeys.ENV_KEY], e.message))
