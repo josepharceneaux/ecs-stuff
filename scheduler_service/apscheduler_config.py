1f2bd7f198160013e04f6f659fbde3fb0304112a
Saad <saadfast.qc@gmail.com>
2016-10-15 03:04:28 +0500

    Increased scheduler workers

--- scheduler_service/apscheduler_config.py
+++ scheduler_service/apscheduler_config.py
@@ -19,7 +19,7 @@ else:
 
 executors = {
     'default': ThreadPoolExecutor(MAX_THREAD_POOLS),
-    'processpool': ProcessPoolExecutor(max_workers=8)
+    'processpool': ProcessPoolExecutor(max_workers=12)
 }
 
 jobstores = {
