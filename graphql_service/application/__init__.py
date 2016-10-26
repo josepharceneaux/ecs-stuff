18b77bc00c0c335339c4207341419a8de2658677
Hafiz Muhammad Basit <basit.gettalent@gmail.com>
2016-10-26 18:38:56 +0500

    GET-1718: Added /graphql route in __init__ and removed unused var from docker file

--- graphql_service/application/__init__.py
+++ graphql_service/application/__init__.py
@@ -1,8 +1,36 @@
-from graphql_service.common.utils.models_utils import init_talent_app
+# Third Party
+from graphene import Schema
+from flask_graphql import GraphQLView
+
+# Application Specific
 from graphql_service.common.models.db import db
+from graphql_service.common.talent_config_manager import TalentEnvs
+from graphql_service.common.error_handling import InternalServerError
+from graphql_service.common.utils.models_utils import init_talent_app
 from graphql_service.common.talent_config_manager import TalentConfigKeys
 
 app, logger = init_talent_app(__name__)
+
+# Creating Schema
+try:
+    from graphql_service.application.query import Query
+    from graphql_service.application.mutation import Mutation
+
+    schema = Schema(query=Query, mutation=Mutation, auto_camelcase=False)
+except Exception as e:
+    logger.exception("Error: {}".format(e.message))
+    raise InternalServerError('Unable to create schema because: {}'.format(e.message))
+
+# Adding URL Rule
+app.add_url_rule(
+    rule='/graphql',
+    view_func=GraphQLView.as_view(
+        'graphql',
+        schema=schema,
+        # graphiql should only run for testing
+        graphiql=app.config[TalentConfigKeys.ENV_KEY] in [TalentEnvs.DEV, TalentEnvs.JENKINS]
+    )
+)
 
 try:
     db.create_all()
--- graphql_service/run.py
+++ graphql_service/run.py
@@ -1,37 +1,9 @@
 """
-Initializer for graphql_service
+Start graphql_service
 """
-# Third Party
-from graphene import Schema
-from flask_graphql import GraphQLView
-
 # Application Specific
+from graphql_service.application import app
 from graphql_service.common.routes import GTApis
-from graphql_service.application import app, logger
-from graphql_service.common.talent_config_manager import TalentEnvs
-from graphql_service.common.error_handling import InternalServerError
-
-# Creating Schema
-try:
-    from graphql_service.application.query import Query
-    from graphql_service.application.mutation import Mutation
-
-    schema = Schema(query=Query, mutation=Mutation, auto_camelcase=False)
-except Exception as e:
-    logger.exception("Error: {}".format(e.message))
-    raise InternalServerError('Unable to create schema because: {}'.format(e.message))
 
 if __name__ == '__main__':
-    # Adding URL Rule
-    app.add_url_rule(
-        rule='/graphql',
-        view_func=GraphQLView.as_view(
-            'graphql',
-            schema=schema,
-            # graphiql should only run for testing
-            # graphiql=app.config['GT_ENVIRONMENT'] in [TalentEnvs.DEV, TalentEnvs.JENKINS]
-            graphiql=True
-        )
-    )
-
     app.run(host='0.0.0.0', port=GTApis.GRAPHQL_SERVICE_PORT, use_reloader=True, debug=False, threaded=True)
