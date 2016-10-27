89a3943788ce211756ff6707cce546b22bc6711d
Umar Farooqi <farooqiumar@rocketmail.com>
2016-10-27 17:24:30 -0400

    Removed Unnecessary tests from UserService for DomainCustomFields

--- email_campaign_service/tests/unit_tests/test_units.py
+++ email_campaign_service/tests/unit_tests/test_units.py
@@ -27,10 +27,9 @@ def test_mergetag_replacements(user_first, candidate_first):
 
     # Merge tags for candidate
     campaign = create_email_campaign_with_merge_tags(user_first)
-    candidate_address = candidate_first.emails[0].address
 
     [subject, body_text, body_html] = do_mergetag_replacements([campaign.subject, campaign.body_text,
-                                                                campaign.body_html], candidate_first, candidate_address)
+                                                                campaign.body_html], candidate_first)
     for item in [subject, body_text, body_html]:
         assert candidate_first.first_name in item
         assert candidate_first.last_name in item
