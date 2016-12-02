03d1036af7520153285fb25444e636d7e45f53bb
Amir <amir@gettalent.com>
2016-12-02 15:55:47 -0800

    GET-1944
    - Add migration script to update domain-candidate's source ID

--- app_common/common/models/user.py
+++ app_common/common/models/user.py
@@ -300,7 +300,8 @@ class Domain(db.Model):
 
     # Relationships
     users = relationship('User', backref='domain')
-    candidate_sources = relationship('CandidateSource', backref='domain')
+    candidate_sources = relationship('CandidateSource', backref='domain', cascade='all,delete-orphan',
+                                     passive_deletes=True)
     areas_of_interest = relationship('AreaOfInterest', backref='domain')
     custom_fields = relationship('CustomField', backref='domain')
     email_template_folders = relationship('EmailTemplateFolder', backref='domain')
