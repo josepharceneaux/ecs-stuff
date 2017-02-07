68f497142461f272aba3474a37caa798450d5fc6
erik-farmer <erikdfarmer@gmail.com>
2017-02-07 14:11:12 -0800

    add docker related items and update jenkins file

--- start_jenkins.sh
+++ start_jenkins.sh
@@ -37,6 +37,7 @@ cd ats_service && tar -czh . | docker build -t gettalent/ats-service:latest - &&
 cd mock_service && tar -czh . | docker build -t gettalent/mock-service:latest - && cd ../
 cd talentbot_service && tar -czh . | docker build -t gettalent/talentbot-service:latest - && cd ../
 cd graphql_service && tar -czh . | docker build -t gettalent/graphql-service:latest - && cd ../
+cd banner_service && tar -czh . | docker build -t gettalent/banner-service:latest - && cd ../
 
 # TODO: Move scheduler service admin to another repo
 # cd scheduler_service_admin && tar -czh . | docker build -t gettalent/scheduler-service-admin:latest - && cd ../
@@ -49,10 +50,10 @@ python setup_environment/reset_database_and_cloud_search.py
 
 ENV_VARIABLES=("GT_ENVIRONMENT" "AWS_ACCESS_KEY_ID" "AWS_SECRET_ACCESS_KEY")
 
-FLASK_APPS=("auth-service" "activity-service" "resume-parsing-service" "user-service" "candidate-service" "social-network-service" "candidate-pool-service" "spreadsheet-import-service" "scheduler-service" "sms-campaign-service" "push-campaign-service" "email-campaign-service" "ats-service" "mock-service" "talentbot-service" "graphql-service")
+FLASK_APPS=("auth-service" "activity-service" "resume-parsing-service" "user-service" "candidate-service" "social-network-service" "candidate-pool-service" "spreadsheet-import-service" "scheduler-service" "sms-campaign-service" "push-campaign-service" "email-campaign-service" "ats-service" "mock-service" "talentbot-service" "graphql-service" "banner_service")
 
 # Note that port 8016 is used for scheduler admin web app
-FLASK_APP_PORTS=("8001" "8002" "8003" "8004" "8005" "8007" "8008" "8009" "8011" "8012" "8013" "8014" "8015" "8016" "8017" "8018")
+FLASK_APP_PORTS=("8001" "8002" "8003" "8004" "8005" "8007" "8008" "8009" "8011" "8012" "8013" "8014" "8015" "8016" "8017" "8018" "8019")
 
 
 env_variable_parameters=""
