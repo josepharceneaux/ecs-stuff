b8f2777e2154d565adea12affd68588c7628bb16
erik-farmer <erikdfarmer@gmail.com>
2017-02-07 10:06:59 -0800

    Finish out most of create end point with redis dict creation

--- banner_service/app/__init__.py
+++ banner_service/app/__init__.py
@@ -6,10 +6,6 @@ from flask.ext.redis import FlaskRedis
 
 from banner_service.common.utils.models_utils import init_talent_app
 from banner_service.common.talent_config_manager import TalentConfigKeys
-# from banner_service.common.routes import ResumeApi, GTApis
-# from banner_service.common.talent_config_manager import load_gettalent_config, TalentConfigKeys
-# from banner_service.common.utils.talent_ec2 import get_ec2_instance_id
-# from banner_service.common.talent_flask import TalentFlask
 # from banner_service.common.models.db import db
 
 app, logger = init_talent_app(__name__)
--- banner_service/app/views/v1_banners_api.py
+++ banner_service/app/views/v1_banners_api.py
@@ -1,12 +1,50 @@
-from flask import jsonify, Blueprint
+# StdLib
+from time import time
+# Third Party
+from flask import Blueprint, jsonify, request
 from flask_restful import Resource
+# Module Specific
+from banner_service.app import redis_store
+from banner_service.common.error_handling import InvalidUsage
 from banner_service.common.talent_api import TalentApi
+
+BANNER_REDIS_KEY = 'gt_global_banner'
+REQUIRED_DATA = ('title', 'text', 'link', 'color')
 
 banner_api_bp = Blueprint('v1_banner_api', __name__)
 api = TalentApi(banner_api_bp)
 
+
 class BannerResource(Resource):
+    """
+    CRD API for interacting with Redis.
+    """
+
     def get(self):
         return jsonify({'Response': 'Landed'})
+
+    #TODO content-type decorator
+    def post(self):
+        # Check to see if there is an existing entry at the key prefix
+        existing_banner = redis_store.hgetall(BANNER_REDIS_KEY)
+        # Return Error if so
+        if existing_banner:
+            raise InvalidUsage(
+                error_message="Cannot POST banner when an active banner exists")
+
+        posted_data = request.get_json()
+        for required_param in REQUIRED_DATA:
+            if not posted_data.get(required_param):
+                raise InvalidUsage(
+                    error_message='Missing param: {}'.format(required_param))
+
+        posted_data['timestamp'] = time()
+        # TODO Implement validated user ID
+        # posted_data['owner_id'] = request.user.id
+
+        current_banner = redis_store.hmset(posted_data)
+
+        return jsonify(current_banner)
+
 
 api.add_resource(BannerResource, '/banners')
