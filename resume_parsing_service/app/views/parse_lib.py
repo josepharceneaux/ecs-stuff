e34b74ab7339ecbc52e073f7c96873d417f41762
erik-farmer <erikdfarmer@gmail.com>
2017-02-10 12:03:30 -0800

    formatting

--- resume_parsing_service/app/views/parse_lib.py
+++ resume_parsing_service/app/views/parse_lib.py
@@ -17,7 +17,8 @@ from resume_parsing_service.app.constants import error_constants
 from resume_parsing_service.app.views.optic_parse_lib import fetch_optic_response
 from resume_parsing_service.app.views.optic_parse_lib import parse_optic_xml
 from resume_parsing_service.app.views.ocr_lib import ocr_image
-from resume_parsing_service.app.views.pdf_utils import (convert_pdf_to_text, decrypt_pdf)
+from resume_parsing_service.app.views.pdf_utils import (convert_pdf_to_text,
+                                                        decrypt_pdf)
 from resume_parsing_service.common.error_handling import InvalidUsage
 from resume_parsing_service.common.utils.talent_s3 import boto3_put
 from resume_parsing_service.common.utils.resume_utils import IMAGE_FORMATS, DOC_FORMATS
@@ -37,7 +38,8 @@ def parse_resume(file_obj, filename_str, cache_key):
     """
     logger.info("Beginning parse_resume(%s)", filename_str)
 
-    file_ext = basename(splitext(filename_str.lower())[-1]) if filename_str else ""
+    file_ext = basename(
+        splitext(filename_str.lower())[-1]) if filename_str else ""
 
     # PDFs must have attempted decryption with the empty string password if applicable.
     if file_ext == '.pdf':
@@ -67,15 +69,13 @@ def parse_resume(file_obj, filename_str, cache_key):
             doc_content = ocr_image(file_obj, filename_str)
 
         logger.info(
-            "ResumeParsingService::Benchmark: OCR for {}: took {}s to process".format(
-                filename_str, time() - start_time)
-        )
+            "ResumeParsingService::Benchmark: OCR for {}: took {}s to process".
+            format(filename_str, time() - start_time))
 
     else:
         doc_content = file_obj.getvalue()
 
     validate_content_len(doc_content, file_obj, filename_str)
-
 
     try:
         encoded_resume = base64.b64encode(doc_content)
@@ -84,18 +84,18 @@ def parse_resume(file_obj, filename_str, cache_key):
         logger.exception('Error encoding resume before sending to BG Optic.')
         raise InvalidUsage(
             error_message=error_constants.ERROR_ENCODING_TEXT['message'],
-            error_code=error_constants.ERROR_ENCODING_TEXT['code']
-        )
+            error_code=error_constants.ERROR_ENCODING_TEXT['code'])
 
     optic_response = fetch_optic_response(encoded_resume, filename_str)
 
     if optic_response:
 
-        if all(fail_word in optic_response for fail_word in ('error', 'text', 'not', 'generated')):
+        if all(
+                fail_word in optic_response
+                for fail_word in ('error', 'text', 'not', 'generated')):
             raise InvalidUsage(
                 error_message=error_constants.NO_TEXT_EXTRACTED['message'],
-                error_code=error_constants.NO_TEXT_EXTRACTED['code']
-            )
+                error_code=error_constants.NO_TEXT_EXTRACTED['code'])
 
         redis_store.set(cache_key, optic_response)
         redis_store.expire(cache_key, RESUME_EXPIRE_TIME)
@@ -103,11 +103,11 @@ def parse_resume(file_obj, filename_str, cache_key):
         return {'raw_response': optic_response, 'candidate': candidate_data}
 
     else:
-        logger.info('No XML text received from Optic Response for {}'.format(filename_str))
+        logger.info('No XML text received from Optic Response for {}'.format(
+            filename_str))
         raise InvalidUsage(
             error_message=error_constants.BG_NO_PARSED_TEXT['message'],
-            error_code=error_constants.BG_NO_PARSED_TEXT['code']
-        )
+            error_code=error_constants.BG_NO_PARSED_TEXT['code'])
 
 
 @contract
@@ -124,11 +124,12 @@ def is_resume_image(file_ext, file_obj):
         file_ext = ".{}".format(file_ext)
 
     if file_ext not in IMAGE_FORMATS and file_ext not in DOC_FORMATS:
-        logger.info('File ext \'{}\' not in accepted image or document formats'.format(file_ext))
+        logger.info(
+            'File ext \'{}\' not in accepted image or document formats'.format(
+                file_ext))
         raise InvalidUsage(
             error_message=error_constants.INVALID_FILE_TYPE['message'],
-            error_code=error_constants.INVALID_FILE_TYPE['code']
-        )
+            error_code=error_constants.INVALID_FILE_TYPE['code'])
 
     # Find out if the file is an image
     if file_ext in IMAGE_FORMATS:
@@ -142,13 +143,17 @@ def is_resume_image(file_ext, file_obj):
 
     return resume_is_image
 
+
 def validate_content_len(doc_content, file_obj, filename_str):
-    if not doc_content or len(doc_content) < 10: #  If doc content is < 10 not worth parsing.
+    if not doc_content or len(
+            doc_content) < 10:  #  If doc content is < 10 not worth parsing.
         bucket = current_app.config['S3_BUCKET_NAME']
         boto3_put(file_obj.getvalue(), bucket, filename_str, 'FailedResumes')
-        logger.error('ResumeParsingService::UncaughtError::parse_resume Key {}. Unable to determine the documents contents of'.format(filename_str))
-        file_obj.close()  # Free file from memory after attempted upload caused by failure.
+        logger.error(
+            'ResumeParsingService::UncaughtError::parse_resume Key {}. Unable to determine the documents contents of'.
+            format(filename_str))
+        file_obj.close(
+        )  # Free file from memory after attempted upload caused by failure.
         raise InvalidUsage(
             error_message=error_constants.NO_TEXT_EXTRACTED['message'],
-            error_code=error_constants.NO_TEXT_EXTRACTED['code']
-        )
+            error_code=error_constants.NO_TEXT_EXTRACTED['code'])
