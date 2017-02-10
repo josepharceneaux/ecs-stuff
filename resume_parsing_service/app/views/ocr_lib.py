f2620b66ad54ea808d32d57af226d7fb2859b244
erik-farmer <erikdfarmer@gmail.com>
2017-02-10 12:01:13 -0800

    formatting

--- resume_parsing_service/app/views/ocr_lib.py
+++ resume_parsing_service/app/views/ocr_lib.py
@@ -25,34 +25,39 @@ def ocr_image(img_file_obj, filename_str):
     }
 
     try:
-        ocr_response = requests.post(OCR_URL, files={filename_str: img_file_obj}, data=payload)
+        ocr_response = requests.post(
+            OCR_URL, files={filename_str: img_file_obj}, data=payload)
     except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
-                logger.error("Could not reach OCRSpace.")
-                raise InternalServerError(
-                    error_message=error_constants.OCR_SPACE_UNAVAILABLE['message'],
-                    error_code=error_constants.OCR_SPACE_UNAVAILABLE['code']
-                )
+        logger.error("Could not reach OCRSpace.")
+        raise InternalServerError(
+            error_message=error_constants.OCR_SPACE_UNAVAILABLE['message'],
+            error_code=error_constants.OCR_SPACE_UNAVAILABLE['code'])
 
     decoded_ocr_response = ocr_response.content.decode('utf8')
     json_ocr_response = json.loads(decoded_ocr_response)
 
-    logger.info('ResumeParsingService::Info::OCRSpace response: {}'.format(json_ocr_response))
+    logger.info('ResumeParsingService::Info::OCRSpace response: {}'.format(
+        json_ocr_response))
     ocr_confidence = json_ocr_response.get('OCRExitCode')
 
     if ocr_confidence in (3, 4):
         logger.error('ResumeParsingService::Error::Error in OCR Response')
         raise InternalServerError(
             error_message=error_constants.OCR_SPACE_ERROR['message'],
-            error_code=error_constants.OCR_SPACE_ERROR['code']
-        )
+            error_code=error_constants.OCR_SPACE_ERROR['code'])
 
     else:
-        logger.info('ResumeParsingService::Info::OCR Score {}'.format(ocr_confidence))
+        logger.info(
+            'ResumeParsingService::Info::OCR Score {}'.format(ocr_confidence))
 
-    results =  ''.join(result.get('ParsedText') for result in json_ocr_response.get('ParsedResults'))
+    results = ''.join(
+        result.get('ParsedText')
+        for result in json_ocr_response.get('ParsedResults'))
     unicode_subbed_results = re.sub(u'[^\x00-\x7F]+', ' ', results)
     try:
-        logger.info('ResumeParsingService::Info::OCRSpace ParsedText: {}'.format(unicode_subbed_results))
+        logger.info('ResumeParsingService::Info::OCRSpace ParsedText: {}'.
+                    format(unicode_subbed_results))
     except Exception as e:
-        logger.error('ResumeParsingService::Error::OCRSpace couldn\'t log results')
+        logger.error(
+            'ResumeParsingService::Error::OCRSpace couldn\'t log results')
     return unicode_subbed_results
