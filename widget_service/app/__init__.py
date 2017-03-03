443f4eb339a4d9e8a0d2c8a69fe6bb7403bf1070
erik-farmer <erikdfarmer@gmail.com>
2017-03-03 09:56:06 -0800

    Mimic RPS init more to see why no init in jenkins or migrations

--- widget_service/app/__init__.py
+++ widget_service/app/__init__.py
@@ -1,10 +1,14 @@
+"""Initializer for Widget App"""
+
 __author__ = 'erikfarmer'
 
 from flask.ext.redis import FlaskRedis
 
 from widget_service.common.utils.models_utils import init_talent_app
 from widget_service.common.routes import WidgetApi
-from widget_service.common.talent_config_manager import TalentConfigKeys
+from widget_service.common.talent_config_manager import load_gettalent_config, TalentConfigKeys
+from widget_service.common.utils.talent_ec2 import get_ec2_instance_id
+from widget_service.common.talent_flask import TalentFlask
 from widget_service.common.models.db import db
 
 app, logger = init_talent_app(__name__)
