be98ab1654977dfe0d8ac8a405d223d61b1cc6d8
kamalhasanqc <kamalhasan.qc@gmail.com>
2017-03-09 18:29:00 +0500

    Hotfix: Changed chunk size for email campaign lambda sender

--- email_campaign_service/modules/email_marketing.py
+++ email_campaign_service/modules/email_marketing.py
@@ -364,8 +364,8 @@ def process_campaign_send(celery_result, user_id, campaign_id, list_ids, new_can
             except Exception as error:
                 logger.error("Couldn't get boto3 lambda client Error: %s" % error.message)
                 return
-            chunks_of_candidate_ids_list = (candidate_ids_and_emails[x:x + 10] for x in
-                                            xrange(0, len(candidate_ids_and_emails), 10))
+            chunks_of_candidate_ids_list = (candidate_ids_and_emails[x:x + 100] for x in
+                                            xrange(0, len(candidate_ids_and_emails), 100))
             for chunk in chunks_of_candidate_ids_list:
                 chunk_of_candidate_ids_and_address = []
                 number_of_lambda_invocations = 0
@@ -377,7 +377,7 @@ def process_campaign_send(celery_result, user_id, campaign_id, list_ids, new_can
                 try:
                     invoke_lambda_sender(_lambda, chunk_of_candidate_ids_and_address)
                     number_of_lambda_invocations += 1
-                    if number_of_lambda_invocations % 100 == 0:
+                    if number_of_lambda_invocations % 10 == 0:
                         # 3 seconds delay after 100 lambda invocations
                         sleep(3)
                 except Exception as error:
