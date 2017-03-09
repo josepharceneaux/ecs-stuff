33e479205b2f5db4368620a9dc49575c1e5b882f
erik-farmer <erikdfarmer@gmail.com>
2017-03-09 13:14:34 -0800

    Add tracker and improve formatting/readability

--- widget_service/app/modules/v1_api_handlers.py
+++ widget_service/app/modules/v1_api_handlers.py
@@ -1,10 +1,12 @@
 __author__ = 'erik@gettalent'
+# Standard Library.
 import json
 from collections import defaultdict
-
+from uuid import uuid4
+# Third Party.
 import requests
 from flask import jsonify
-
+# Module Specific.
 from widget_service.app import db
 from widget_service.app import logger
 from widget_service.app.modules.utils import parse_city_and_state_ids_from_form
@@ -19,16 +21,20 @@ from widget_service.common.routes import CandidateApiUrl
 
 
 def create_widget_candidate(form, talent_pool_hash):
-    # Initial Setup/Limited validation based on TalentPool and WidgetPage info
-    logger.info('WidgetService::FormInfo - {}'.format(form))
+    # Initial Setup/Limited validation based on TalentPool and WidgetPage info.
+    api_tracker = str(uuid4())
+    logger.info('WidgetService::Info  {} - {}'.format(api_tracker, form))
+
     talent_pool = TalentPool.get_by_simple_hash(talent_pool_hash)
     if not talent_pool:
         raise InvalidUsage(error_message='Unknown talent pool')
     domain_id = talent_pool.domain_id
+
     widget = WidgetPage.get_by_simple_hash(talent_pool_hash)
     if not widget:
         raise InvalidUsage(error_message='Unknown widget page')
     widget_user_id = widget.user_id
+
     widget_token = User.generate_jw_token(expiration=60, user_id=widget_user_id)
 
     candidate_dict = defaultdict(dict)
@@ -60,14 +66,16 @@ def create_widget_candidate(form, talent_pool_hash):
 
     # Location based fields.
     candidate_locations = form.get('hidden-tags-location')
-    candidate_city = form.get('city')
-    candidate_state = form.get('state')
     if candidate_locations:
         custom_fields = parse_city_and_state_ids_from_form(candidate_locations, domain_id)
         candidate_dict.setdefault('custom_fields', []).extend(custom_fields)
+
+    candidate_city = form.get('city')
+    candidate_state = form.get('state')
     if candidate_city and candidate_state:
         city_state_list = process_city_and_state_from_fields(candidate_city, candidate_state, domain_id)
         candidate_dict.setdefault('custom_fields', []).extend(city_state_list)
+
     candidate_frequency = form.get('jobFrequency')
     if candidate_frequency:
         frequency_field = db.session.query(CustomField).filter_by(
@@ -78,30 +86,32 @@ def create_widget_candidate(form, talent_pool_hash):
         })
 
     # Education fields.
-    candidate_graduation_date = form.get('graduation', '')
+    candidate_graduation_date = form.get('graduation')
     if candidate_graduation_date:
         candidate_graduation_date = candidate_graduation_date.split(' ')[1]
-        candidate_dict['educations'] = [{
-            'school_name':
-            form.get('university'),
-            'degrees': [{
-                'type': form.get('degree'),
-                'title': form.get('major'),
-                'end_year': candidate_graduation_date,
-            }]
+
+    candidate_dict['educations'] = [{
+        'school_name':
+        form.get('university'),
+        'degrees': [{
+            'type': form.get('degree'),
+            'title': form.get('major'),
+            'end_year': candidate_graduation_date,
         }]
+    }]
 
     # Military specific processing.
-    military_service_dict = dict()
-    military_service_dict['branch'] = form.get('militaryBranch')
-    military_service_dict['status'] = form.get('militaryStatus')
-    military_service_dict['highest_grade'] = form.get('militaryGrade')
-    military_service_dict['to_date'] = form.get('militaryToDate')
+    military_service_dict = {
+        'branch': form.get('militaryBranch'),
+        'status': form.get('militaryStatus'),
+        'highest_grade': form.get('militaryGrade'),
+        'to_date': form.get('militaryToDate')
+    }
 
     candidate_dict['military_services'] = [military_service_dict]
 
-    # TODO consider logging the full dict.
     candidate_dict['talent_pool_ids']['add'] = [talent_pool.id]
+    logger.info("WidgetService::Info - {} - Finished Candidate {}".format(api_tracker, candidate_dict))
     payload = json.dumps({'candidates': [candidate_dict]})
 
     response = requests.post(
