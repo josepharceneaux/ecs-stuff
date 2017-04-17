e749b09c175a0dc57d4752e1bc8119e76fb8bd15
erik-farmer <erikdfarmer@gmail.com>
2017-04-17 16:36:08 -0700

    PR suggestions
    Added docstrings
    Raised Exceptions instead of manually specifying error output
    Improved exception param logging

--- candidate_service/candidate_app/api/v1_candidates.py
+++ candidate_service/candidate_app/api/v1_candidates.py
@@ -2101,10 +2101,14 @@ class CandidateDocumentResource(Resource):
 
     @require_all_permissions(Permission.PermissionNames.CAN_EDIT_CANDIDATES)
     def post(self, **kwargs):
-        # kwargs are {'candidate_id': 489}
+        """
+        Consumes a json dict containing class defined keys and creates a CandidateDocument entry for them.
+        S3 upload is handled by FilePicker.
+        :param kwargs: dict like {'candidate_id': 489}
+        """
         request_json = request.json
-        if not all(key in self.REQUIRED_POST_KEYS for key in request_json.keys()):
-            return 'Missing required json keys', 400
+        if not all(key in self.REQUIRED_POST_KEYS for key in request_json):
+            raise InvalidUsage('Missing required JSON keys')
 
         candidate_id = kwargs['candidate_id']
         if not does_candidate_belong_to_users_domain(request.user, candidate_id):
@@ -2117,12 +2121,15 @@ class CandidateDocumentResource(Resource):
             db.session.commit()
         except Exception as e:
             logger.exception('Error recording Candidate Document')
-            raise InternalServerError('Error Saving Candidate Document')
+            raise InternalServerError('Error Saving Candidate Document: {}'.format(str(request_json)))
 
         return json.dumps({'document_id': candidate_document.id}), 201
 
     @require_all_permissions(Permission.PermissionNames.CAN_EDIT_CANDIDATES)
     def get(self, **kwargs):
+        """
+        Returns CandidateDocuments associated with a candidate from the users domain 
+        """
         candidate_id = kwargs['candidate_id']
         if not does_candidate_belong_to_users_domain(request.user, candidate_id):
             raise InvalidUsage('Candidate does not belong to User\'s domain')
