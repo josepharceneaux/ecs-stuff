27200716ae0827c4754a4d7df3c201caa17ec038
erik-farmer <erikdfarmer@gmail.com>
2017-05-02 16:45:17 -0700

    GET-2344: User Level Activities
     * Add v2 endpoint with paginated user level activities
     * Endpoint is authenticated and authorized at the domain level
     * Endpoint returns paginated response
     * Adds v2 activity manager
     * Added fake name as param in User model for test cases

--- activity_service/app/modules/activity_fetchers.py
+++ activity_service/app/modules/activity_fetchers.py
@@ -1,11 +1,16 @@
-from activity_service.common.models.user import User
 from activity_service.common.models.misc import Activity
 
-def fetch_aggregated_activities(params):
-    user_ids = User.query.filter_by(domain_id=params['domain_id']).values('id')
-    flattened_user_ids = [item for sublist in user_ids for item in sublist]
-    filters = [Activity.user_id.in_(flattened_user_ids)]
-    start = params.start_param
-    if start:
-        filters.append(Activity.added_time >= start)
-    activities = Activity.query.filter(*filters).order_by(Activity.added_time.desc()).limit(200).all()
+
+def fetch_user_activities(user_id, page, qty):
+    return Activity.query.filter(Activity.user_id == user_id).order_by(Activity.added_time.desc()).paginate(
+        page, qty, False)
+
+
+# def fetch_aggregated_activities(params):
+#     user_ids = User.query.filter_by(domain_id=params['domain_id']).values('id')
+#     flattened_user_ids = [item for sublist in user_ids for item in sublist]
+#     filters = [Activity.user_id.in_(flattened_user_ids)]
+#     start = params.start_param
+#     if start:
+#         filters.append(Activity.added_time >= start)
+#     activities = Activity.query.filter(*filters).order_by(Activity.added_time.desc()).limit(200).all()
--- activity_service/app/views/api_v2.py
+++ activity_service/app/views/api_v2.py
@@ -1,51 +1,80 @@
-from uuid import uuid4
 from flask import Blueprint
 from flask import request
-from activity_service.app import db, logger
-
+from flask import jsonify
+# from activity_service.app import db, logger
+from activity_service.common.utils.auth_utils import require_oauth
+from activity_service.app.modules.validators import users_are_in_same_domain
+from activity_service.common.models.db import db
+from activity_service.common.models.user import User
+from activity_service.common.error_handling import UnauthorizedError
+from activity_service.app.modules.activity_fetchers import fetch_user_activities
+from activity_service.app.modules.v2_activity_manager import get_recent_readable
 
 api_v2 = Blueprint('activities_api_v2', __name__)
+DEFAULT_LIMIT = 20
 
 
-# Create activities
-@api_v2.route('/activities', methods=['POST'])
-def create_activity():
-    return 'create'
+@api_v2.route('/activities/<int:page>/user/<int:user_id>')
+@require_oauth()
+def get_user_activities(page, user_id):
+    requested_user_id = user_id
+    requesting_user_id = request.user.id
+    user_name = 'You'
+    if requested_user_id != requesting_user_id:
+        if not users_are_in_same_domain(set([requested_user_id, requesting_user_id]), request.user.domain_id):
+            raise UnauthorizedError(error_message='You are not authorized to view that users activity')
+        user_name = db.session.query(User).get(requested_user_id).first_name
+
+    requested_qty = request.args.get('qty')
+    if requested_qty:
+        qty = int(requested_qty)
+    else:
+        qty = DEFAULT_LIMIT
+
+    activities = fetch_user_activities(requested_user_id, page, qty)
+    readable_activities = get_recent_readable(activities, user_name)
+    return jsonify(readable_activities), 200
 
 
-# Get list of domain activities excluding user for toasts
-@api_v2.route('/activities', methods=['GET'])
-def get_domain_activities():
-    rargs = request.args
-    params = {
-        'api_call': uuid4(),
-        'user_id': request.user.id,
-        'start_param': rargs.get('start_datetime'),
-    }
-    logger.info("ActivityService::Info::DomainInit: {}".format(params['api_id']))
-    return 'domain'
-
-
-# Get paginated domain activities for list page for last 30 days
-@api_v2.route('/activities/<page>', methods=['GET'])
-def get_paginated_activities(page):
-    params = {
-        'api_call': uuid4(),
-        'user_id': request.user.id,
-        'page': page,
-    }
-    logger.info("ActivityService::Info::PaginatedInit: {}".format(params['api_id']))
-    return 'domain paginated {}'.format(page)
-
-
-# Get aggregated activities for drop down
-@api_v2.route('/aggregate', methods=['GET'])
-def get_aggregated_activities():
-    params = {
-        'api_call': uuid4(),
-        'domain_id': request.user.domain_id,
-        'start': request.args.get('start_datetime'),
-        'user_id': request.user.id,
-    }
-    logger.info("ActivityService::Info::AggregateInit: {}".format(params['api_id']))
-    return 'aggregated'
+# # Create activities
+# @api_v2.route('/activities', methods=['POST'])
+# def create_activity():
+#     return 'create'
+#
+#
+# # Get list of domain activities excluding user for toasts
+# @api_v2.route('/activities', methods=['GET'])
+# def get_domain_activities():
+#     rargs = request.args
+#     params = {
+#         'api_call': uuid4(),
+#         'user_id': request.user.id,
+#         'start_param': rargs.get('start_datetime'),
+#     }
+#     logger.info("ActivityService::Info::DomainInit: {}".format(params['api_id']))
+#     return 'domain'
+#
+#
+# # Get paginated domain activities for list page for last 30 days
+# @api_v2.route('/activities/<page>', methods=['GET'])
+# def get_paginated_activities(page):
+#     params = {
+#         'api_call': uuid4(),
+#         'user_id': request.user.id,
+#         'page': page,
+#     }
+#     logger.info("ActivityService::Info::PaginatedInit: {}".format(params['api_id']))
+#     return 'domain paginated {}'.format(page)
+#
+#
+# # Get aggregated activities for drop down
+# @api_v2.route('/aggregate', methods=['GET'])
+# def get_aggregated_activities():
+#     params = {
+#         'api_call': uuid4(),
+#         'domain_id': request.user.domain_id,
+#         'start': request.args.get('start_datetime'),
+#         'user_id': request.user.id,
+#     }
+#     logger.info("ActivityService::Info::AggregateInit: {}".format(params['api_id']))
+#     return 'aggregated'
