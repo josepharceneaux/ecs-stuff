62afeda3a822c736286d2478c1b7c53896ac5717
Scotland Peters <scotland@gettalent.com>
2017-05-31 10:23:21 -0700

    Removed old file

--- candidate_service/migrations/2017-05-31-10-00-00.py
+++
@@ -1,8 +0,0 @@
-"""
-This script adds description to the candidate_experience table (in preparation of a migration from candidate_experience_bullet)
-"""
-
-from candidate_service.candidate_app import app
-from candidate_service.common.models.db import db
-
-db.session.execute("ALTER TABLE `candidate_experience` ADD COLUMN `Description` varchar(10000) DEFAULT NULL")
