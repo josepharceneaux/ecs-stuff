cc8617827e200a0ca4932b42796e73bde4946cf5
Scotland Peters <scotland@gettalent.com>
2017-06-01 13:28:11 -0700

    Added import for app, removed .all for improved performance, improved check to make sure description is present

--- candidate_service/migration_scripts/candidate_experience_bullet_migration.py
+++ candidate_service/migration_scripts/candidate_experience_bullet_migration.py
@@ -2,11 +2,13 @@
 This migration script will move "description" column data from "candidate_experience_bullet" table (table will later be removed)
 to a newly-created description field in "candidate_experience"
 """
+from candidate_service.candidate_app import app
 from time import time
 from candidate_service.common.models.db import db
 from candidate_service.common.models.candidate import Candidate
 from candidate_service.common.models.candidate import CandidateExperience
 from candidate_service.common.models.candidate import CandidateExperienceBullet
+
 
 def migrate_description_from_bullet_to_candidate_experience():
 
@@ -16,14 +18,14 @@ def migrate_description_from_bullet_to_candidate_experience():
     start = 0
     batch = 1
     while start < total_number_of_candidates:
-        candidates = Candidate.query.slice(start=start, stop=start + 100).all()
+        candidates = Candidate.query.slice(start=start, stop=start + 100)
         for candidate in candidates:
             # Migrate the work experience bullets
             can_exp = CandidateExperience.query.filter_by(candidate_id=candidate.id).all()
             for experience in can_exp:
                 # We only want the first bullet -- other bullets will be discarded
                 can_exp_bullet = CandidateExperienceBullet.query.filter_by(candidate_experience_id=experience.id).first()
-                if can_exp_bullet:
+                if can_exp_bullet and can_exp_bullet.description:
                     experience.description = can_exp_bullet.description
 
         db.session.commit()
